81cc2d3c12e22bd3b279780e9e3d0bf9a2867100
Emmanuel Proulx <emmanuel@heyday.ai>
2020-07-06 21:59:06 -0400

    Add barebones content for login view

--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -1,9 +1,76 @@
 // FLUTTER / DART / THIRD-PARTIES
 import 'package:flutter/material.dart';
+import 'package:notredame/generated/l10n.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // Define a test. The TestWidgets function also provides a WidgetTester
+  // to work with. The WidgetTester allows building and interacting
+  // with widgets in the test environment.
+  testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(LoginView());
+  });
+}
 
 class LoginView extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => const Scaffold(
-    body: Center(child: Text("Welcome to the new ÉTS Mobile!")),
-  );
+  Widget build(BuildContext context) => Scaffold(
+          body: Container(
+        padding: const EdgeInsets.only(
+            left: 20.0, right: 20.0, top: 60.0, bottom: 60.0),
+        color: const Color.fromRGBO(239, 62, 69, 1),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              cursorColor: Colors.white,
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide(color: Colors.white70)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide(color: Colors.white)),
+                  hintText: AppIntl.of(context).login_prompt_universal_code,
+                  hintStyle: TextStyle(color: Colors.white54)),
+              style: TextStyle(color: Colors.white),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              cursorColor: Colors.white,
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide(color: Colors.white70)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide(color: Colors.white)),
+                  hintText: AppIntl.of(context).login_prompt_password,
+                  hintStyle: TextStyle(color: Colors.white54)),
+              style: TextStyle(color: Colors.white),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: FlatButton(
+                onPressed: () {},
+                color: Colors.white,
+                padding: const EdgeInsets.only(bottom: 16.0, top: 16.0),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)),
+                child: Text(
+                  AppIntl.of(context).login_title,
+                  style: const TextStyle(
+                      color: Color.fromRGBO(239, 62, 69, 1), fontSize: 18),
+                ),
+              ),
+            )
+          ],
+        ),
+      ));
 }
