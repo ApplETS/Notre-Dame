c7be4887a3b4c6337816bec07b4eb6a8f9905093
Xavier Chrétien <chretienxavier42@gmail.com>
2020-07-26 01:06:42 -0400

    Linking ViewModel and View. Still need testing and some UI adjustments (App|ETS credit at the end of the view)

--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -1,65 +1,118 @@
 // FLUTTER / DART / THIRD-PARTIES
 import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+// VIEW MODEL
+import 'package:notredame/core/viewmodels/login_viewmodel.dart';
+
+// WIDGETS
+import 'package:notredame/ui/widgets/password_text_field.dart';
+
+// GENERATED
 import 'package:notredame/generated/l10n.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
   @override
-  Widget build(BuildContext context) => Scaffold(
-          body: Container(
-        padding: const EdgeInsets.only(
-            left: 20.0, right: 20.0, top: 60.0, bottom: 60.0),
-        color: const Color.fromRGBO(239, 62, 69, 1),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              cursorColor: Colors.white,
-              decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide(color: Colors.white70)),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide(color: Colors.white)),
-                  hintText: AppIntl.of(context).login_prompt_universal_code,
-                  hintStyle: TextStyle(color: Colors.white54)),
-              style: TextStyle(color: Colors.white),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              cursorColor: Colors.white,
-              decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide(color: Colors.white70)),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide(color: Colors.white)),
-                  hintText: AppIntl.of(context).login_prompt_password,
-                  hintStyle: TextStyle(color: Colors.white54)),
-              style: TextStyle(color: Colors.white),
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: FlatButton(
-                onPressed: () {},
-                color: Colors.white,
-                padding: const EdgeInsets.only(bottom: 16.0, top: 16.0),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0)),
-                child: Text(
-                  AppIntl.of(context).login_title,
-                  style: const TextStyle(
-                      color: Color.fromRGBO(239, 62, 69, 1), fontSize: 18),
-                ),
+  _LoginViewState createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final double borderRadiusOnFocus = 2.0;
+
+  final FocusScopeNode _focusNode = FocusScopeNode();
+
+  @override
+  Widget build(BuildContext context) =>
+      ViewModelBuilder<LoginViewModel>.reactive(
+        viewModelBuilder: () => LoginViewModel(intl: AppIntl.of(context)),
+        builder: (context, model, child) => Scaffold(
+          resizeToAvoidBottomInset: false,
+            body: Container(
+          padding: const EdgeInsets.only(
+              left: 20.0, right: 20.0, top: 20.0, bottom: 60.0),
+          color: const Color.fromRGBO(239, 62, 69, 1),
+          child: Form(
+            key: model.formKey,
+            onChanged: () {
+              model.formKey.currentState.validate();
+              setState(() {});
+            },
+            child: FocusScope(
+              node: _focusNode,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    'assets/images/ets_white_logo.png',
+                    excludeFromSemantics: true,
+                    width: 216,
+                    height: 216,
+                  ),
+                  TextFormField(
+                    cursorColor: Colors.white,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white70)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.white, width: borderRadiusOnFocus)),
+                      focusedErrorBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.amberAccent,
+                          width: borderRadiusOnFocus
+                        )
+                      ),
+                      errorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.amberAccent,
+                              width: borderRadiusOnFocus)),
+                      labelText:
+                          AppIntl.of(context).login_prompt_universal_code,
+                      labelStyle: TextStyle(color: Colors.white54),
+                      errorStyle: TextStyle(color: Colors.amberAccent),
+                    ),
+                    autofocus: true,
+                    style: TextStyle(color: Colors.white),
+                    onEditingComplete: _focusNode.nextFocus,
+                    validator: model.validateUniversalCode,
+                    initialValue: model.universalCode,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  PasswordFormField(
+                      validator: model.validatePassword,
+                      onEditionComplete: _focusNode.nextFocus),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: FlatButton(
+                      onPressed: model.canSubmit ? model.authenticate : null,
+                      disabledColor: Colors.white38,
+                      color: Colors.white,
+                      padding: const EdgeInsets.only(bottom: 16.0, top: 16.0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: Text(
+                        AppIntl.of(context).login_action_sign_in,
+                        style: const TextStyle(
+                            color: Color.fromRGBO(239, 62, 69, 1),
+                            fontSize: 18),
+                      ),
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
-      ));
+            ),
+          ),
+        )),
+      );
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
 }
