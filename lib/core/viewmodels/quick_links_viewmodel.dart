0f456e0756088e73d68c7d21b5cd80350662a71f
Alexandre Markus <alexandre.markus.1@ens.etsmtl.ca>
2020-08-18 20:53:23 -0400

    open links Chrome Custom Tabs on Android / SFSafariViewController on iOS

--- lib/core/viewmodels/quick_links_viewmodel.dart
+++ lib/core/viewmodels/quick_links_viewmodel.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class QuickLinks {
   String image;
   String name;
   String link;
   QuickLinks({@required this.image, @required this.name, this.link});
+}
+
+Future<void> makePhoneCall(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
 }
--- lib/ui/widgets/quick_links.dart
+++ lib/ui/widgets/quick_links.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:notredame/core/viewmodels/quick_links_viewmodel.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:notredame/ui/views/security_view.dart';
 
 class QuickLinksWidget extends StatelessWidget {
   final QuickLinks _links;
@@ -9,48 +10,70 @@ class QuickLinksWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 125,
-      height: 125,
+      width: 130,
+      height: 130,
       child: Card(
         elevation: 4.0,
-        color: Colors.white,
         child: InkWell(
           splashColor: Colors.red.withAlpha(50),
-          onTap: onLinkClicked,
-          child: Column(
-            children: [
-              Expanded(
-                flex: 40,
-                child: FittedBox(
-                  fit: BoxFit.fitWidth,
-                  child: Image.asset(_links.image),
+          onTap: () => onLinkClicked(context),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 40,
+                  child: FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: Image.asset(_links.image),
+                  ),
                 ),
-              ),
-              Text(
-                _links.name,
-                style: const TextStyle(color: Colors.red, fontSize: 20.0),
-              ),
-            ],
+                Text(
+                  _links.name,
+                  style: const TextStyle(color: Colors.red, fontSize: 18.0),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  void onLinkClicked() {
-    if (_links.link != null) _launchInBrowser(_links.link);
+  void onLinkClicked(BuildContext context) {
+    if (_links.link == 'security') {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => SecurityView()));
+    } else {
+      _launchInBrowser(_links.link);
+    }
+    /*Navigator.push(    //this option shows a webview with a back arrow
+        context,
+        MaterialPageRoute(
+            builder: (context) => WebViewContainer(
+                  selectedUrl: _links.link,
+                  title: _links.name,
+                )));*/
   }
 }
 
 Future<void> _launchInBrowser(String url) async {
-  if (await canLaunch(url)) {
+  final ChromeSafariBrowser browser =
+      ChromeSafariBrowser(); // this options opens urls with Chrome Custom Tabs on Android / SFSafariViewController on iOS. but needs minSdkVersion 17 and Xcode >= 11
+  await browser.open(
+      url: url,
+      options: ChromeSafariBrowserClassOptions(
+          android: AndroidChromeCustomTabsOptions(
+              addDefaultShareMenuItem: false, toolbarBackgroundColor: "Red"),
+          ios: IOSSafariOptions(
+              barCollapsingEnabled: true, preferredBarTintColor: "Red")));
+
+  /*if (await canLaunch(url)) {  //this option shows a webview or opens in browser
     await launch(
       url,
-      forceSafariVC: true,
-      forceWebView: true,
       enableJavaScript: true,
     );
   } else {
     throw 'Could not launch $url';
-  }
+  }*/
 }
