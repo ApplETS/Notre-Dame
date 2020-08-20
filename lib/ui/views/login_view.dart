ed806d363c52fccd920e018ae8bf0ff22268710f
Xavier Chrétien <chretienxavier42@gmail.com>
2020-08-20 11:39:34 -0400

    Adding "Made by" at the end of the LoginView

--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -27,101 +27,125 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) =>
       ViewModelBuilder<LoginViewModel>.reactive(
-        viewModelBuilder: () => LoginViewModel(
-            intl: AppIntl.of(context)),
+        viewModelBuilder: () => LoginViewModel(intl: AppIntl.of(context)),
         builder: (context, model, child) => Scaffold(
             resizeToAvoidBottomInset: false,
             body: Builder(
               builder: (BuildContext context) => Container(
                 padding: const EdgeInsets.only(
-                    left: 20.0, right: 20.0, top: 20.0, bottom: 60.0),
+                    left: 20.0, right: 20.0, top: 20.0, bottom: 20.0),
                 color: const Color.fromRGBO(239, 62, 69, 1),
-                child: Form(
-                  key: formKey,
-                  onChanged: () {
-                    setState(() {
-                      formKey.currentState.validate();
-                    });
-                  },
-                  child: FocusScope(
-                    node: _focusNode,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Image.asset(
-                          'assets/images/ets_white_logo.png',
-                          excludeFromSemantics: true,
-                          width: 216,
-                          height: 216,
-                        ),
-                        TextFormField(
-                          cursorColor: Colors.white,
-                          decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.white70)),
-                            focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: Colors.white,
-                                    width: borderRadiusOnFocus)),
-                            focusedErrorBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: Colors.amberAccent,
-                                    width: borderRadiusOnFocus)),
-                            errorBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                    color: Colors.amberAccent,
-                                    width: borderRadiusOnFocus)),
-                            labelText:
-                                AppIntl.of(context).login_prompt_universal_code,
-                            labelStyle: TextStyle(color: Colors.white54),
-                            errorStyle: TextStyle(color: Colors.amberAccent),
-                          ),
-                          autofocus: true,
-                          style: TextStyle(color: Colors.white),
-                          onEditingComplete: _focusNode.nextFocus,
-                          validator: model.validateUniversalCode,
-                          initialValue: model.universalCode,
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        PasswordFormField(
-                            validator: model.validatePassword,
-                            onEditionComplete: _focusNode.nextFocus),
-                        const SizedBox(
-                          height: 30.0,
-                        ),
-                        SizedBox(
-                          width: double.infinity,
-                          child: FlatButton(
-                            onPressed: !model.canSubmit
-                                ? null
-                                : () async {
-                                    final String error = await model.authenticate();
-
-                                    setState(() {
-                                      Scaffold.of(context).showSnackBar(
-                                          SnackBar(content: Text(error)));
-                                      formKey.currentState.reset();
-                                    });
-                                  },
-                            disabledColor: Colors.white38,
-                            color: Colors.white,
-                            padding:
-                                const EdgeInsets.only(bottom: 16.0, top: 16.0),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0)),
-                            child: Text(
-                              AppIntl.of(context).login_action_sign_in,
-                              style: const TextStyle(
-                                  color: Color.fromRGBO(239, 62, 69, 1),
-                                  fontSize: 18),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Form(
+                      key: formKey,
+                      onChanged: () {
+                        setState(() {
+                          formKey.currentState.validate();
+                        });
+                      },
+                      child: FocusScope(
+                        node: _focusNode,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Image.asset(
+                              'assets/images/ets_white_logo.png',
+                              excludeFromSemantics: true,
+                              width: 216,
+                              height: 216,
                             ),
-                          ),
-                        )
+                            TextFormField(
+                              cursorColor: Colors.white,
+                              decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.white70)),
+                                focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Colors.white,
+                                        width: borderRadiusOnFocus)),
+                                focusedErrorBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Colors.amberAccent,
+                                        width: borderRadiusOnFocus)),
+                                errorBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Colors.amberAccent,
+                                        width: borderRadiusOnFocus)),
+                                labelText: AppIntl.of(context)
+                                    .login_prompt_universal_code,
+                                labelStyle: TextStyle(color: Colors.white54),
+                                errorStyle:
+                                    TextStyle(color: Colors.amberAccent),
+                              ),
+                              autofocus: true,
+                              style: TextStyle(color: Colors.white),
+                              onEditingComplete: _focusNode.nextFocus,
+                              validator: model.validateUniversalCode,
+                              initialValue: model.universalCode,
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            PasswordFormField(
+                                validator: model.validatePassword,
+                                onEditionComplete: _focusNode.nextFocus),
+                            const SizedBox(
+                              height: 30.0,
+                            ),
+                            SizedBox(
+                              width: double.infinity,
+                              child: FlatButton(
+                                onPressed: !model.canSubmit
+                                    ? null
+                                    : () async {
+                                        final String error =
+                                            await model.authenticate();
+
+                                        setState(() {
+                                          Scaffold.of(context).showSnackBar(
+                                              SnackBar(content: Text(error)));
+                                          formKey.currentState.reset();
+                                        });
+                                      },
+                                disabledColor: Colors.white38,
+                                color: Colors.white,
+                                padding: const EdgeInsets.only(
+                                    bottom: 16.0, top: 16.0),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                child: Text(
+                                  AppIntl.of(context).login_action_sign_in,
+                                  style: const TextStyle(
+                                      color: Color.fromRGBO(239, 62, 69, 1),
+                                      fontSize: 18),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Wrap(
+                      direction: Axis.vertical,
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      spacing: -20,
+                      children: <Widget>[
+                        Text(
+                          AppIntl.of(context).login_applets_logo,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Image.asset(
+                          'assets/images/applets_white_logo.png',
+                          excludeFromSemantics: true,
+                          width: 100,
+                          height: 100,
+                        ),
                       ],
                     ),
-                  ),
+                  ],
                 ),
               ),
             )),
