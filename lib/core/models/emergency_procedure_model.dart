96ff682021b981f32320ae55ba4339f474628fd4
Alexandre Markus <alexandre.markus.1@ens.etsmtl.ca>
2020-08-24 16:32:50 -0400

    added models

---
+++ lib/core/models/emergency_procedure_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class EmergencyProcedure {
+  final String title;
+  final String detail;
+
+  EmergencyProcedure({@required this.title, @required this.detail});
+}
---
+++ lib/core/models/quick_link_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class QuickLink {
+  final String image;
+  final String name;
+  final String link;
+
+  QuickLink({@required this.image, @required this.name, @required this.link});
+}
