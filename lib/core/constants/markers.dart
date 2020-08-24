67e319a31a94d6d680b3691d73e4d7c79f8175b2
Alexandre Markus <alexandre.markus.1@ens.etsmtl.ca>
2020-08-24 16:34:43 -0400

    added info window to markers for google maps

--- lib/core/constants/markers.dart
+++ lib/core/constants/markers.dart
@@ -1,16 +1,20 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:notredame/generated/l10n.dart';
 
 final List<Marker> markers = [
   Marker(
     position: const LatLng(45.49511855948888, -73.56270170940309),
-    markerId: MarkerId('marker_id_1'),
+    markerId: MarkerId('security_building_A'),
+    infoWindow: InfoWindow(title: AppIntl.current.security_station),
   ),
   Marker(
     position: const LatLng(45.495089693692194, -73.56374294991838),
-    markerId: MarkerId('marker_id_2'),
+    markerId: MarkerId('security_building_B'),
+    infoWindow: InfoWindow(title: AppIntl.current.security_station),
   ),
   Marker(
     position: const LatLng(45.49391646843658, -73.5634878349083),
-    markerId: MarkerId('marker_id_3'),
+    markerId: MarkerId('security_building_D'),
+    infoWindow: InfoWindow(title: AppIntl.current.security_station),
   ),
 ];
