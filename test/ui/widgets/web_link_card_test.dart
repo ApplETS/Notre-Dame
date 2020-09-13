e5bb52c42d74a7a16ff286b84f7eee1b3a4751ef
AlexandreMarkus <59664238+AlexandreMarkus@users.noreply.github.com>
2020-09-13 19:27:39 -0400

    updated files name

--- test/ui/widgets/web_link_card_test.dart
+++ test/ui/widgets/web_link_card_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:notredame/core/models/quick_link_model.dart';
+import 'package:notredame/core/models/quick_link.dart';
 import 'package:notredame/ui/widgets/web_link_card.dart';
 
 import '../../helpers.dart';
