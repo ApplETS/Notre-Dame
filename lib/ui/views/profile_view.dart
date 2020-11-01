ace7ac3bfe6149e979a61ed5478aa17e775606fb
Alexandre Markus <alexandre.markus.1@ens.etsmtl.ca>
2020-11-01 13:42:51 -0500

    profile view

---
+++ lib/ui/views/profile_view.dart
@@ -0,0 +1,64 @@
+// FLUTTER / DART / THIRD-PARTIES
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+// VIEW-MODEL
+import 'package:notredame/core/viewmodels/profile_viewmodel.dart';
+
+// WIDGETS
+import 'package:notredame/ui/widgets/student_program.dart';
+
+// OTHER
+import 'package:notredame/generated/l10n.dart';
+import 'package:notredame/ui/utils/app_theme.dart';
+
+class ProfileView extends StatefulWidget {
+  @override
+  _ProfileViewState createState() => _ProfileViewState();
+}
+
+class _ProfileViewState extends State<ProfileView> {
+  @override
+  Widget build(BuildContext context) =>
+      ViewModelBuilder<ProfileViewModel>.reactive(
+        viewModelBuilder: () => ProfileViewModel(),
+        builder: (context, model, child) => Scaffold(
+          body: ListView(children: [
+            ListTile(
+              title: Text(
+                AppIntl.of(context).profile_student_status_title,
+                style: const TextStyle(color: AppTheme.etsLightRed),
+              ),
+            ),
+            ListTile(
+              title: Text(AppIntl.of(context).profile_balance),
+              trailing: Text(model.profileStudent.balance),
+            ),
+            const Divider(
+              thickness: 2,
+              indent: 10,
+              endIndent: 10,
+            ),
+            ListTile(
+              title: Text(
+                AppIntl.of(context).profile_personal_information_title,
+                style: const TextStyle(color: AppTheme.etsLightRed),
+              ),
+            ),
+            ListTile(
+              title: Text(AppIntl.of(context).profile_first_name),
+              trailing: Text(model.profileStudent.firstName),
+            ),
+            ListTile(
+              title: Text(AppIntl.of(context).profile_last_name),
+              trailing: Text(model.profileStudent.lastName),
+            ),
+            ListTile(
+                title: Text(AppIntl.of(context).profile_permanent_code),
+                trailing: Text(model.profileStudent.permanentCode)),
+            for (int index = 0; index < model.programsList.length; index++)
+              StudentProgram(model.programsList[index]),
+          ]),
+        ),
+      );
+}
