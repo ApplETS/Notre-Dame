1e0e997d85c4acf3f830fb87cd0644964380a237
Alexandre Markus <alexandre.markus.1@ens.etsmtl.ca>
2020-11-08 16:54:20 -0500

    added test for student program widget

---
+++ test/ui/widgets/student_program_test.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:notredame/core/models/program.dart';
+import 'package:notredame/ui/widgets/student_program.dart';
+
+// GENERATED
+import 'package:notredame/generated/l10n.dart';
+
+import '../../helpers.dart';
+
+final _program = Program(
+    name: 'Bac',
+    average: '3.01/4.30',
+    status: 'Actif',
+    code: '7425',
+    completedCourses: '34',
+    registeredCredits: '31',
+    accumulatedCredits: '75',
+    equivalentCourses: '0',
+    failedCourses: '0');
+
+void main() {
+  AppIntl intl;
+
+  group('Student program - ', () {
+    setUp(() async {
+      intl = await setupAppIntl();
+    });
+
+    tearDown(() {});
+
+    group('has all categories - ', () {
+      testWidgets('contains one divider and one column',
+          (WidgetTester tester) async {
+        await tester
+            .pumpWidget(localizedWidget(child: StudentProgram(_program)));
+        await tester.pumpAndSettle();
+
+        final divider = find.byType(Divider);
+        expect(divider, findsNWidgets(1));
+
+        final column = find.byType(Column);
+        expect(column, findsNWidgets(1));
+      });
+
+      testWidgets('contains 9 listTile', (WidgetTester tester) async {
+        await tester
+            .pumpWidget(localizedWidget(child: StudentProgram(_program)));
+        await tester.pumpAndSettle();
+
+        final listTiles = find.byType(ListTile);
+        expect(listTiles, findsNWidgets(9));
+
+        expect(find.widgetWithText(ListTile, intl.profile_code_program),
+            findsOneWidget);
+
+        expect(find.widgetWithText(ListTile, intl.profile_average_program),
+            findsOneWidget);
+
+        expect(
+            find.widgetWithText(
+                ListTile, intl.profile_number_accumulated_credits_program),
+            findsOneWidget);
+
+        expect(
+            find.widgetWithText(
+                ListTile, intl.profile_number_registered_credits_program),
+            findsOneWidget);
+
+        expect(
+            find.widgetWithText(
+                ListTile, intl.profile_number_completed_courses_program),
+            findsOneWidget);
+
+        expect(
+            find.widgetWithText(
+                ListTile, intl.profile_number_failed_courses_program),
+            findsOneWidget);
+
+        expect(
+            find.widgetWithText(
+                ListTile, intl.profile_number_equivalent_courses_program),
+            findsOneWidget);
+
+        expect(find.widgetWithText(ListTile, intl.profile_status_program),
+            findsOneWidget);
+      });
+
+      testWidgets('contains 17 Text fields', (WidgetTester tester) async {
+        await tester
+            .pumpWidget(localizedWidget(child: StudentProgram(_program)));
+        await tester.pumpAndSettle();
+
+        final text = find.byType(Text);
+        expect(text, findsNWidgets(17));
+      });
+    });
+  });
+}
