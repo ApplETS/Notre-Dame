95008a810b7569e44f03297efc3c69fdd2fc192c
Xavier Chrétien <chretienxavier42@gmail.com>
2020-12-27 14:54:12 -0500

    Update documentation and fix warnings.

--- test/services/signets_api_test.dart
+++ test/services/signets_api_test.dart
@@ -36,9 +36,12 @@ void main() {
       const String username = "username";
       const String password = "password";
 
+      // ignore: missing_whitespace_between_adjacent_strings
       const expectedResult = '<?xml version="1.0" encoding="utf-8"?>'
+          // ignore: missing_whitespace_between_adjacent_strings
           '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">'
           '<soap:Body>'
+          // ignore: missing_whitespace_between_adjacent_strings
           '<testOperation xmlns="http://etsmtl.ca/">'
           '<codeAccesUniversel>username</codeAccesUniversel>'
           '<motPasse>password</motPasse>'
