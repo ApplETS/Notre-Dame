d699e5cfecbe4904bf99f53ad3600c6a4a1ea9ba
Xavier Chrétien <chretienxavier42@gmail.com>
2020-12-27 14:07:09 -0500

    Test.

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -5,4 +5,5 @@
 pwd
 openssl version -v
 # Decrypt Signets API
-openssl aes-256-cbc -d -k "$ENCRYPTED_SIGNETS_API_CERT_PASSWORD" -in "$pwd/assets/encryptedFiles/signets_cert.crt.enc" -out "$pwd/assets/certificates/signets_cert.crt"
+mkdir ./assets/certificates
+openssl aes-256-cbc -d -k "$ENCRYPTED_SIGNETS_API_CERT_PASSWORD" -in ./assets/encryptedFiles/signets_cert.crt.enc -out ./assets/certificates/signets_cert.crt
