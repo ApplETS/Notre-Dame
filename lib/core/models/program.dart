ec89a1af6ed6b6ea9954302f3d8ead21e1049e6c
alexandremarkus <alexandre.markus.1@ens.etsmtl.ca>
2020-12-31 16:43:37 -0500

    Implementing Programs cache manager and SignetsApi functions

--- lib/core/models/program.dart
+++ lib/core/models/program.dart
@@ -1,5 +1,6 @@
 // FLUTTER / DART / THIRD-PARTIES
 import 'package:flutter/material.dart';
+import 'package:xml/xml.dart';
 
 class Program {
   final String name;
@@ -22,4 +23,40 @@ class Program {
       @required this.failedCourses,
       @required this.equivalentCourses,
       @required this.status});
+
+  /// Used to create a new [Program] instance from a [XMLElement].
+  factory Program.fromXmlNode(XmlElement node) => Program(
+      name: node.getElement('libelle').innerText,
+      code: node.getElement('code').innerText,
+      average: node.getElement('moyenne').innerText,
+      accumulatedCredits: node.getElement('nbCreditsCompletes').innerText,
+      registeredCredits: node.getElement('nbCreditsInscrits').innerText,
+      completedCourses: node.getElement('nbCrsReussis').innerText,
+      failedCourses: node.getElement('nbCrsEchoues').innerText,
+      equivalentCourses: node.getElement('nbEquivalences').innerText,
+      status: node.getElement('statut').innerText);
+
+  /// Used to create [CourseActivity] instance from a JSON file
+  factory Program.fromJson(Map<String, dynamic> map) => Program(
+      name: map['libelle'] as String,
+      code: map['code'] as String,
+      average: map['moyenne'] as String,
+      accumulatedCredits: map['nbCreditsCompletes'] as String,
+      registeredCredits: map['nbCreditsInscrits'] as String,
+      completedCourses: map['nbCrsReussis'] as String,
+      failedCourses: map['nbCrsEchoues'] as String,
+      equivalentCourses: map['nbEquivalences'] as String,
+      status: map['statut'] as String);
+
+  Map<String, dynamic> toJson() => {
+        'libelle': name,
+        'code': code,
+        'moyenne': average,
+        'nbCreditsCompletes': accumulatedCredits,
+        'nbCreditsInscrits': registeredCredits,
+        'nbCrsReussis': completedCourses,
+        'nbCrsEchoues': failedCourses,
+        'nbEquivalences': equivalentCourses,
+        'statut': status,
+      };
 }
