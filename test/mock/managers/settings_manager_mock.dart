1bd80658df9846311e7a582a4d0b92d97e9df149
Xavier Chrétien <xavier.chretien@bnc.ca>
2021-01-08 17:46:21 -0500

    schedule_viewmodel is now tested!

--- test/mock/managers/settings_manager_mock.dart
+++ test/mock/managers/settings_manager_mock.dart
@@ -4,4 +4,22 @@ import 'package:mockito/mockito.dart';
 // MANAGER
 import 'package:notredame/core/managers/settings_manager.dart';
 
-class SettingsManagerMock extends Mock implements SettingsManager {}
+// MODEL
+import 'package:notredame/core/constants/preferences_flags.dart';
+
+class SettingsManagerMock extends Mock implements SettingsManager {
+  /// Stub the [getScheduleSettings] function of [mock], when called return [toReturn].
+  static void stubGetScheduleSettings(SettingsManagerMock mock, {Map<PreferencesFlag, dynamic> toReturn = const {}}) {
+    when(mock.getScheduleSettings()).thenAnswer((_) async => toReturn);
+  }
+
+  /// Stub the [setString] function of [mock], when called with [flag] return [toReturn].
+  static void stubSetString(SettingsManagerMock mock, PreferencesFlag flag, {bool toReturn = true}) {
+    when(mock.setString(flag, any)).thenAnswer((_) async => toReturn);
+  }
+
+  /// Stub the [setBool] function of [mock], when called with [flag] return [toReturn].
+  static void stubSetBool(SettingsManagerMock mock, PreferencesFlag flag, {bool toReturn = true}) {
+    when(mock.setBool(flag, any)).thenAnswer((_) async => toReturn);
+  }
+}
