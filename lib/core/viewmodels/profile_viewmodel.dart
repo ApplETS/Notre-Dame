894c88a8171f22cb1b187a9d94c446e5b6b046da
alexandremarkus <alexandre.markus.1@ens.etsmtl.ca>
2021-01-10 10:44:22 -0500

    optimize loading and added on error

--- lib/core/viewmodels/profile_viewmodel.dart
+++ lib/core/viewmodels/profile_viewmodel.dart
@@ -1,9 +1,10 @@
 // FLUTTER / DART / THIRD-PARTIES
 import 'package:notredame/core/managers/user_repository.dart';
-import 'package:notredame/core/models/mon_ets_user.dart';
 import 'package:notredame/core/models/profile_student.dart';
 import 'package:notredame/core/models/program.dart';
+import 'package:oktoast/oktoast.dart';
 import 'package:stacked/stacked.dart';
+import 'package:notredame/generated/l10n.dart';
 
 import '../../locator.dart';
 
@@ -20,6 +21,12 @@ class ProfileViewModel extends FutureViewModel<List<Program>> {
 
   String get universalAccessCode => _userRepository.monETSUser.universalCode;
 
+  @override
+  // ignore: type_annotate_public_apis
+  void onError(error) {
+    showToast(AppIntl.current.error);
+  }
+
   Future<List<Program>> getProgramsList() async {
     if (_programList == null || _programList.isEmpty) {
       _programList = [];
@@ -32,10 +39,17 @@ class ProfileViewModel extends FutureViewModel<List<Program>> {
     return _programList;
   }
 
+  bool isLoadingEvents = false;
+
   @override
-  Future<List<Program>> futureToRun() {
-    return _userRepository
-        .getInfo()
-        .then((value) => _userRepository.getPrograms());
-  }
+  Future<List<Program>> futureToRun() => _userRepository
+          .getInfo(fromCacheOnly: true)
+          .then((value) => _userRepository.getPrograms(fromCacheOnly: true))
+          .then((value) {
+        setBusyForObject(isLoadingEvents, true);
+        _userRepository.getInfo().catchError(onError).then((value) {
+          _userRepository.getPrograms().catchError(onError);
+        }).whenComplete(() => setBusyForObject(isLoadingEvents, false));
+        return value;
+      });
 }
