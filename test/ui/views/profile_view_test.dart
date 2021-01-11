8a15e413e8510a6f72ff92c22f17ed49a345c962
alexandremarkus <alexandre.markus.1@ens.etsmtl.ca>
2021-01-11 12:16:51 -0500

    added one golden test

--- test/ui/views/profile_view_test.dart
+++ test/ui/views/profile_view_test.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:notredame/core/managers/user_repository.dart';
 import 'package:notredame/ui/views/profile_view.dart';
 
 // GENERATED
 import 'package:notredame/generated/l10n.dart';
 
 import '../../helpers.dart';
+import '../../mock/managers/user_repository_mock.dart';
 
 void main() {
   AppIntl intl;
+  UserRepository userRepository;
   group('Profile view - ', () {
     setUp(() async {
       intl = await setupAppIntl();
+      setupNavigationServiceMock();
+      userRepository = setupUserRepositoryMock();
     });
 
     tearDown(() {});
@@ -52,6 +57,23 @@ void main() {
 
       expect(find.widgetWithText(ListTile, intl.profile_permanent_code),
           findsOneWidget);
+    });
+
+    group("golden - ", () {
+      testWidgets("default view (no events)", (WidgetTester tester) async {
+        tester.binding.window.physicalSizeTestValue = const Size(800, 1410);
+
+        UserRepositoryMock.stubGetInfo(userRepository as UserRepositoryMock);
+
+        UserRepositoryMock.stubGetPrograms(
+            userRepository as UserRepositoryMock);
+
+        await tester.pumpWidget(localizedWidget(child: ProfileView()));
+        await tester.pumpAndSettle();
+
+        await expectLater(find.byType(ProfileView),
+            matchesGoldenFile(goldenFilePath("profileView_1")));
+      });
     });
   });
 }
