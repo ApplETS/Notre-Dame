d0571da0c55478727074dfd6978eb56b0acc3072
Xavier Chrétien <chretienxavier42@gmail.com>
2021-01-13 22:17:32 -0500

    Loading implemented ;)

--- lib/ui/views/profile_view.dart
+++ lib/ui/views/profile_view.dart
@@ -1,87 +1,90 @@
 // FLUTTER / DART / THIRD-PARTIES
 import 'package:flutter/material.dart';
-import 'package:notredame/core/models/program.dart';
-import 'package:notredame/ui/widgets/base_scaffold.dart';
 import 'package:stacked/stacked.dart';
+
+// MODELS
+import 'package:notredame/core/models/program.dart';
 
 // VIEW-MODEL
 import 'package:notredame/core/viewmodels/profile_viewmodel.dart';
 
 // WIDGETS
 import 'package:notredame/ui/widgets/student_program.dart';
+import 'package:notredame/ui/utils/loading.dart';
 
 // OTHER
 import 'package:notredame/generated/l10n.dart';
 import 'package:notredame/ui/utils/app_theme.dart';
 
-class ProfileView extends StatefulWidget {
-  @override
-  _ProfileViewState createState() => _ProfileViewState();
-}
+class ProfileView extends StatelessWidget {
 
-class _ProfileViewState extends State<ProfileView> {
   @override
   Widget build(BuildContext context) =>
       ViewModelBuilder<ProfileViewModel>.reactive(
-        viewModelBuilder: () => ProfileViewModel(),
-        builder: (context, model, child) => BaseScaffold(
-          isLoading: model.busy(model.isLoadingEvents),
-          isInteractionLimitedWhileLoading: false,
-          body: ListView(children: [
-            ListTile(
-              title: Text(
-                AppIntl.of(context).profile_student_status_title,
-                style: const TextStyle(color: AppTheme.etsLightRed),
-              ),
-            ),
-            ListTile(
-              title: Text(AppIntl.of(context).profile_balance),
-              trailing: Text(model.profileStudent.balance),
-            ),
-            const Divider(
-              thickness: 2,
-              indent: 10,
-              endIndent: 10,
-            ),
-            ListTile(
-              title: Text(
-                AppIntl.of(context).profile_personal_information_title,
-                style: const TextStyle(color: AppTheme.etsLightRed),
-              ),
-            ),
-            ListTile(
-              title: Text(AppIntl.of(context).profile_first_name),
-              trailing: Text(model.profileStudent.firstName),
-            ),
-            ListTile(
-              title: Text(AppIntl.of(context).profile_last_name),
-              trailing: Text(model.profileStudent.lastName),
-            ),
-            ListTile(
-                title: Text(AppIntl.of(context).profile_permanent_code),
-                trailing: Text(model.profileStudent.permanentCode)),
-            ListTile(
-                title: Text(AppIntl.of(context).login_prompt_universal_code),
-                trailing: Text(model.universalAccessCode)),
-            FutureBuilder(
-              future: model.programList(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (!snapshot.hasData) {
-                  return Container();
-                } else {
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    reverse: true,
-                    physics: const ScrollPhysics(),
-                    itemCount: snapshot.data.length as int,
-                    itemBuilder: (BuildContext context, int index) {
-                      return StudentProgram(snapshot.data[index] as Program);
+          viewModelBuilder: () => ProfileViewModel(),
+          builder: (context, model, child) {
+            return Stack(
+              children: [
+                ListView(children: [
+                  ListTile(
+                    title: Text(
+                      AppIntl.of(context).profile_student_status_title,
+                      style: const TextStyle(color: AppTheme.etsLightRed),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(AppIntl.of(context).profile_balance),
+                    trailing: Text(model.profileStudent.balance),
+                  ),
+                  const Divider(
+                    thickness: 2,
+                    indent: 10,
+                    endIndent: 10,
+                  ),
+                  ListTile(
+                    title: Text(
+                      AppIntl.of(context).profile_personal_information_title,
+                      style: const TextStyle(color: AppTheme.etsLightRed),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(AppIntl.of(context).profile_first_name),
+                    trailing: Text(model.profileStudent.firstName),
+                  ),
+                  ListTile(
+                    title: Text(AppIntl.of(context).profile_last_name),
+                    trailing: Text(model.profileStudent.lastName),
+                  ),
+                  ListTile(
+                      title: Text(AppIntl.of(context).profile_permanent_code),
+                      trailing: Text(model.profileStudent.permanentCode)),
+                  ListTile(
+                      title:
+                          Text(AppIntl.of(context).login_prompt_universal_code),
+                      trailing: Text(model.universalAccessCode)),
+                  FutureBuilder(
+                    future: model.programList(),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (!snapshot.hasData) {
+                        return Container();
+                      } else {
+                        return ListView.builder(
+                          shrinkWrap: true,
+                          reverse: true,
+                          physics: const ScrollPhysics(),
+                          itemCount: snapshot.data.length as int,
+                          itemBuilder: (BuildContext context, int index) {
+                            return StudentProgram(
+                                snapshot.data[index] as Program);
+                          },
+                        );
+                      }
                     },
-                  );
-                }
-              },
-            ),
-          ]),
-        ),
-      );
+                  ),
+                ]),
+
+                if(model.isBusy) buildLoading(isInteractionLimitedWhileLoading: false) else const SizedBox()
+              ],
+            );
+          });
 }
--- lib/ui/views/student_view.dart
+++ lib/ui/views/student_view.dart
@@ -1,9 +1,9 @@
+// FLUTTER / DART / THIRD-PARTIES
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:notredame/core/viewmodels/profile_viewmodel.dart';
+
+// WIDGET
 import 'package:notredame/ui/views/profile_view.dart';
 import 'package:notredame/ui/widgets/base_scaffold.dart';
-import 'package:stacked/stacked.dart';
 
 // OTHER
 import 'package:notredame/generated/l10n.dart';
@@ -19,36 +19,40 @@ class _StudentViewState extends State<StudentView> {
     AppIntl.current.profile_title
   ];
 
-  List<Widget> tabsView = [Scaffold(), ProfileView()];
+  List<Widget> tabsView = [Container(), ProfileView()];
 
   @override
-  Widget build(BuildContext context) => DefaultTabController(
-        length: tabs.length,
-        child: NestedScrollView(
-          floatHeaderSlivers: true,
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return <Widget>[
-              SliverAppBar(
-                elevation: 4.0,
-                automaticallyImplyLeading: false,
-                pinned: true,
-                floating: true,
-                title: Text(AppIntl.of(context).title_student),
-                forceElevated: innerBoxIsScrolled,
-                bottom: TabBar(
-                  indicatorColor: Colors.white,
-                  tabs: List.generate(
-                    tabs.length,
-                    (index) => Tab(
-                      text: tabs[index],
+  Widget build(BuildContext context) => BaseScaffold(
+        isInteractionLimitedWhileLoading: false,
+        body: DefaultTabController(
+          length: tabs.length,
+          child: NestedScrollView(
+            floatHeaderSlivers: true,
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                  elevation: 4.0,
+                  automaticallyImplyLeading: false,
+                  pinned: true,
+                  floating: true,
+                  title: Text(AppIntl.of(context).title_student),
+                  forceElevated: innerBoxIsScrolled,
+                  bottom: TabBar(
+                    indicatorColor: Colors.white,
+                    tabs: List.generate(
+                      tabs.length,
+                      (index) => Tab(
+                        text: tabs[index],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ];
-          },
-          body: TabBarView(
-            children: tabsView,
+              ];
+            },
+            body: TabBarView(
+              children: tabsView,
+            ),
           ),
         ),
       );
