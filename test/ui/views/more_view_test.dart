b954626b55a831d0e5b5f7a1f8b4ca009377c3ec
alexandremarkus <alexandre.markus.1@ens.etsmtl.ca>
2021-03-06 10:11:14 -0500

    added tests for navigation

--- test/ui/views/more_view_test.dart
+++ test/ui/views/more_view_test.dart
@@ -1,6 +1,17 @@
 // FLUTTER / DART / THIRD-PARTIES
+import 'package:feedback/feedback.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+// CONSTANTS
+import 'package:notredame/core/constants/router_paths.dart';
+
+// GENERATED
+import 'package:notredame/generated/l10n.dart';
+
+// SERVICES
+import 'package:notredame/core/services/navigation_service.dart';
 
 // VIEW
 import 'package:notredame/ui/views/more_view.dart';
@@ -8,18 +19,21 @@ import 'package:notredame/ui/views/more_view.dart';
 import '../../helpers.dart';
 
 void main() {
+  AppIntl intl;
+  NavigationService navigation;
   group('MoreView - ', () {
     setUp(() async {
-      setupNavigationServiceMock();
+      intl = await setupAppIntl();
+      navigation = setupNavigationServiceMock();
       setupCacheManagerMock();
     });
 
     tearDown(() {});
 
     group('UI - ', () {
-      testWidgets('has 1 listView and 6 listTiles', (WidgetTester tester) async {
-        await tester.pumpWidget(localizedWidget(
-            child: MoreView()));
+      testWidgets('has 1 listView and 6 listTiles',
+          (WidgetTester tester) async {
+        await tester.pumpWidget(localizedWidget(child: MoreView()));
         await tester.pumpAndSettle();
 
         final listview = find.byType(ListView);
@@ -27,6 +41,49 @@ void main() {
 
         final listTile = find.byType(ListTile);
         expect(listTile, findsNWidgets(6));
+      });
+
+      group('navigation - ', () {
+        testWidgets('about', (WidgetTester tester) async {
+          await tester.pumpWidget(localizedWidget(child: MoreView()));
+          await tester.pumpAndSettle();
+
+          // Tap the button.
+          await tester.tap(
+              find.widgetWithText(ListTile, intl.more_about_applets_title));
+
+          // Rebuild the widget after the state has changed.
+          await tester.pump();
+
+          verify(navigation.pushNamed(RouterPaths.about)).called(1);
+        });
+
+        testWidgets('contributors', (WidgetTester tester) async {
+          await tester.pumpWidget(localizedWidget(child: MoreView()));
+          await tester.pumpAndSettle();
+
+          // Tap the button.
+          await tester
+              .tap(find.widgetWithText(ListTile, intl.more_contributors));
+
+          // Rebuild the widget after the state has changed.
+          await tester.pump();
+
+          verify(navigation.pushNamed(RouterPaths.contributors)).called(1);
+        });
+
+        testWidgets('parameters', (WidgetTester tester) async {
+          await tester.pumpWidget(localizedWidget(child: MoreView()));
+          await tester.pumpAndSettle();
+
+          // Tap the button.
+          await tester.tap(find.widgetWithText(ListTile, intl.settings_title));
+
+          // Rebuild the widget after the state has changed.
+          await tester.pump();
+
+          verify(navigation.pushNamed(RouterPaths.settings)).called(1);
+        });
       });
 
       group("golden - ", () {
