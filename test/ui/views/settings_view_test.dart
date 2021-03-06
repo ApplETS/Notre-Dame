1e93f5cb85acef873328f952146e8da52c6623e2
alexandremarkus <alexandre.markus.1@ens.etsmtl.ca>
2021-03-06 17:24:35 -0500

    added tests for PopupMenuButtons

--- test/ui/views/settings_view_test.dart
+++ test/ui/views/settings_view_test.dart
@@ -2,14 +2,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+// GENERATED
+import 'package:notredame/generated/l10n.dart';
+
 // VIEW
 import 'package:notredame/ui/views/settings_view.dart';
 
 import '../../helpers.dart';
 
 void main() {
+  AppIntl intl;
   group('SettingsView - ', () {
     setUp(() async {
+      intl = await setupAppIntl();
       setupNavigationServiceMock();
       setupCacheManagerMock();
       setupSettingsManagerMock();
@@ -18,9 +23,9 @@ void main() {
     tearDown(() {});
 
     group('UI - ', () {
-      testWidgets('has 1 listView and 4 listTiles and 1 divider', (WidgetTester tester) async {
-        await tester.pumpWidget(localizedWidget(
-            child: SettingsView()));
+      testWidgets('has 1 listView and 4 listTiles and 1 divider',
+          (WidgetTester tester) async {
+        await tester.pumpWidget(localizedWidget(child: SettingsView()));
         await tester.pumpAndSettle();
 
         final listview = find.byType(ListView);
@@ -33,8 +38,125 @@ void main() {
         expect(divider, findsOneWidget);
       });
 
+      group('Theme button - ', () {
+        testWidgets('light theme', (WidgetTester tester) async {
+          await tester.pumpWidget(localizedWidget(child: SettingsView()));
+          await tester.pumpAndSettle();
+
+          // Tap the button.
+          await tester.tap(
+              find.widgetWithText(ListTile, intl.settings_dark_theme_pref));
+
+          // Rebuild the widget after the state has changed.
+          await tester.pumpAndSettle();
+
+          expect(find.text(intl.light_theme), findsOneWidget);
+          expect(find.text(intl.dark_theme), findsOneWidget);
+          expect(find.text(intl.system_theme), findsNWidgets(2));
+
+          // Tap the button.
+          await tester.tap(find.text(intl.light_theme));
+          // Rebuild the widget after the state has changed.
+          await tester.pumpAndSettle();
+
+          expect(find.byIcon(Icons.wb_sunny), findsOneWidget);
+        });
+
+        testWidgets('dark theme', (WidgetTester tester) async {
+          await tester.pumpWidget(localizedWidget(child: SettingsView()));
+          await tester.pumpAndSettle();
+
+          // Tap the button.
+          await tester.tap(
+              find.widgetWithText(ListTile, intl.settings_dark_theme_pref));
+
+          // Rebuild the widget after the state has changed.
+          await tester.pumpAndSettle();
+
+          expect(find.text(intl.light_theme), findsOneWidget);
+          expect(find.text(intl.dark_theme), findsOneWidget);
+          expect(find.text(intl.system_theme), findsNWidgets(2));
+
+          // Tap the button.
+          await tester.tap(find.text(intl.dark_theme));
+          // Rebuild the widget after the state has changed.
+          await tester.pumpAndSettle();
+
+          expect(find.byIcon(Icons.nightlight_round), findsOneWidget);
+        });
+
+        testWidgets('system theme', (WidgetTester tester) async {
+          await tester.pumpWidget(localizedWidget(child: SettingsView()));
+          await tester.pumpAndSettle();
+
+          // Tap the button.
+          await tester.tap(
+              find.widgetWithText(ListTile, intl.settings_dark_theme_pref));
+
+          // Rebuild the widget after the state has changed.
+          await tester.pumpAndSettle();
+
+          expect(find.text(intl.light_theme), findsOneWidget);
+          expect(find.text(intl.dark_theme), findsOneWidget);
+          expect(find.text(intl.system_theme), findsNWidgets(2));
+
+          // Tap the button.
+          await tester.tap(find.text(intl.system_theme).last);
+          // Rebuild the widget after the state has changed.
+          await tester.pumpAndSettle();
+
+          expect(find.byIcon(Icons.brightness_auto), findsOneWidget);
+        });
+      });
+
+      group('Language button - ', () {
+        testWidgets('french', (WidgetTester tester) async {
+          await tester.pumpWidget(localizedWidget(child: SettingsView()));
+          await tester.pumpAndSettle();
+
+          // Tap the button.
+          await tester
+              .tap(find.widgetWithText(ListTile, intl.settings_language_pref));
+
+          // Rebuild the widget after the state has changed.
+          await tester.pumpAndSettle();
+
+          expect(find.text(intl.settings_french), findsNWidgets(2));
+          expect(find.text(intl.settings_english), findsOneWidget);
+
+          // Tap the button.
+          await tester.tap(find.text(intl.settings_french).last);
+          // Rebuild the widget after the state has changed.
+          await tester.pumpAndSettle();
+
+          expect(find.text(intl.settings_french), findsOneWidget);
+        });
+
+        testWidgets('english', (WidgetTester tester) async {
+          await tester.pumpWidget(localizedWidget(child: SettingsView()));
+          await tester.pumpAndSettle();
+
+          // Tap the button.
+          await tester
+              .tap(find.widgetWithText(ListTile, intl.settings_language_pref));
+
+          // Rebuild the widget after the state has changed.
+          await tester.pumpAndSettle();
+
+          expect(find.text(intl.settings_french), findsNWidgets(2));
+          expect(find.text(intl.settings_english), findsOneWidget);
+
+          // Tap the button.
+          await tester.tap(find.text(intl.settings_english));
+          // Rebuild the widget after the state has changed.
+          await tester.pumpAndSettle();
+
+          expect(find.text(intl.settings_english), findsOneWidget);
+        });
+      });
+
       group("golden - ", () {
-        testWidgets("default view (no events)", (WidgetTester tester) async {
+        testWidgets("default view", (WidgetTester tester) async {
           tester.binding.window.physicalSizeTestValue = const Size(800, 1410);
 
           await tester.pumpWidget(localizedWidget(child: SettingsView()));
