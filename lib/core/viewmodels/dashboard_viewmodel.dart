31368ded2a50b64ae2151a46a044a5aa152616af
elornHamon <elorn.hamon@gmail.com>
2021-03-16 00:32:10 -0400

    dashboard: Apply review changes
    Fix imports ordering, use launchURL instead of launch,
    use urls.dart strings, use Listview instead of Column to present cards,
    add intl in test

--- lib/core/viewmodels/dashboard_viewmodel.dart
+++ lib/core/viewmodels/dashboard_viewmodel.dart
@@ -1,24 +1,6 @@
 // FLUTTER / DART / THIRD-PARTIES
-import 'package:flutter/cupertino.dart';
-import 'package:oktoast/oktoast.dart';
 import 'package:stacked/stacked.dart';
-import 'package:table_calendar/table_calendar.dart';
-
-// MANAGER
-import 'package:notredame/core/managers/course_repository.dart';
-import 'package:notredame/core/managers/settings_manager.dart';
-
-// MODELS
-import 'package:notredame/core/models/course_activity.dart';
-
-// OTHER
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:notredame/locator.dart';
-import 'package:notredame/core/constants/preferences_flags.dart';
 
 class DashboardViewModel extends BaseViewModel {
-  /// Localization class of the application.
-  final AppIntl _appIntl;
-
-  DashboardViewModel({@required AppIntl intl}) : _appIntl = intl;
+  DashboardViewModel();
 }
