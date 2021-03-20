90f387bf7ff8f44e650699add8967c82a10438dd
Samuel Montambault <amos.samuel@hotmail.com>
2021-03-20 12:40:08 -0400

    Not found page (#63)
    * Not found page
    
    addition of a not found page widget.
    addition of a view model to handle the navigation service.
    addition of test to cover the view model and the view.
    
    * Correct merge issue & generate golden file
    
    * Correct some test on not found
    
    * Remove the base scaffold - not needed
    
    * Update Golden file
    
    * Addition of a page name & of analyticsService
    
    * Addition of placeholder function to include the pagename
    
    * Fix flutter analyse & modify the back button string
    
    * Modify UI of the page
    
    * Modification of the golden file
    
    * Added a test for the new prop and revert to the previous button style
    
    * Rive File Animation
    
    Correction of page not found templating
    Addition of a riveAnimationService for better accessibility througout the code
    Addition of the rive file and function in view model to handle starting it
    Mocking and testing  of new functionalities
    
    * Correct test error
    
    Clean code using lambda in not found model
    Modify a bad behavior where two instance of view model were created
    Addition of some tests for the view and viewmodel
    
    * Update golden files
    
    * Addition of the pubspec lock file
    
    * fix distance inter-widget
    
    * update golden
    
    also fix a UI problem
    
    * remove trailing whitespace in intl file
    
    * remove getter for final variable
    
    * Fix issue in comments
    
    Also added some files in gitignore which were automatically added

---
+++ lib/ui/views/not_found_view.dart
@@ -0,0 +1,105 @@
+// FLUTTER / DART / THIRD-PARTIES
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'package:rive/rive.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+// VIEWMODELS
+import 'package:notredame/core/viewmodels/not_found_viewmodel.dart';
+
+// OTHER
+import 'package:notredame/ui/utils/app_theme.dart';
+
+class NotFoundView extends StatefulWidget {
+  final String pageName;
+
+  const NotFoundView({this.pageName});
+
+  @override
+  _NotFoundState createState() => _NotFoundState();
+}
+
+class _NotFoundState extends State<NotFoundView> {
+  NotFoundViewModel viewModel;
+
+  _NotFoundState();
+
+  @override
+  void initState() {
+    viewModel = NotFoundViewModel(pageName: widget.pageName);
+    viewModel
+        .loadRiveAnimation()
+        .then((_) => setState(() => viewModel.startRiveAnimation()));
+
+    super.initState();
+  }
+
+  Widget _buildAnimatedWidget() {
+    return viewModel.artboard != null
+        ? SizedBox(
+            width: 100,
+            height: 80,
+            child: Rive(
+              artboard: viewModel.artboard,
+              fit: BoxFit.fitWidth,
+            ))
+        : Container();
+  }
+
+  @override
+  Widget build(BuildContext context) =>
+      ViewModelBuilder<NotFoundViewModel>.nonReactive(
+          viewModelBuilder: () => viewModel,
+          builder: (context, model, child) => Scaffold(
+                body: SafeArea(
+                  minimum: const EdgeInsets.all(20),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            bottom: 60,
+                          ),
+                          child: _buildAnimatedWidget(),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            bottom: 80,
+                          ),
+                          child: Text(
+                            AppIntl.of(context).not_found_title,
+                            style: const TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            bottom: 70,
+                          ),
+                          child: Text(
+                            AppIntl.of(context)
+                              .not_found_message(model.notFoundPageName),
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: AppTheme.primary,
+                            ),
+                            onPressed: () {
+                              model.navigateToDashboard();
+                            },
+                            child: Text(AppIntl.of(context).go_to_dashboard),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ));
+}
