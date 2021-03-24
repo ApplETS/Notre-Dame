94d516eaf94dd6e5b54905874d476017247fa5e4
Samuel Montambault <amos.samuel@hotmail.com>
2021-03-23 21:09:18 -0400

    Addition of a way to pass a detail widget for the discovery

--- lib/core/models/discovery.dart
+++ lib/core/models/discovery.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class Discovery {
   final String id;
   final String title;
-  final String detail;
+  final Widget detail;
 
   Discovery({@required this.id, @required this.title, @required this.detail});
 }
