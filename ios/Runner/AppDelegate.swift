9daa412ec901deca1e93e77a3a4f675226bcea81
alexandremarkus <alexandre.markus.1@ens.etsmtl.ca>
2021-03-27 12:43:45 -0400

    try to fix maps api from env on IOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,14 +1,16 @@
 import UIKit
 import Flutter
 import GoogleMaps
+#import "FlutterConfigPlugin.h"
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  NSString *apiMaps = [FlutterConfigPlugin envFor:@"MAPS_API_KEY"];
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey(Bundle.main.object(forInfoDictionaryKey: "MAPS_API_KEY") as? String ?? "")
+    GMSServices.provideAPIKey(Bundle.main.object(forInfoDictionaryKey: "MAPS_API_KEY") as? String ?? "@apiMaps")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
