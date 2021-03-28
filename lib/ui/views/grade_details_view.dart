d49f238f3b75520c26e11b2a5565f5afa653e25a
Camille Brulotte <37625944+camillebrulotte@users.noreply.github.com>
2021-03-27 21:27:59 -0400

    Add REALLY basic dropdown to show assigment details

--- lib/ui/views/grade_details_view.dart
+++ lib/ui/views/grade_details_view.dart
@@ -25,84 +25,114 @@ class _GradesDetailsViewState extends State<GradesDetailsView> {
         appBar: AppBar(
           elevation: 0,
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              constraints: BoxConstraints(minWidth: MediaQuery.of(context).size.width, maxHeight: 75),
-              decoration: const BoxDecoration(color: AppTheme.primary,),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(left: 15.0),
-                    child: Text("COM110", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 20)),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 15.0, top: 3),
-                    child: Text("Méthodes de communication", style: TextStyle(color: Colors.white)),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 15.0, top: 3),
-                    child: Text("Groupe 02", style: TextStyle(color: Colors.white)),
-                  ),
-                ]
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                children: <Widget> [
-                  Row(
-                    children: [
-                      Container(width: 100, height: 100, decoration: const BoxDecoration(color: Colors.green)),
+        body: 
+            ListView(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              children: <Widget>[
+                Container(
+                  constraints: BoxConstraints(minWidth: MediaQuery.of(context).size.width, maxHeight: 75),
+                  decoration: const BoxDecoration(color: AppTheme.primary,),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const <Widget>[
                       Padding(
-                        padding: const EdgeInsets.only(left: 25.0),
-                        child: Column(
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: const <Widget> [
-                                Text("85,3/100 (85 %)", style: TextStyle(color: Colors.green)),                                
-                                Text("Votre note", style: TextStyle(color: Colors.green)),
+                        padding: EdgeInsets.only(left: 15.0),
+                        child: Text("COM110", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 20)),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 15.0, top: 3),
+                        child: Text("Méthodes de communication", style: TextStyle(color: Colors.white)),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 15.0, top: 3),
+                        child: Text("Groupe 02", style: TextStyle(color: Colors.white)),
+                      ),
+                    ]
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: <Widget> [
+                      Row(
+                        children: [
+                          Container(width: 100, height: 100, decoration: const BoxDecoration(color: Colors.green)),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 25.0),
+                            child: Column(
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: const <Widget> [
+                                    Text("85,3/100 (85 %)", style: TextStyle(color: Colors.green)),                                
+                                    Text("Votre note", style: TextStyle(color: Colors.green)),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 15.0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: const <Widget> [
+                                      Text("85,3/100 (85 %)", style: TextStyle(color: Colors.red)),                                  
+                                      Text("Moyenne", style: TextStyle(color: Colors.red)),
+                                    ],
+                                  ),
+                                ),
                               ],
                             ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 15.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: const <Widget> [
-                                  Text("85,3/100 (85 %)", style: TextStyle(color: Colors.red)),                                  
-                                  Text("Moyenne", style: TextStyle(color: Colors.red)),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
+                          )
+                        ],
+                      ),                  
                     ],
-                  ),                  
-                ],
-              ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Divider(color: Colors.black),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget> [
+                      const Text("Sommaire", style: TextStyle(color: AppTheme.etsLightRed)),
+                      getSummary("Médiane", "82,9"),
+                      getSummary("Écart-type", "4,7"),
+                      getSummary("Rang centile", "96"),
+                    ],
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Divider(color: Colors.black),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 15.0),
+                  child: Text("Évaluations", style: TextStyle(color: AppTheme.etsLightRed))
+                ),
+                Column(
+                  children: [
+                    Theme(
+                      data: ThemeData().copyWith(dividerColor: Colors.transparent),
+                      child: getEvaluationTile("Devoir 1", "5 %"),
+                    ),
+                    Theme(
+                      data: ThemeData().copyWith(dividerColor: Colors.transparent),
+                      child: getEvaluationTile("Final", "25 %"),
+                    ),
+                    Theme(
+                      data: ThemeData().copyWith(dividerColor: Colors.transparent),
+                      child: getEvaluationTile("Intra", "50 %"),
+                    ),
+                    Theme(
+                      data: ThemeData().copyWith(dividerColor: Colors.transparent),
+                      child: getEvaluationTile("Devoir 3", "10 %"),
+                    )
+                  ],
+                )
+              ],
             ),
-            const Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Divider(color: Colors.black),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget> [
-                  const Text("Sommaire", style: TextStyle(color: AppTheme.etsLightRed)),
-                  getSummary("Médiane", "82,9"),
-                  getSummary("Écart-type", "4,7"),
-                  getSummary("Rang centile", "96"),
-                ],
-              ),
-            ),
-          ],
-        )
       ),
     );
 
@@ -118,4 +148,43 @@ class _GradesDetailsViewState extends State<GradesDetailsView> {
         ),
       );
     }
+
+  Padding getEvaluationTile(String title, String weight) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: ExpansionTile(
+        leading: Container(width: 100, height: 150, decoration: const BoxDecoration(color: Colors.green)),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(title, style: const TextStyle(fontSize: 15)),
+            Text('Pondération: $weight', style: const TextStyle(fontSize: 15)),
+          ],
+        ),
+        trailing: Transform.rotate(angle: 180 * 3.14 / 360, child: const IconButton(icon: Icon(Icons.arrow_forward_ios, color: AppTheme.etsLightRed, size: 20,), onPressed: null,),),
+        children: <Widget>[
+          evaluationsSummary()
+        ],
+      ),
+    );
+  }
+
+  Widget evaluationsSummary() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 5.0, bottom: 8),
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.91,
+        child: Column(
+          children: [
+            getSummary("Note", "82,9"),
+            getSummary("Moyenne", "4,7"),
+            getSummary("Médianne", "4,2"),
+            getSummary("Écart-type", "0,6"),
+            getSummary("Rang centile", "79"),
+            getSummary("Rang centile", "18 septembre 2020"),
+          ],
+        )
+      ),
+    );
+  }
 }
