bd0dafd6a7b1a8474ee1fe6e319f8cb61b0f36fd
Camille Brulotte <37625944+camillebrulotte@users.noreply.github.com>
2021-03-27 20:16:28 -0400

    Add static grade + average container with no progress bar yet

--- lib/ui/views/grade_details_view.dart
+++ lib/ui/views/grade_details_view.dart
@@ -25,26 +25,68 @@ class _GradesDetailsViewState extends State<GradesDetailsView> {
         appBar: AppBar(
           elevation: 0,
         ),
-        body: Container(
-          constraints: BoxConstraints(minWidth: MediaQuery.of(context).size.width, maxHeight: 75),
-          decoration: const BoxDecoration(color: AppTheme.primary,),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.only(left: 15.0),
-                child: Text("COM110", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 20)),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              constraints: BoxConstraints(minWidth: MediaQuery.of(context).size.width, maxHeight: 75),
+              decoration: const BoxDecoration(color: AppTheme.primary,),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Padding(
+                    padding: EdgeInsets.only(left: 15.0),
+                    child: Text("COM110", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 20)),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 15.0, top: 3),
+                    child: Text("Méthodes de communication", style: TextStyle(color: Colors.white)),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 15.0, top: 3),
+                    child: Text("Groupe 02", style: TextStyle(color: Colors.white)),
+                  ),
+                ]
               ),
-              const Padding(
-                padding: EdgeInsets.only(left: 15.0, top: 3),
-                child: Text("Méthodes de communication", style: TextStyle(color: Colors.white)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: <Widget> [
+                  Row(
+                    children: [
+                      Container(width: 100, height: 100, decoration: const BoxDecoration(color: Colors.green)),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 25.0),
+                        child: Column(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text("85,3/100 (85 %)", style: TextStyle(color: Colors.green)),                                
+                                const Text("Votre note", style: TextStyle(color: Colors.green)),
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 15.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text("85,3/100 (85 %)", style: TextStyle(color: Colors.red)),                                  
+                                  const Text("Moyenne", style: TextStyle(color: Colors.red)),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  const Divider(color: Colors.black)
+                ],
               ),
-              const Padding(
-                padding: EdgeInsets.only(left: 15.0, top: 3),
-                child: Text("Groupe 02", style: TextStyle(color: Colors.white)),
-              ),
-            ]
-          ),
+            )
+          ],
         )
       ),
     );
