d757d7ce16f07fd8b4ff51de583a507e65707940
elornHamon <elorn.hamon@gmail.com>
2021-04-02 15:00:57 -0400

    Clean imports

--- lib/core/managers/dashboard_manager.dart
+++ lib/core/managers/dashboard_manager.dart
@@ -1,19 +1,6 @@
 // FLUTTER / DART / THIRD-PARTIES
-import 'package:enum_to_string/enum_to_string.dart';
+
 import 'package:flutter/material.dart';
-import 'package:logger/logger.dart';
-import 'package:table_calendar/table_calendar.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
-// SERVICES
-import 'package:notredame/core/services/analytics_service.dart';
-import 'package:notredame/core/services/preferences_service.dart';
-
-// CONSTANTS
-import 'package:notredame/core/constants/preferences_flags.dart';
-
-// OTHER
-import 'package:notredame/locator.dart';
 
 class DashboardManager with ChangeNotifier {
   bool _showAboutUsCard = true;
