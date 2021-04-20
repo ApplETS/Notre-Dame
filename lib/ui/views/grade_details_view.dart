8319cba04334373ce8d53e14e85e5199819eb1e1
Camille Brulotte <37625944+camillebrulotte@users.noreply.github.com>
2021-04-20 09:02:57 -0400

    Try to fix golden overflows

--- lib/ui/views/grade_details_view.dart
+++ lib/ui/views/grade_details_view.dart
@@ -41,21 +41,24 @@ class _GradesDetailsViewState extends State<GradesDetailsView> {
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: <Widget>[
-                          GradeCircularProgress(
-                            1.0,
-                            key: const Key("GradeCircularProgress_summary"),
-                            finalGrade: model.course.grade,
-                            studentGrade: getGradeInPercentage(
-                              model.course.summary.currentMark,
-                              model.course.summary.markOutOf,
-                            ),
-                            averageGrade: getGradeInPercentage(
-                              model.course.summary.passMark,
-                              model.course.summary.markOutOf,
+                          Expanded(
+                            flex: 6,
+                            child: GradeCircularProgress(
+                              1.0,
+                              key: const Key("GradeCircularProgress_summary"),
+                              finalGrade: model.course.grade,
+                              studentGrade: getGradeInPercentage(
+                                model.course.summary.currentMark,
+                                model.course.summary.markOutOf,
+                              ),
+                              averageGrade: getGradeInPercentage(
+                                model.course.summary.passMark,
+                                model.course.summary.markOutOf,
+                              ),
                             ),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 55.0),
+                          Expanded(
+                            flex: 4,
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
@@ -89,29 +92,38 @@ class _GradesDetailsViewState extends State<GradesDetailsView> {
                   Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: <Widget>[
-                        getHeadersSummary(
-                          AppIntl.of(context).grades_median,
-                          validateGrade(
-                            context,
-                            model.course.summary.median.toString(),
-                            AppIntl.of(context).grades_grade_in_percentage(
-                                model.course.summary.median),
+                        Expanded(
+                          flex: 3,
+                          child: getHeadersSummary(
+                            AppIntl.of(context).grades_median,
+                            validateGrade(
+                              context,
+                              model.course.summary.median.toString(),
+                              AppIntl.of(context).grades_grade_in_percentage(
+                                  model.course.summary.median),
+                            ),
                           ),
                         ),
-                        getHeadersSummary(
-                          AppIntl.of(context).grades_standard_deviation,
-                          validateGrade(
-                            context,
-                            model.course.summary.standardDeviation.toString(),
-                            model.course.summary.standardDeviation.toString(),
+                        Expanded(
+                          flex: 3,
+                          child: getHeadersSummary(
+                            AppIntl.of(context).grades_standard_deviation,
+                            validateGrade(
+                              context,
+                              model.course.summary.standardDeviation.toString(),
+                              model.course.summary.standardDeviation.toString(),
+                            ),
                           ),
                         ),
-                        getHeadersSummary(
-                          AppIntl.of(context).grades_percentile_rank,
-                          validateGrade(
-                            context,
-                            model.course.summary.percentileRank.toString(),
-                            model.course.summary.percentileRank.toString(),
+                        Expanded(
+                          flex: 3,
+                          child: getHeadersSummary(
+                            AppIntl.of(context).grades_percentile_rank,
+                            validateGrade(
+                              context,
+                              model.course.summary.percentileRank.toString(),
+                              model.course.summary.percentileRank.toString(),
+                            ),
                           ),
                         ),
                       ]),
@@ -269,15 +281,21 @@ class _GradesDetailsViewState extends State<GradesDetailsView> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 10.0),
-              child: Text(title ?? "", textAlign: TextAlign.center),
+            Expanded(
+              flex: 5,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 14.0),
+                child: Text(title ?? "", textAlign: TextAlign.center),
+              ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 25.0),
-              child: Text(
-                number,
-                style: const TextStyle(fontSize: 19),
+            Expanded(
+              flex: 5,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 2.0),
+                child: Text(
+                  number,
+                  style: const TextStyle(fontSize: 19),
+                ),
               ),
             ),
           ],
