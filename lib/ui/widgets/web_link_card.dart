0ef14f5808bf731cb6a0c0d6151f63fa743c9a76
Camille Brulotte <37625944+camillebrulotte@users.noreply.github.com>
2021-05-06 17:39:42 -0400

    Surround launchInBrowser in try catch

--- lib/ui/widgets/web_link_card.dart
+++ lib/ui/widgets/web_link_card.dart
@@ -1,11 +1,15 @@
 // FLUTTER / DART / THIRD-PARTIES
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:package_info/package_info.dart';
 
 // CONSTANT
 import 'package:notredame/core/constants/router_paths.dart';
 
 // MODEL
 import 'package:notredame/core/models/quick_link.dart';
+import 'package:notredame/core/services/analytics_service.dart';
 
 // SERVICE
 import 'package:notredame/core/services/navigation_service.dart';
@@ -19,6 +23,8 @@ class WebLinkCard extends StatelessWidget {
 
   /// used to redirect on the security.
   final NavigationService _navigationService = locator<NavigationService>();
+
+  final AnalyticsService _analyticsService = locator<AnalyticsService>();
 
   WebLinkCard(this._links);
 
@@ -56,11 +62,39 @@ class WebLinkCard extends StatelessWidget {
   }
 
   /// used to open a website or the security view
-  void _onLinkClicked(String link, BuildContext context) {
+  Future<void> _onLinkClicked(String link, BuildContext context) async {
     if (link == 'security') {
       _navigationService.pushNamed(RouterPaths.security);
     } else {
-      _navigationService.pushNamed(RouterPaths.webView, arguments: _links);
+      try {
+        await _launchInBrowser(link);
+      } catch(e) {        
+        final PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+        final errorMessage = "**Error message : ${e.toString()}\n**Device Infos** \n"
+                "- **Version:** ${packageInfo.version} \n"
+                "- **Build number:** ${packageInfo.buildNumber} \n"
+                "- **Platform operating system:** ${Platform.operatingSystem} \n"
+                "- **Platform operating system version:** ${Platform.operatingSystemVersion} \n";
+
+        _analyticsService.logError("web_link_card", errorMessage);
+        _navigationService.pushNamed(RouterPaths.webView, arguments: _links);
+      }
     }
+  }
+
+  /// used to open a website inside AndroidChromeCustomTabs or SFSafariViewController
+  Future<void> _launchInBrowser(String url) async {
+    final ChromeSafariBrowser browser = ChromeSafariBrowser();
+    await browser.open(
+        url: Uri.parse(url),
+        options: ChromeSafariBrowserClassOptions(
+            android: AndroidChromeCustomTabsOptions(
+                addDefaultShareMenuItem: false,
+                enableUrlBarHiding: true,
+                toolbarBackgroundColor: Colors.red),
+            ios: IOSSafariOptions(
+                barCollapsingEnabled: true,
+                preferredBarTintColor: Colors.red)));
   }
 }
