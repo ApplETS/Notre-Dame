5b545e9d1fbcbdc391a75b8ba681f1ac5c13df95
Camille Brulotte <37625944+camillebrulotte@users.noreply.github.com>
2021-05-07 09:13:29 -0400

    Refactor to use ViewModelBuilder

--- lib/ui/widgets/web_link_card.dart
+++ lib/ui/widgets/web_link_card.dart
@@ -1,5 +1,6 @@
 // FLUTTER / DART / THIRD-PARTIES
 import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
 
 // VIEWMODEL
 import 'package:notredame/core/viewmodels/web_link_card_viewmodel.dart';
@@ -7,58 +8,49 @@ import 'package:notredame/core/viewmodels/web_link_card_viewmodel.dart';
 // MODEL
 import 'package:notredame/core/models/quick_link.dart';
 
-// OTHER
+// UTILS
 import 'package:notredame/ui/utils/app_theme.dart';
 
-class WebLinkCard extends StatefulWidget {
+class WebLinkCard extends StatelessWidget {
   final QuickLink _links;
 
   const WebLinkCard(this._links);
 
   @override
-  _WebLinkCardState createState() => _WebLinkCardState();
-}
-
-class _WebLinkCardState extends State<WebLinkCard> {
-  WebLinkCardViewModel model;
-
-  @override
-  void initState() {
-    super.initState();
-    model = WebLinkCardViewModel();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width / 3.1,
-      height: 130,
-      child: Card(
-        elevation: 4.0,
-        child: InkWell(
-          onTap: () => model.onLinkClicked(widget._links),
-          splashColor: AppTheme.etsLightRed.withAlpha(50),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Expanded(
-                  flex: 40,
-                  child: Image.asset(widget._links.image, color: AppTheme.etsLightRed),
+  Widget build(BuildContext context) =>
+    ViewModelBuilder<WebLinkCardViewModel>.reactive(
+      viewModelBuilder: () => WebLinkCardViewModel(),
+      builder: (context, model, child) {
+        return SizedBox(
+          width: MediaQuery.of(context).size.width / 3.1,
+          height: 130,
+          child: Card(
+            elevation: 4.0,
+            child: InkWell(
+              onTap: () => model.onLinkClicked(_links),
+              splashColor: AppTheme.etsLightRed.withAlpha(50),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 40,
+                      child: Image.asset(_links.image, color: AppTheme.etsLightRed),
+                    ),
+                    FittedBox(
+                      fit: BoxFit.fitWidth,
+                      child: Text(
+                        _links.name,
+                        style: const TextStyle(color: Colors.red, fontSize: 18.0),
+                      ),
+                    ),
+                  ],
                 ),
-                FittedBox(
-                  fit: BoxFit.fitWidth,
-                  child: Text(
-                    widget._links.name,
-                    style: const TextStyle(color: Colors.red, fontSize: 18.0),
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      }
     );
-  }
 }
  
