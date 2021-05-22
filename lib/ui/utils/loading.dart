4081d2c8c09300b5d86cf51f91ea8cf60016bd4b
alexandremarkus <alexandre.markus.1@ens.etsmtl.ca>
2021-05-22 10:15:59 -0400

    changed colors after upgrade to flutter 2.2.0

--- lib/ui/utils/loading.dart
+++ lib/ui/utils/loading.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:notredame/ui/utils/app_theme.dart';
 
 Widget buildLoading({bool isInteractionLimitedWhileLoading = true}) => Stack(
-  children: [
-    if (isInteractionLimitedWhileLoading)
-      const Opacity(
-        opacity: 0.5,
-        child: ModalBarrier(dismissible: false, color: Colors.grey),
-      ),
-    const Center(child: CircularProgressIndicator())
-  ],
-);
+      children: [
+        if (isInteractionLimitedWhileLoading)
+          const Opacity(
+            opacity: 0.5,
+            child: ModalBarrier(dismissible: false, color: Colors.grey),
+          ),
+        const Center(
+            child: CircularProgressIndicator(
+          color: AppTheme.etsLightRed,
+        ))
+      ],
+    );
