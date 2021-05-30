02d6ba7d329521b68a41ceb2edfb9535efea679c
Xavier Chretien <chretienxavier42@gmail.com>
2021-05-30 13:52:29 -0400

    Apply flutter format.

--- lib/core/services/internal_info_service.dart
+++ lib/core/services/internal_info_service.dart
@@ -13,12 +13,12 @@ class InternalInfoService {
   Future<String> getDeviceInfoForErrorReporting() async {
     final PackageInfo packageInfo = await PackageInfo.fromPlatform();
     final NetworkingService _networkingService = locator<NetworkingService>();
-    
+
     return "**Device Infos** \n"
-            "- **Version:** ${packageInfo.version} \n"
-            "- **Connectivity:** ${await _networkingService.getConnectionType()} \n"
-            "- **Build number:** ${packageInfo.buildNumber} \n"
-            "- **Platform operating system:** ${Platform.operatingSystem} \n"
-            "- **Platform operating system version:** ${Platform.operatingSystemVersion} \n";
+        "- **Version:** ${packageInfo.version} \n"
+        "- **Connectivity:** ${await _networkingService.getConnectionType()} \n"
+        "- **Build number:** ${packageInfo.buildNumber} \n"
+        "- **Platform operating system:** ${Platform.operatingSystem} \n"
+        "- **Platform operating system version:** ${Platform.operatingSystemVersion} \n";
   }
 }
