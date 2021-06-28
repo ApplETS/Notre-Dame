4e09e8ab39a0e6a9428a755497635f921de19e21
Camille Brulotte <camille.brulotte@icloud.com>
2021-06-27 20:01:45 -0400

    Change back to stateless

--- lib/ui/views/profile_view.dart
+++ lib/ui/views/profile_view.dart
@@ -13,12 +13,7 @@ import 'package:notredame/ui/utils/loading.dart';
 // OTHER
 import 'package:notredame/ui/utils/app_theme.dart';
 
-class ProfileView extends StatefulWidget {
-  @override
-  _ProfileViewState createState() => _ProfileViewState();
-}
-
-class _ProfileViewState extends State<ProfileView> {
+class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) =>
       ViewModelBuilder<ProfileViewModel>.reactive(
