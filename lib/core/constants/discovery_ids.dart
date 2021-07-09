812c434e5ec286f804d04a9c7b4c9a97675ef661
Camille Brulotte <camille.brulotte@icloud.com>
2021-07-09 18:35:39 -0400

    [CI UPDATE GOLDENS]

--- lib/core/constants/discovery_ids.dart
+++ lib/core/constants/discovery_ids.dart
@@ -16,6 +16,7 @@ class DiscoveryIds {
   static const String detailsScheduleSettings = "page_schedule_settings";
 
   static const String detailsStudentGradeButton = "page_student_grade_button";
+  static const String detailsStudentGradeDetails = "page_student_grade_details";
   static const String detailsStudentProfile = "page_student_profile";
 
   static const String detailsMoreBugReport = "page_more_bug_report";
