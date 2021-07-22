448148e95bfbef309d57cafbd80271d0951d0bf8
JonathanDuvalV <duvalvenne.jonathan@gmail.com>
2021-07-22 15:25:31 -0400

    Removed unused import

--- lib/ui/widgets/base_scaffold.dart
+++ lib/ui/widgets/base_scaffold.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 // UTILS
-import 'package:notredame/ui/utils/app_theme.dart';
 import 'package:notredame/ui/utils/loading.dart';
 
 // WIDGETS
