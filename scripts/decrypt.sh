0b6ae600240d017101243f7a50d36c50517fd4ef
Xavier Chrétien <chretienxavier42@gmail.com>
2021-08-18 19:26:45 -0400

    Remove google services files (given by Firebase) to improve security. The password to decode them have been added to the club drive.

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -7,3 +7,10 @@ openssl version -v
 # Decrypt Signets API
 mkdir -p ./assets/certificates
 openssl enc -aes-256-cbc -pbkdf2 -d -k "$ENCRYPTED_SIGNETS_API_CERT_PASSWORD" -in ./assets/encryptedFiles/signets_cert.crt.enc -out ./assets/certificates/signets_cert.crt -md md5
+
+# Decrypt Google service files (Android and iOS).
+if [[ -n "$ENCRYPTED_GOOGLE_SERVICE_PASSWORD" ]]; then
+  echo "Decoding google-services files"
+  openssl enc -aes-256-cbc -pbkdf2 -d -k "$ENCRYPTED_GOOGLE_SERVICE_PASSWORD" -in ./assets/encryptedFiles/google-services.json.enc -out ./android/app/google-services.json -md md5
+  openssl enc -aes-256-cbc -pbkdf2 -d -k "$ENCRYPTED_GOOGLE_SERVICE_PASSWORD" -in ./assets/encryptedFiles/GoogleService-Info.plist.enc -out ./ios/Runner/GoogleService-Info.plist -md md5
+fi
