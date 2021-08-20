077b1c94dbf4e26d8cd1e95c07ca7d53502724ce
Xavier Chrétien <chretienxavier42@gmail.com>
2021-08-20 00:33:15 -0400

    Final preparation for android release.

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -14,3 +14,11 @@ if [[ -n "$ENCRYPTED_GOOGLE_SERVICE_PASSWORD" ]]; then
   openssl enc -aes-256-cbc -pbkdf2 -d -k "$ENCRYPTED_GOOGLE_SERVICE_PASSWORD" -in ./encryptedFiles/google-services.json.enc -out ./android/app/google-services.json -md md5
   openssl enc -aes-256-cbc -pbkdf2 -d -k "$ENCRYPTED_GOOGLE_SERVICE_PASSWORD" -in ./encryptedFiles/GoogleService-Info.plist.enc -out ./ios/Runner/GoogleService-Info.plist -md md5
 fi
+
+# Decrypt Android Keystore, JKS and service account credentials
+if [[ -n $ENCRYPTED_ETSMOBILE_KEYSTORE_PASSWORD && -n $ENCRYPTED_KEYSTORE_PROPERTIES_PASSWORD && -n $ENCRYPTED_SERVICE_ACCOUNT_CREDENTIALS_PASSWORD ]]; then
+  echo "Decoding Android keystore, JKS and service account credentials"
+  openssl aes-256-cbc -k "$ENCRYPTED_ETSMOBILE_KEYSTORE_PASSWORD" -in ./encryptedFiles/etsm_upload_ks.jks.enc -out ./android/etsm_upload_ks.jks -d
+  openssl aes-256-cbc -k "$ENCRYPTED_KEYSTORE_PROPERTIES_PASSWORD" -in ./encryptedFiles/keystore.properties.enc -out ./android/keystore.properties -d
+  openssl aes-256-cbc -k "$ENCRYPTED_SERVICE_ACCOUNT_CREDENTIALS_PASSWORD" -in ./encryptedFiles/service_account_credentials.json.encrypted -out ./service_account_credentials.json -d
+fi
