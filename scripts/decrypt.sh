6a8c3e80b703eba8ce7cf6a0cdc003ed817cd8c5
Xavier Chrétien <chretienxavier42@gmail.com>
2021-08-30 11:17:32 -0400

    Add correct Android Keystore and keystore properties

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -4,9 +4,13 @@
 
 pwd
 openssl version -v
+
 # Decrypt Signets API
-mkdir -p ./assets/certificates
-openssl enc -aes-256-cbc -pbkdf2 -d -k "$ENCRYPTED_SIGNETS_API_CERT_PASSWORD" -in ./encryptedFiles/signets_cert.crt.enc -out ./assets/certificates/signets_cert.crt -md md5
+if [[ -n "$ENCRYPTED_GOOGLE_SERVICE_PASSWORD" ]]; then
+  mkdir -p ./assets/certificates
+  echo "Decoding SignETS certificates"
+  openssl enc -aes-256-cbc -pbkdf2 -d -k "$ENCRYPTED_SIGNETS_API_CERT_PASSWORD" -in ./encryptedFiles/signets_cert.crt.enc -out ./assets/certificates/signets_cert.crt -md md5
+fi
 
 # Decrypt Google service files (Android and iOS).
 if [[ -n "$ENCRYPTED_GOOGLE_SERVICE_PASSWORD" ]]; then
@@ -16,9 +20,13 @@ if [[ -n "$ENCRYPTED_GOOGLE_SERVICE_PASSWORD" ]]; then
 fi
 
 # Decrypt Android Keystore, JKS and service account credentials
-if [[ -n $ENCRYPTED_ETSMOBILE_KEYSTORE_PASSWORD && -n $ENCRYPTED_KEYSTORE_PROPERTIES_PASSWORD && -n $ENCRYPTED_SERVICE_ACCOUNT_CREDENTIALS_PASSWORD ]]; then
-  echo "Decoding Android keystore, JKS and service account credentials"
-  openssl aes-256-cbc -k "$ENCRYPTED_ETSMOBILE_KEYSTORE_PASSWORD" -in ./encryptedFiles/etsm_upload_ks.jks.enc -out ./android/etsm_upload_ks.jks -d
-  openssl aes-256-cbc -k "$ENCRYPTED_KEYSTORE_PROPERTIES_PASSWORD" -in ./encryptedFiles/keystore.properties.enc -out ./android/keystore.properties -d
-  openssl aes-256-cbc -k "$ENCRYPTED_SERVICE_ACCOUNT_CREDENTIALS_PASSWORD" -in ./encryptedFiles/service_account_credentials.json.encrypted -out ./service_account_credentials.json -d
+if [[ -n $ENCRYPTED_ETSMOBILE_KEYSTORE_PASSWORD && -n $ENCRYPTED_KEYSTORE_PROPERTIES_PASSWORD ]]; then
+  echo "Decoding Android keystore and JKS"
+  openssl aes-256-cbc -pbkdf2 -d -k "$ENCRYPTED_ETSMOBILE_KEYSTORE_PASSWORD" -in ./encryptedFiles/etsm_ks.jks.enc -out ./android/etsm_ks.jks -md md5
+  openssl aes-256-cbc -pbkdf2 -d -k "$ENCRYPTED_KEYSTORE_PROPERTIES_PASSWORD" -in ./encryptedFiles/keystore.properties.enc -out ./android/keystore.properties -md md5
+fi
+
+if [[ -n $ENCRYPTED_ANDROID_SERVICE_ACCOUNT_CREDENTIALS_PASSWORD ]]; then
+  echo "Decoding Android service account credentials"
+  openssl aes-256-cbc -pbkdf2 -d -k "$ENCRYPTED_ANDROID_SERVICE_ACCOUNT_CREDENTIALS_PASSWORD" -in ./encryptedFiles/service_account_credentials.json.enc -out ./android/service_account_credentials.json -md md5
 fi
