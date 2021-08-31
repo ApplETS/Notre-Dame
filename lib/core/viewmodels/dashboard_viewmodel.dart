2a4b785f6c92da79c5d5fc60d8cbd4c1d0c5d6da
Xavier Chrétien <chretienxavier42@gmail.com>
2021-08-31 17:43:07 -0400

    Add analytics to the GithubApi

--- lib/core/viewmodels/dashboard_viewmodel.dart
+++ lib/core/viewmodels/dashboard_viewmodel.dart
@@ -2,7 +2,6 @@
 import 'dart:collection';
 import 'package:feature_discovery/feature_discovery.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:logger/logger.dart';
 import 'package:stacked/stacked.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
