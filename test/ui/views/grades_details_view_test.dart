0ff683b72d7e9ae1bf743f05147314d9bc378031
Xavier Chrétien <chretienxavier42@gmail.com>
2021-09-29 11:18:28 -0400

    Add setupNetworkingService when missing.

--- test/ui/views/grades_details_view_test.dart
+++ test/ui/views/grades_details_view_test.dart
@@ -77,6 +77,7 @@ void main() {
       setupNavigationServiceMock();
       courseRepository = setupCourseRepositoryMock();
       setupSettingsManagerMock();
+      setupNetworkingServiceMock();
     });
 
     tearDown(() {
