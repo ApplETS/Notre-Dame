5b04aa82b0af757a3dbb74ddeb88f6b79f3eafe6
alexandremarkus <alexandre.markus.1@ens.etsmtl.ca>
2021-10-15 17:36:36 -0400

    added device name to bug report

--- lib/core/services/internal_info_service.dart
+++ lib/core/services/internal_info_service.dart
@@ -1,5 +1,6 @@
 // FLUTTER / DART / THIRD-PARTIES
 import 'dart:io';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 // UTILS
@@ -13,8 +14,19 @@ class InternalInfoService {
   Future<String> getDeviceInfoForErrorReporting() async {
     final PackageInfo packageInfo = await PackageInfo.fromPlatform();
     final NetworkingService _networkingService = locator<NetworkingService>();
+    final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    String deviceName;
+
+    if (Platform.isAndroid) {
+      final AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+      deviceName = androidInfo.model;
+    } else if (Platform.isIOS) {
+      final IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+      deviceName = iosInfo.utsname.machine;
+    }
 
     return "**Device Infos** \n"
+        "- **Device:** $deviceName \n"
         "- **Version:** ${packageInfo.version} \n"
         "- **Connectivity:** ${await _networkingService.getConnectionType()} \n"
         "- **Build number:** ${packageInfo.buildNumber} \n"
