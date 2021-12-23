5c31350640570aa3d6926062708f51fec945906f
Emma Neiss <emma.neiss@insa-lyon.fr>
2021-12-23 18:51:54 -0500

    Update progression widget UI and gitignore for mac

--- ios/ETSMobile Widget/ETSMobile_Widget.swift
+++ ios/ETSMobile Widget/ETSMobile_Widget.swift
@@ -14,18 +14,19 @@ struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), progress: 0.5)
     }
-
+    
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
         let entry: SimpleEntry
         if context.isPreview {
             entry = SimpleEntry(date: Date(), progress: 0.5)
         } else {
-            let data = UserDefaults.init(suiteName:widgetGroupId)
-            entry = SimpleEntry(date: Date(), progress: data?.double(forKey: "progress") ?? 0.5)
+            entry = SimpleEntry(date: Date(), progress: 0.5)
+//            let data = UserDefaults.init(suiteName:widgetGroupId)
+//            entry = SimpleEntry(date: Date(), progress: data?.double(forKey: "progress") ?? 0.5)
         }
         completion(entry)
     }
-
+    
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         getSnapshot(in: context) { (entry) in
             let timeline = Timeline(entries: [entry], policy: .atEnd)
@@ -46,24 +47,28 @@ struct CustomProgressView: View {
         VStack {
             GeometryReader { geometry in
                 let height = 10.0
-//                let height = geometry.size.height
-//                let width = 50.0
                 let width = geometry.size.width
-                        ZStack(alignment: .leading) {
-                            Rectangle()
-                                .foregroundColor(Color.gray)
-                                .frame(width: width,
-                                       height: height)
-                                .cornerRadius(height / 2.0)
-
-                            Rectangle()
-                                .foregroundColor(Color.green)
-                                .frame(width: width * self.progress,
-                                       height: height)
-                                .cornerRadius(height / 2.0)
-                        }
-                    }
-            .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+                
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .foregroundColor(Color.gray)
+                        .frame(width: width,
+                               height: height)
+                        .cornerRadius(height / 2.0)
+                    
+                    Rectangle()
+                        .foregroundColor(Color.green)
+                        .frame(width: width * self.progress,
+                               height: height)
+                        .cornerRadius(height / 2.0)
+                }
+                .position(x: geometry.frame(in: .local).midX, y: geometry.frame(in: .local).midY)
+                .frame(
+                    width: geometry.frame(in: .global).width,
+                    height: geometry.frame(in: .global).height
+                )
+            }
+            .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
         }
     }
 }
@@ -71,13 +76,20 @@ struct CustomProgressView: View {
 
 struct ETSMobile_WidgetEntryView : View {
     var entry: Provider.Entry
-
+    
     var body: some View {
         VStack {
-            Text("Progression de la session")
-            Text("\(entry.progress)")
             
             Spacer()
+            
+            Text("Progression de la session")
+                .multilineTextAlignment(.center)
+                .font(.system(size: 18))
+            
+            Spacer()
+            
+            Text("\(Int(entry.progress * 100))%")
+                .font(.system(size: 22))
             
             CustomProgressView(progress: entry.progress)
         }
@@ -87,7 +99,7 @@ struct ETSMobile_WidgetEntryView : View {
 @main
 struct ETSMobile_Widget: Widget {
     let kind: String = "ETSMobile_Widget"
-
+    
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             ETSMobile_WidgetEntryView(entry: entry)
