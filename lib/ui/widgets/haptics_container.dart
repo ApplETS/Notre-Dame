fc72b400e210f1bcba374b668227a8468967aeb1
Xavier Chrétien <chretienxavier42@gmail.com>
2022-01-17 14:46:57 -0500

    Update lib/ui/widgets/haptics_container.dart

--- lib/ui/widgets/haptics_container.dart
+++ lib/ui/widgets/haptics_container.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 
 /// A wrapper widget to add haptic feedback on an existing widget.
 ///
-/// This is used to add haptic feedback on dashboard cards when reordering them:
+/// For example this is used to add haptic feedback on dashboard cards when reordering them:
 /// when long-pressed, a new HapticsContainer widget holding the actual card
 /// widget is created (through ReorderableListView's proxyDecorator) and haptic
 /// feedback is generated.
