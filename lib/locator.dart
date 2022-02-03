5a9125a93844a29ca71898a3767a2e211589d084
Samuel Montambault <amos.samuel@hotmail.com>
2022-02-03 17:06:54 -0500

    fix analyse

--- lib/locator.dart
+++ lib/locator.dart
@@ -2,7 +2,6 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:logger/logger.dart';
-import 'package:http/http.dart' as http;
 
 // SERVICES
 import 'package:notredame/core/services/navigation_service.dart';
