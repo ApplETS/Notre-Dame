a884ab784567a4880b77cf3d715fb3b9c5b085ad
alexandremarkus <alexandre.markus.1@ens.etsmtl.ca>
2022-03-19 16:31:23 -0400

    added translations

--- lib/core/models/feedback.dart
+++ lib/core/models/feedback.dart
@@ -1,5 +1,3 @@
-import 'package:notredame/core/constants/feedback_types.dart';
-
 /// A data type holding user feedback consisting of a feedback type and a feedback text.
 class CustomFeedback {
   CustomFeedback({
@@ -7,20 +5,20 @@ class CustomFeedback {
     this.feedbackText,
   });
 
-  FeedbackType feedbackType;
+  String feedbackType;
   String feedbackText;
 
   @override
   String toString() {
     return {
-      'feedback_type': feedbackType.toString(),
+      'feedback_type': feedbackType,
       'feedback_text': feedbackText,
     }.toString();
   }
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
-      'feedback_type': feedbackType.toString(),
+      'feedback_type': feedbackType,
       'feedback_text': feedbackText,
     };
   }
