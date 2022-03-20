b8f3070d735785d74eed0611138c9d3fafa7d1a4
alexandremarkus <alexandre.markus.1@ens.etsmtl.ca>
2022-03-20 11:49:34 -0400

    changed button for floating action button

--- lib/ui/views/feedback_view.dart
+++ lib/ui/views/feedback_view.dart
@@ -5,21 +5,18 @@ import 'package:notredame/core/viewmodels/feedback_viewmodel.dart';
 import 'package:stacked/stacked.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-// OTHERS
-import 'package:notredame/ui/widgets/base_scaffold.dart';
-
 class FeedbackView extends StatelessWidget {
   @override
   Widget build(BuildContext context) =>
       ViewModelBuilder<FeedbackViewModel>.reactive(
         viewModelBuilder: () => FeedbackViewModel(intl: AppIntl.of(context)),
         builder: (context, model, child) {
-          return BaseScaffold(
-              appBar: AppBar(
-                title: Text(AppIntl.of(context).more_report_bug),
-              ),
-              showBottomBar: false,
-              body: ListView(children: <Widget>[
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(AppIntl.of(context).more_report_bug),
+            ),
+            body: ListView(
+              children: <Widget>[
                 const SizedBox(height: 10),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
@@ -45,25 +42,25 @@ class FeedbackView extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 8),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: ElevatedButton(
-                    child: Text(AppIntl.of(context).more_report_bug_button),
-                    onPressed: () =>
-                        BetterFeedback.of(context).show((feedback) {
-                      model
-                          .sendFeedback(
-                              feedback.text,
-                              feedback.screenshot,
-                              feedback.extra.entries.first.value
-                                  .toString()
-                                  .split('.')
-                                  .last)
-                          .then((value) => BetterFeedback.of(context).hide());
-                    }),
-                  ),
-                ),
-              ]));
+              ],
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+            floatingActionButton: FloatingActionButton.extended(
+              onPressed: () => BetterFeedback.of(context).show((feedback) {
+                model
+                    .sendFeedback(
+                        feedback.text,
+                        feedback.screenshot,
+                        feedback.extra.entries.first.value
+                            .toString()
+                            .split('.')
+                            .last)
+                    .then((value) => BetterFeedback.of(context).hide());
+              }),
+              label: Text(AppIntl.of(context).more_report_bug_button),
+            ),
+          );
         },
       );
 }
