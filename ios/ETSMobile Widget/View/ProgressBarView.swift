5149ed79c57978c38282ee53fc0ee41627e701e3
Emma Neiss <emma.neiss@insa-lyon.fr>
2022-04-26 14:25:45 -0400

    Improve progress view scaling & readability

--- ios/ETSMobile Widget/View/ProgressBarView.swift
+++ ios/ETSMobile Widget/View/ProgressBarView.swift
@@ -13,30 +13,32 @@ struct ProgressBarView: View {
     var totalDays: Int
     var suffix: String
     
-    let lineWidth = 10.0        // TODO: use metrics.size.height to adapt line width
-    
     var body: some View {
         GeometryReader { metrics in
+            
+            let lineWidth = metrics.size.height * 0.12
+            let innerCircleDiameter = metrics.size.height * 0.75
+            
             ZStack {
-                // background (full) circle
+                // background (full) outer circle
                 Circle()
                     .stroke(lineWidth: lineWidth)
                     .opacity(0.4)
                     .foregroundColor(Color("ProgressBackground"))
                 
-                // progress circle
+                // progress (outer) circle
                 Circle()
                     .trim(from: 0.0, to: progress)
                     .stroke(style: StrokeStyle(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
                     .foregroundColor(Color("ProgressForeground"))
                     .rotationEffect(Angle(degrees: 270.0))      // start at the top
                 
-                // inner circle
+                // inner circle (text background)
                 Circle()
                     .fill(Color("ProgressBackground"))
                     .opacity(0.4)
-                    .frame(width: metrics.size.height * 0.7,
-                           height: metrics.size.height * 0.7)
+                    .frame(width: innerCircleDiameter,
+                           height: innerCircleDiameter)
                 
                 // progress text
                 VStack {
@@ -44,12 +46,12 @@ struct ProgressBarView: View {
                         .lineLimit(1)
                         .minimumScaleFactor(0.5)
                     Text("\(elapsedDays) / \(totalDays) \(suffix)")
-                        .font(.system(size: 9))
+                        .font(.system(size: 10))
                         .lineLimit(1)
                         .minimumScaleFactor(0.5)
                 }
-                .frame(width: metrics.size.height * 0.5,
-                       height: metrics.size.height * 0.6)
+                .frame(width: metrics.size.height * 0.6,
+                       height: metrics.size.height * 0.7)
             }
         }
     }
