aed971d735f3c22828fdde16925933505b834f62
Camille Brulotte <camille.brulotte@icloud.com>
2022-06-09 20:37:26 -0400

    Add tests

--- test/ui/views/login_view_test.dart
+++ test/ui/views/login_view_test.dart
@@ -2,12 +2,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:mockito/mockito.dart';
 
 // MANAGERS / SERVICES
 import 'package:notredame/core/managers/user_repository.dart';
+import 'package:notredame/core/services/analytics_service.dart';
+import 'package:notredame/core/services/launch_url_service.dart';
 import 'package:notredame/core/services/navigation_service.dart';
 import 'package:notredame/core/managers/settings_manager.dart';
 import 'package:notredame/core/services/preferences_service.dart';
+import '../../mock/services/analytics_service_mock.dart';
+import '../../mock/services/launch_url_service_mock.dart';
 
 // VIEWS
 import 'package:notredame/ui/views/login_view.dart';
@@ -18,6 +23,8 @@ import '../../helpers.dart';
 
 void main() {
   AppIntl intl;
+  LaunchUrlServiceMock launchUrlService;
+  AnalyticsServiceMock analyticsService;
 
   group('LoginView - ', () {
     setUp(() async {
@@ -26,6 +33,8 @@ void main() {
       setupNavigationServiceMock();
       setupSettingsManagerMock();
       setupPreferencesServiceMock();
+      launchUrlService = setupLaunchUrlServiceMock() as LaunchUrlServiceMock;
+      analyticsService = setupAnalyticsServiceMock() as AnalyticsServiceMock;
     });
 
     tearDown(() {
@@ -33,6 +42,8 @@ void main() {
       unregister<NavigationService>();
       unregister<SettingsManager>();
       unregister<PreferencesService>();
+      unregister<LaunchUrlService>();
+      unregister<AnalyticsService>();
     });
 
     group('UI - ', () {
@@ -56,6 +67,59 @@ void main() {
             tester.widget(signInButton),
             isA<ElevatedButton>()
                 .having((source) => source.onPressed, 'onPressed', isNull));
+      });
+
+      testWidgets('should open emails',
+          (WidgetTester tester) async {
+        const url = 'mailto:applets@etsmtl.net?subject=Problème ÉTS Mobile';
+        LaunchUrlServiceMock.stubCanLaunchUrl(
+            launchUrlService,
+            url);
+        LaunchUrlServiceMock.stubLaunchUrl(
+            launchUrlService,
+            url);
+
+        await tester.pumpWidget(localizedWidget(child: LoginView()));
+        await tester.pumpAndSettle();
+
+        await tester.tap(
+              find.widgetWithText(ListTile, intl.need_help_contact_us));
+
+        // Rebuild the widget after the state has changed.
+        await tester.pump();
+
+        verify(launchUrlService.canLaunchUrl(url)).called(1);
+        verify(launchUrlService.launchUrl(url)).called(1);
+        verifyNoMoreInteractions(launchUrlService);
+      });
+
+      testWidgets('cannot launch email on this platform',
+          (WidgetTester tester) async {
+        const url = 'mailto:applets@etsmtl.net?subject=Problème ÉTS Mobile';
+        LaunchUrlServiceMock.stubCanLaunchUrl(
+            launchUrlService,
+            url,
+            toReturn: false);
+        LaunchUrlServiceMock.stubLaunchUrl(
+            launchUrlService,
+            url,
+            toReturn: false);
+
+        await tester.pumpWidget(localizedWidget(child: LoginView()));
+        await tester.pumpAndSettle();
+
+        await tester.tap(
+              find.widgetWithText(ListTile, intl.need_help_contact_us));
+
+        // Rebuild the widget after the state has changed.
+        await tester.pump();
+
+        verify(launchUrlService.canLaunchUrl(url)).called(1);
+        verifyNever(launchUrlService.launchUrl(url));
+        verifyNoMoreInteractions(launchUrlService);
+
+        verify(analyticsService.logError(any, any)).called(1);
+        verifyNoMoreInteractions(analyticsService);
       });
     });
   });
