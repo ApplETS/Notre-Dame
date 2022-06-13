0bcd06295f2f40e731b23e84df1bf674728fad84
Hugo Migner <hugo.migner@gmail.com>
2022-06-13 19:21:23 -0400

    Add scrolling title when opening evaluation

--- lib/ui/widgets/grade_evaluation_tile.dart
+++ lib/ui/widgets/grade_evaluation_tile.dart
@@ -2,6 +2,7 @@
 import 'dart:math';
 import 'package:feature_discovery/feature_discovery.dart';
 import 'package:flutter/material.dart';
+import 'package:marquee/marquee.dart';
 import 'package:intl/intl.dart';
 
 // CONSTANTS
@@ -118,18 +119,36 @@ class _GradeEvaluationTileState extends State<GradeEvaluationTile>
               title: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30.0),
-                    child: Text(
-                      widget.evaluation.title,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Utils.getColorByBrightness(
-                            context, Colors.black, Colors.white),
+                  if (!showEvaluationDetails)
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30.0),
+                      child: Text(
+                        widget.evaluation.title,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Utils.getColorByBrightness(
+                              context, Colors.black, Colors.white),
+                        ),
+                      ),
+                    )
+                  else
+                    SizedBox(
+                      height: 49,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 30.0),
+                        child: Marquee(
+                          text: widget.evaluation.title,
+                          velocity: 15,
+                          blankSpace: 200,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Utils.getColorByBrightness(
+                                context, Colors.black, Colors.white),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
                   Padding(
                     padding: const EdgeInsets.only(top: 10.0, bottom: 20.0),
                     child: Text(
