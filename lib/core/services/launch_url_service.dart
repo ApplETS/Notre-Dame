f84447da740d263c7a9e8f2b4432e2a8dae3b73b
Camille Brulotte <37625944+camillebrulotte@users.noreply.github.com>
2022-07-13 23:17:25 -0400

    Feature/need help button login page (#477)

---
+++ lib/core/services/launch_url_service.dart
@@ -0,0 +1,12 @@
+// FLUTTER / DART / THIRD-PARTIES
+import 'package:url_launcher/url_launcher.dart';
+
+class LaunchUrlService {
+  Future<bool> canLaunchUrl(String url) async {
+    return canLaunch(url);
+  }
+
+  Future<bool> launchUrl(String url) async {
+    return launch(url);
+  }
+}
