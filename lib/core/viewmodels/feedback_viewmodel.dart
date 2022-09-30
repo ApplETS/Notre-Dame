d11b57770ebcf088a1af57dab9003c86540140af
anthony-karabian <antranik-anthony.karabian.1@ens.etsmtl.ca>
2022-09-30 18:12:19 -0400

    BugFix #663 Refactor code

--- lib/core/viewmodels/feedback_viewmodel.dart
+++ lib/core/viewmodels/feedback_viewmodel.dart
@@ -1,5 +1,6 @@
 // FLUTTER / DART / THIRD-PARTIES
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:feedback/feedback.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
