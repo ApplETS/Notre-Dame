bb3b5f5b49216ecef7cba6575fb28487f0076d91
JonathanDuvalV <duvalvenne.jonathan@gmail.com>
2023-07-13 20:38:06 -0400

    Refactor for readability

--- lib/ui/views/quick_links_view.dart
+++ lib/ui/views/quick_links_view.dart
@@ -1,6 +1,7 @@
 // FLUTTER / DART / THIRD-PARTIES
 import 'package:flutter/material.dart';
 import 'package:notredame/core/models/quick_link.dart';
+import 'package:notredame/ui/utils/app_theme.dart';
 import 'package:reorderable_grid_view/reorderable_grid_view.dart';
 import 'package:stacked/stacked.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -46,99 +47,116 @@ class _QuickLinksViewState extends State<QuickLinksView>
         viewModelBuilder: () => QuickLinksViewModel(AppIntl.of(context)),
         builder: (context, model, child) => BaseScaffold(
           isLoading: model.isBusy,
-          appBar: AppBar(
-            title: Text(AppIntl.of(context).title_ets),
-            automaticallyImplyLeading: false,
-          ),
-          body: GestureDetector(
-            onTap: () {
-              if (_editMode) {
-                _controller
-                    .reset(); // Reset the animation when _editMode is set to false
-                setState(() {
-                  _editMode = false;
-                });
-              }
-            },
-            child: SafeArea(
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 8.0),
-                  child: ReorderableGridView.count(
-                    mainAxisSpacing: 2.0,
-                    crossAxisSpacing: 2.0,
-                    crossAxisCount: 3,
-                    children: List.generate(
-                      model.quickLinkList.length,
-                      (index) {
-                        return KeyedSubtree(
-                          key: ValueKey(model.quickLinkList[index].id),
-                          child: GestureDetector(
-                            onLongPress: _editMode
-                                ? null
-                                : () {
-                                    _controller.repeat(reverse: true);
-                                    setState(() {
-                                      _editMode = true;
-                                    });
-                                  },
-                            child: AnimatedBuilder(
-                              animation: _animation,
-                              builder: (BuildContext context, Widget child) {
-                                return Transform.rotate(
-                                  angle: _editMode ? _animation.value : 0,
-                                  child: child,
-                                );
-                              },
-                              child: Stack(
-                                children: [
-                                  WebLinkCard(model.quickLinkList[index]),
-                                  if (_editMode)
-                                    Positioned(
-                                      top: 0,
-                                      left: 0,
-                                      child: Container(
-                                        width: 32,
-                                        height: 32,
-                                        decoration: const BoxDecoration(
-                                          color: Colors.red,
-                                          shape: BoxShape.circle,
-                                        ),
-                                        child: IconButton(
-                                          padding: EdgeInsets.zero,
-                                          icon: const Icon(Icons.close,
-                                              color: Colors.white, size: 16),
-                                          onPressed: () {
-                                            setState(() {
-                                              model.quickLinkList
-                                                  .removeAt(index);
-                                            });
-                                          },
-                                        ),
-                                      ),
-                                    ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                    onReorder: (oldIndex, newIndex) {
-                      setState(() {
-                        final QuickLink item =
-                            model.quickLinkList.removeAt(oldIndex);
-                        model.quickLinkList.insert(newIndex, item);
-                      });
-                    },
-                  ),
-                ),
-              ),
-            ),
-          ),
+          appBar: _buildAppBar(context),
+          body: _buildBody(context, model),
         ),
       );
+
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      title: Text(AppIntl.of(context).title_ets),
+      automaticallyImplyLeading: false,
+    );
+  }
+
+  Widget _buildBody(BuildContext context, QuickLinksViewModel model) {
+    return GestureDetector(
+      onTap: () {
+        if (_editMode) {
+          _controller.reset();
+          setState(() {
+            _editMode = false;
+          });
+        }
+      },
+      child: SafeArea(
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: _buildReorderableGridView(model),
+          ),
+        ),
+      ),
+    );
+  }
+
+  ReorderableGridView _buildReorderableGridView(QuickLinksViewModel model) {
+    return ReorderableGridView.count(
+      mainAxisSpacing: 2.0,
+      crossAxisSpacing: 2.0,
+      crossAxisCount: 3,
+      children: List.generate(
+        model.quickLinkList.length,
+        (index) {
+          return KeyedSubtree(
+            key: ValueKey(model.quickLinkList[index].id),
+            child: _buildGridChild(context, model, index),
+          );
+        },
+      ),
+      onReorder: (oldIndex, newIndex) {
+        setState(() {
+          final QuickLink item = model.quickLinkList.removeAt(oldIndex);
+          model.quickLinkList.insert(newIndex, item);
+        });
+      },
+    );
+  }
+
+  Widget _buildGridChild(
+      BuildContext context, QuickLinksViewModel model, int index) {
+    return GestureDetector(
+      onLongPress: _editMode
+          ? null
+          : () {
+              _controller.repeat(reverse: true);
+              setState(() {
+                _editMode = true;
+              });
+            },
+      child: AnimatedBuilder(
+        animation: _animation,
+        builder: (BuildContext context, Widget child) {
+          return Transform.rotate(
+            angle: _editMode ? _animation.value : 0,
+            child: child,
+          );
+        },
+        child: Stack(
+          children: [
+            WebLinkCard(model.quickLinkList[index]),
+            if (_editMode)
+              Positioned(
+                top: 0,
+                left: 0,
+                child: _buildDeleteButton(model, index),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _buildDeleteButton(QuickLinksViewModel model, int index) {
+    return Container(
+      width: 32,
+      height: 32,
+      decoration: const BoxDecoration(
+        color: AppTheme.etsDarkGrey,
+        shape: BoxShape.circle,
+      ),
+      child: IconButton(
+        padding: EdgeInsets.zero,
+        icon: const Icon(Icons.close, color: Colors.white, size: 16),
+        onPressed: () {
+          setState(() {
+            model.quickLinkList.removeAt(index);
+          });
+        },
+      ),
+    );
+  }
 
   @override
   void dispose() {
