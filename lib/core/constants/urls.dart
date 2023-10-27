5f20dad625aa4e47075f48a090473f1e5cd7061a
AmineMihoubi <64211553+AmineMihoubi@users.noreply.github.com>
2023-10-27 15:54:48 -0400

    update the email

--- lib/core/constants/urls.dart
+++ lib/core/constants/urls.dart
@@ -6,7 +6,7 @@ class Urls {
   static const String clubFacebook = "https://facebook.com/ClubApplETS";
   static const String clubInstagram = "https://www.instagram.com/clubapplets";
   static const String clubTwitter = "https://twitter.com/ClubApplETS";
-  static const String clubEmail = "mailto:info@clubapplets.ca";
+  static const String clubEmail = "mailto:ApplETS@etsmtl.ca";
   static const String clubYoutube =
       "https://youtube.com/channel/UCiSzzfW1bVbE_0KcEZO52ew";
   static const String clubDiscord = "https://discord.gg/adMkWptn6Y";
