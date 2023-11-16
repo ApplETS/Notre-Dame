ed66b9f83af8e492e78ad6f5b22dfc5f49bc1a68
clubapplets-server <clubapplets-server@users.noreply.github.com>
2023-11-16 15:23:28 +0000

    [BOT] Applying format.

--- lib/ui/views/quick_links_view.dart
+++ lib/ui/views/quick_links_view.dart
@@ -107,7 +107,8 @@ class _QuickLinksViewState extends State<QuickLinksView>
     return ReorderableGridView.count(
       mainAxisSpacing: 2.0,
       crossAxisSpacing: 2.0,
-      crossAxisCount: (MediaQuery.of(context).orientation == Orientation.portrait) ? 3 : 6,
+      crossAxisCount:
+          (MediaQuery.of(context).orientation == Orientation.portrait) ? 3 : 6,
       children: List.generate(
         quickLinks.length,
         (index) {
