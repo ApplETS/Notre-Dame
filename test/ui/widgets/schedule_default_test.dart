7759c95725072cdb9b8f6dd83132338c4e224791
Camille Brulotte <camille.brulotte@icloud.com>
2023-12-29 12:25:44 -0500

    Add tests

---
+++ test/ui/widgets/schedule_default_test.dart
@@ -0,0 +1,24 @@
+// Package imports:
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+// Project imports:
+import '../../helpers.dart';
+import 'package:notredame/ui/widgets/schedule_default.dart';
+
+void main() {
+  AppIntl intl;
+
+  group('ScheduleDefault Tests', () {
+    setUpAll(() async {
+      intl = await setupAppIntl();
+    });
+    
+    testWidgets('Displays no schedule message when there are no events', (WidgetTester tester) async {
+      await tester.pumpWidget(localizedWidget(
+            child: const ScheduleDefault(calendarEvents: [])));
+      await tester.pumpAndSettle();
+      expect(find.text(intl.no_schedule_available), findsOneWidget);
+    });
+  });
+}
