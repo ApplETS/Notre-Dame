754bba3819d9c9c3f6f482e19056cc3e08a54fa1
EffBureau <francis.bureau2@gmail.com>
2024-01-04 10:25:40 -0500

    Implement storage_manager

---
+++ lib/core/managers/storage_manager.dart
@@ -0,0 +1,47 @@
+// Dart imports:
+import 'dart:io';
+
+// Package imports:
+import 'package:logger/logger.dart';
+import 'package:path_provider/path_provider.dart';
+
+// Project imports:
+import 'package:notredame/locator.dart';
+
+// Project imports:
+
+class StorageManager {
+  final String tag = "StorageManager";
+
+  final Logger _logger = locator<Logger>();
+
+  Future<String> getAppDocumentsDirectoryPath() async {
+    return Platform.isAndroid
+        ? getExternalStorageDirectory()
+            .then((directory) => directory.path) //FOR ANDROID
+        : getApplicationSupportDirectory()
+            .then((directory) => directory.path); //FOR iOS
+  }
+
+  Future<File> getLocalFile(String filename) async {
+    return File('${await getAppDocumentsDirectoryPath()}/$filename');
+  }
+
+  Future<String> readFile(String filename) async {
+    final file = await getLocalFile(filename);
+
+    final contents = await file.readAsString();
+    _logger.d("$tag - readFile(): $filename - $contents");
+
+    return contents;
+  }
+
+  Future<File> writeToFile(String filename, String contents) async {
+    final file = await getLocalFile(filename);
+
+    final result = await file.writeAsString(contents);
+    _logger.d("$tag - writeToFile: Written to file $filename - ${result.path}");
+
+    return result;
+  }
+}
