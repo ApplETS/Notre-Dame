ab9255aa688e51de2190a175223f92a20340c22e
Xavier Paquet-Rapold <xavier.paquet-rapold.1@ens.etsmtl.ca>
2024-01-12 21:15:38 -0500

    Fix GradeViewModel tests

--- test/viewmodels/grades_viewmodel_test.dart
+++ test/viewmodels/grades_viewmodel_test.dart
@@ -124,6 +124,7 @@ void main() {
         verifyInOrder([
           courseRepositoryMock.getCourses(fromCacheOnly: true),
           courseRepositoryMock.getCourses(),
+          courseRepositoryMock.courses,
           courseRepositoryMock.courses
         ]);
 
@@ -176,8 +177,9 @@ void main() {
         expect(viewModel.coursesBySession, coursesBySession);
         expect(viewModel.sessionOrder, sessionOrder);
 
-        verifyInOrder(
-            [courseRepositoryMock.getCourses(), courseRepositoryMock.courses]);
+        verifyInOrder([courseRepositoryMock.getCourses(),
+              courseRepositoryMock.courses,
+              courseRepositoryMock.courses]);
 
         verifyNoMoreInteractions(courseRepositoryMock);
       });
@@ -213,8 +215,8 @@ void main() {
                 "The list of courses should not change even when an error occurs");
         expect(viewModel.sessionOrder, sessionOrder);
 
-        verifyInOrder(
-            [courseRepositoryMock.getCourses(), courseRepositoryMock.courses]);
+        verifyInOrder([courseRepositoryMock.getCourses(),
+          courseRepositoryMock.courses, courseRepositoryMock.courses]);
 
         verifyNoMoreInteractions(courseRepositoryMock);
       });
