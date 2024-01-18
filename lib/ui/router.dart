56b0767f77505e73c5ffedefaeed9f97f2b61f82
Antoine Martineau <martineau.antoin3@gmail.com>
2024-01-18 09:48:21 -0500

    Update route

--- lib/ui/router.dart
+++ lib/ui/router.dart
@@ -14,6 +14,7 @@ import 'package:notredame/ui/views/feedback_view.dart';
 
 // VIEWS
 import 'package:notredame/ui/views/login_view.dart';
+import 'package:notredame/ui/views/news_view.dart';
 import 'package:notredame/ui/views/not_found_view.dart';
 import 'package:notredame/ui/views/more_view.dart';
 import 'package:notredame/ui/views/outage_view.dart';
@@ -90,10 +91,10 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
       return PageRouteBuilder(
           settings: RouteSettings(name: routeSettings.name),
           pageBuilder: (_, __, ___) => QuickLinksView());
-    case RouterPaths.news: // TODO
+    case RouterPaths.news:
       return PageRouteBuilder(
           settings: RouteSettings(name: routeSettings.name),
-          pageBuilder: (_, __, ___) => QuickLinksView());
+          pageBuilder: (_, __, ___) => NewsView());
     case RouterPaths.webView:
       return PageRouteBuilder(
           pageBuilder: (_, __, ___) =>
