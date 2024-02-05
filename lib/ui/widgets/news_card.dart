a9569cbd18b998552c6c5a00fe95a1103c9c354b
Antoine Martineau <martineau.antoin3@gmail.com>
2024-02-05 09:32:01 -0500

    WIP

--- lib/ui/widgets/news_card.dart
+++ lib/ui/widgets/news_card.dart
@@ -1,15 +1,20 @@
-// FLUTTER / DART / THIRD-PARTIES
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:timeago/timeago.dart' as timeago;
-
-// MODEL
 import 'package:notredame/core/models/news.dart';
 
-class NewsCard extends StatelessWidget {
+class NewsCard extends StatefulWidget {
   final News news;
 
-  const NewsCard(this.news);
+  const NewsCard(this.news, {Key? key}) : super(key: key);
+
+  @override
+  _NewsCardState createState() => _NewsCardState();
+}
+
+class _NewsCardState extends State<NewsCard> {
+  bool _isImageLoaded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -26,9 +31,9 @@ class NewsCard extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                _buildImage(news),
+                _buildImage(widget.news),
                 const SizedBox(height: 8),
-                _buildTitleAndTime(news, context),
+                _shimmerTextEffect(context),
               ],
             ),
           ),
@@ -40,18 +45,59 @@ class NewsCard extends StatelessWidget {
   Widget _buildImage(News news) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(16.0),
-      child: Image.network(
-        news.image,
-        fit: BoxFit.cover,
+      child: _shimmerEffect(),
+    );
+  }
+
+  Widget _shimmerEffect() {
+    return Shimmer.fromColors(
+      baseColor: Colors.grey.shade300,
+      highlightColor: Colors.grey.shade100,
+      enabled: true,
+      child: Container(
+        height: 200, // Specify a fixed height for the shimmer effect
+        color: Colors.grey,
+      ),
+    );
+  }
+
+  Widget _shimmerTextEffect(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: Colors.grey.shade300,
+      highlightColor: Colors.grey.shade100,
+      enabled: true,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            height: 20.0,
+            color: Colors.white,
+          ),
+          const SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 100,
+                height: 20.0,
+                color: Colors.white,
+              ),
+              Container(
+                width: 60,
+                height: 20.0,
+                color: Colors.white,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 
   Widget _buildTitleAndTime(News news, BuildContext context) {
     final TextStyle textStyle =
-        Theme.of(context).textTheme.titleMedium!.copyWith(
-              fontSize: 16,
-            );
+        Theme.of(context).textTheme.titleMedium!.copyWith(fontSize: 16);
 
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -70,5 +116,31 @@ class NewsCard extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _preloadImage();
+  }
+
+  void _preloadImage() {
+    Image.network(widget.news.image)
+        .image
+        .resolve(ImageConfiguration())
+        .addListener(
+          ImageStreamListener(
+            (ImageInfo image, bool synchronousCall) {
+              if (mounted) {
+                setState(() {
+                  _isImageLoaded = true;
+                });
+              }
+            },
+            onError: (exception, stackTrace) {
+              // Handle image load error
+            },
+          ),
+        );
   }
 }
