3bc043f2c757b3c433d7427aecb25d8784f50406
Antoine Martineau <martineau.antoin3@gmail.com>
2024-02-25 18:07:14 -0500

    Authorskeleton test

--- lib/ui/widgets/social_links_card.dart
+++ lib/ui/widgets/social_links_card.dart
@@ -3,14 +3,11 @@ import 'package:flutter/material.dart';
 
 // Package imports:
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:stacked/stacked.dart';
 
 // Project imports:
-import 'package:notredame/core/constants/report_news.dart';
 import 'package:notredame/core/models/socialLink.dart';
 import 'package:notredame/core/utils/utils.dart';
-import 'package:notredame/core/viewmodels/quick_links_viewmodel.dart';
 import 'package:notredame/core/viewmodels/web_link_card_viewmodel.dart';
 import 'package:notredame/ui/utils/app_theme.dart';
 
---
+++ test/ui/widgets/author_info_skeleton_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:notredame/ui/widgets/author_info_skeleton.dart';
+import 'package:shimmer/shimmer.dart';
+import '../../helpers.dart';
+
+void main() {
+  group('AuthorInfoSkeleton Tests', () {
+    setUpAll(() async {
+      await setupAppIntl();
+    });
+
+    testWidgets('Displays an author info skeleton',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(localizedWidget(child: AuthorInfoSkeleton()));
+
+      // Verify that the widget contains shimmer effects for text and icon button
+      expect(find.byType(Shimmer), findsWidgets);
+    });
+  });
+}
