e1beca57227e1a78d13544978b88be3f0de1921e
DylanApardian <dylan.apardian.1@ens.etsmtl.ca>
2024-03-02 18:09:46 -0500

    Added click event on large semester progress widget

--- android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/ProgressWidgetProvider.kt
+++ android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/ProgressWidgetProvider.kt
@@ -1,20 +1,23 @@
 package ca.etsmtl.applets.etsmobile
 
-import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
-import android.content.Intent
-import android.util.Log
-import android.widget.RemoteViews
-import es.antonborri.home_widget.HomeWidgetPlugin
+import android.os.Build
 import android.util.SizeF
-import kotlinx.android.synthetic.main.widget_semester_progress_small.*
+import android.widget.RemoteViews
+import androidx.annotation.RequiresApi
+import android.R.layout.*
+import android.content.Intent
+import android.app.PendingIntent
+import es.antonborri.home_widget.HomeWidgetPlugin
+import android.content.ComponentName
+import android.os.Bundle
 
 class ProgressWidgetProvider : AppWidgetProvider() {
-    private val ACTION_WIDGET_CLICK = "ca.etsmtl.applets.etsmobile.WIDGET_CLICK"
-
     companion object {
+        const val WIDGET_BUTTON_CLICK = "ca.etsmtl.applets.etsmobile.WIDGET_BUTTON_CLICK"
+
         private const val PREFIX_KEY = "progress_"
         const val PROGRESS_KEY = PREFIX_KEY + "progressInt"
         const val ELAPSED_DAYS_KEY = PREFIX_KEY + "elapsedDays"
@@ -27,8 +30,12 @@ class ProgressWidgetProvider : AppWidgetProvider() {
         var totalDays = 0
         var suffix = "days"
         var title = "Semester Progress"
+
+        var progresses = arrayOf<String>()
+        var currentProgressIndex = 1
     }
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         getProgressInfo(context)
         for (appWidgetId in appWidgetIds) {
@@ -36,30 +43,79 @@ class ProgressWidgetProvider : AppWidgetProvider() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+        if (WIDGET_BUTTON_CLICK == intent.action) {
+            // Go to the next progress text
+            currentProgressIndex = (currentProgressIndex + 1) % progresses.size
+            updateAllAppWidgets(context)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onAppWidgetOptionsChanged(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int,
+        newOptions: Bundle
+    ) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions)
+        updateAllAppWidgets(context)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun updateAllAppWidgets(context: Context) {
+        val appWidgetManager = AppWidgetManager.getInstance(context)
+        val appWidgetIds = appWidgetManager.getAppWidgetIds(ComponentName(context, ProgressWidgetProvider::class.java))
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }    
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     private fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+        // Handle clicks on the widget
+        val intent = Intent(context, ProgressWidgetProvider::class.java)
+        intent.action = WIDGET_BUTTON_CLICK
+        val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
         val semesterProgressLarge = RemoteViews(context.packageName, R.layout.widget_semester_progress_large)
         val semesterProgressSmall = RemoteViews(context.packageName, R.layout.widget_semester_progress_small)
 
-        val viewMapping: Map<SizeF, RemoteViews> = mapOf(
-            SizeF(320f, 40f) to semesterProgressLarge, // 5x1
-            SizeF(250f, 40f) to semesterProgressLarge, // 4x1
-            SizeF(110f, 40f) to semesterProgressSmall // 2x1
-        )
+        val widgetSize = getWidgetSize(appWidgetManager, appWidgetId)
+        val views = if (widgetSize.width <= 140) {
+            semesterProgressSmall
+        } else {
+            semesterProgressLarge
+        }
 
-        semesterProgressSmall.setTextViewText(R.id.progress_text, "$progress %" ?: "0")
-        semesterProgressSmall.setTextViewText(R.id.elapsed_days_text, getElapsedDaysOverTotal() ?: "0")
-        semesterProgressSmall.setProgressBar(R.id.progressBar, 100, progress, false)
+        views.apply {
+            // Set the progression for both small and large widget sizes
+            setProgressBar(R.id.progressBar, 100, progress, false)
 
-        semesterProgressLarge.setTextViewText(R.id.semester_progress_title, title)
-        semesterProgressLarge.setProgressBar(R.id.progressBar, 100, progress, false)
-        semesterProgressLarge.setTextViewText(R.id.progressText, "$progress %" ?: "0")
-        // views.setTextViewText(R.id.progressText, getRemainingDays() ?: "0")
+            // Set the progress percentage text for the small widget
+            setTextViewText(R.id.progress_text, "$progress %")
+            setTextViewText(R.id.elapsed_days_text, getElapsedDaysOverTotal(false))
+            
+            // Set the title and progress text for the large widget size
+            setTextViewText(R.id.semester_progress_title, title)
+            setTextViewText(R.id.progressText, progresses[currentProgressIndex])
 
-        val views = RemoteViews(viewMapping)
-
+            setOnClickPendingIntent(R.id.semester_progress_widget, pendingIntent)
+        }
+        
         views.let {
             appWidgetManager.updateAppWidget(appWidgetId, views)
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun getWidgetSize(appWidgetManager: AppWidgetManager, appWidgetId: Int): SizeF {
+        val options = appWidgetManager.getAppWidgetOptions(appWidgetId)
+        val minWidth = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH)
+        val minHeight = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT)
+        return SizeF(minWidth.toFloat(), minHeight.toFloat())
     }
 
     private fun getProgressInfo(context: Context){
@@ -70,14 +126,22 @@ class ProgressWidgetProvider : AppWidgetProvider() {
         elapsedDays = widgetData.getInt(ELAPSED_DAYS_KEY, 0)
         totalDays = widgetData.getInt(TOTAL_DAYS_KEY, 0)
         suffix = widgetData.getString(SUFFIX_KEY, "days") ?: "days"
+
+        progresses = arrayOf("$progress %", getElapsedDaysOverTotal(true), getRemainingDays())
     }
 
     private fun getRemainingDays(): String {
         val remainingDays = totalDays - elapsedDays
-        return "$remainingDays $suffix"
+        val remainingText = if (suffix == "jours") "restant" else "remaining"
+        return "$remainingDays $suffix $remainingText"
     }
 
-    private fun getElapsedDaysOverTotal(): String {
-        return "$elapsedDays / $totalDays"
+    private fun getElapsedDaysOverTotal(addSuffix: Boolean): String {
+        if (!addSuffix) {
+            return "$elapsedDays / $totalDays"
+        }
+
+        val elapsedText = if (suffix == "jours") "écoulés" else "elapsed"
+        return "$elapsedDays $elapsedText / $totalDays $suffix"
     }
 }
