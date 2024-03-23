320b2158e1d4ff4bdd72ef263ae736e983604853
DylanApardian <dylan.apardian.1@ens.etsmtl.ca>
2024-03-22 21:47:16 -0400

    Adapted changes since the null safety changes

--- lib/ui/widgets/grade_evaluation_tile.dart
+++ lib/ui/widgets/grade_evaluation_tile.dart
@@ -233,6 +233,18 @@ class _GradeEvaluationTileState extends State<GradeEvaluationTile>
               ),
             ),
             _buildSummary(
+              AppIntl.of(context)!.grades_weighted,
+              AppIntl.of(context)!.grades_grade_with_percentage(
+                getWeightedGrade(
+                    evaluation.mark,
+                    evaluation.correctedEvaluationOutOfFormatted,
+                    evaluation.weight),
+                evaluation.weight,
+                Utils.getGradeInPercentage(evaluation.mark,
+                    evaluation.correctedEvaluationOutOfFormatted),
+              ),
+            ),
+            _buildSummary(
                 AppIntl.of(context)!.grades_standard_deviation,
                 validateResult(
                     context, evaluation.standardDeviation.toString())),
@@ -253,6 +265,14 @@ class _GradeEvaluationTileState extends State<GradeEvaluationTile>
     }
 
     return AppIntl.of(context)!.grades_not_available;
+  }
+
+  double getWeightedGrade(double? grade, double outOf, double weight) {
+    if (grade == 0.0 || grade == null || outOf == 0.0 || weight == 0.0) {
+      return 0.0;
+    }
+
+    return double.parse(((grade / outOf) * weight).toStringAsFixed(2));
   }
 
   Padding _buildSummary(String title, String grade) {
