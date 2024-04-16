a6a07a19ecc79209e490dc8c64cde7093e831f80
Xavier Paquet-Rapold <80051842+XavierPaquet-Rapold@users.noreply.github.com>
2024-04-16 09:28:55 -0400

    Fix dark theme text color (#969)
    * Put Colors.white in darkTheme textStyle
    
    * [BOT] Applying version.
    
    * Format
    
    ---------
    
    Co-authored-by: XavierPaquet-Rapold <XavierPaquet-Rapold@users.noreply.github.com>

--- lib/ui/utils/app_theme.dart
+++ lib/ui/utils/app_theme.dart
@@ -104,15 +104,19 @@ class AppTheme {
           titleSmall: const TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.w400,
+            color: Colors.white,
           ),
           bodySmall: const TextStyle(
             fontSize: 14,
+            color: Colors.white,
           ),
           bodyMedium: const TextStyle(
             fontSize: 16,
+            color: Colors.white,
           ),
           bodyLarge: const TextStyle(
             fontSize: 18,
+            color: Colors.white,
           ),
         ),
         bottomNavigationBarTheme: darkTheme.bottomNavigationBarTheme
