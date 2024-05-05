acf620f0e00f96999192651f0784f8dfe09791db
Matei Martin <mat4school@gmail.com>
2024-05-05 18:23:22 -0400

    Feature/export to native calendar (#929)
    * Ajout code programe baccalaureat informatique distribuée
    
    * define program by the number of registered credits
    
    * Sort list entries to have main program first.
    
    * Add native requirements for calendar interactions
    
    * Version that used to work
    
    * test custom plugin
    
    * update android files to fix build
    
    * update native calendar even creation to fix creation bug
    
    * comments
    
    * comments
    
    * Add calendar selection widget and update export method signature to specify selected calendar
    
    * Add calendar translations
    
    * fix translations + existing event update.
    
    * Add calendar export feedback + icon updated
    
    * [BOT] Applying version.
    
    * remove unused imports
    
    * [BOT] Update golden files
    
    * [BOT] Applying pod update.
    
    * [BOT] Applying version.
    
    * fix null safety problems
    
    * fix build fail in pipeline
    
    * remove magic numbers for event export to calendar
    
    * [BOT] Applying version.
    
    * [BOT] Applying version.
    
    * fix pub.lock
    
    * fix flutter analysis problem
    
    * [BOT] Update golden files
    
    * [BOT] Update golden files
    
    ---------
    
    Co-authored-by: mataai <mataai@users.noreply.github.com>
    Co-authored-by: clubapplets-server <clubapplets-server@users.noreply.github.com>
    Co-authored-by: mateimartin21 <mateimartin21@gmail.com>

---
+++ lib/ui/widgets/calendar_selector.dart
@@ -0,0 +1,126 @@
+import 'dart:collection';
+
+import 'package:device_calendar/device_calendar.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'package:notredame/ui/utils/app_theme.dart';
+import 'package:notredame/core/managers/course_repository.dart';
+import 'package:notredame/core/utils/calendar_utils.dart';
+import 'package:notredame/locator.dart';
+
+class CalendarSelectionWidget extends StatelessWidget {
+  final AppIntl translations;
+  const CalendarSelectionWidget({super.key, required this.translations});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: CalendarUtils.nativeCalendars,
+      builder:
+          (context, AsyncSnapshot<UnmodifiableListView<Calendar>> calendars) {
+        if (!calendars.hasData) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final items = calendars.data!
+            .map<DropdownMenuItem<String>>(
+              (Calendar value) => DropdownMenuItem<String>(
+                value: value.name,
+                child: Text(value.name!),
+              ),
+            )
+            .toList();
+        items.add(
+          DropdownMenuItem<String>(
+            value: "new",
+            child: Text(translations.calendar_new),
+          ),
+        );
+        String selectedCalendarId = items[0].value ?? '';
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: Text(translations.calendar_export),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(translations.calendar_export_question),
+                  DropdownButton<String>(
+                    items: items,
+                    value: selectedCalendarId,
+                    onChanged: (calendar) {
+                      setState(() {
+                        selectedCalendarId = calendar!;
+                      });
+                    },
+                  ),
+                  Builder(
+                    builder: (context) {
+                      return selectedCalendarId == "new"
+                          ? TextField(
+                              onChanged: (value) {
+                                selectedCalendarId = value;
+                              },
+                              decoration: InputDecoration(
+                                labelText: translations.calendar_name,
+                              ),
+                            )
+                          : const SizedBox(height: 10);
+                    },
+                  ),
+                ],
+              ),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: const Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    if (selectedCalendarId.isEmpty) {
+                      Fluttertoast.showToast(
+                        msg: translations.calendar_select,
+                        backgroundColor: AppTheme.etsLightRed,
+                        textColor: AppTheme.etsBlack,
+                      );
+                      return;
+                    }
+                    Navigator.of(context).pop();
+
+                    final CourseRepository courseRepository =
+                        locator<CourseRepository>();
+
+                    final result = CalendarUtils.export(
+                      courseRepository.coursesActivities!,
+                      selectedCalendarId,
+                    );
+
+                    result.then((value) {
+                      if (value) {
+                        Fluttertoast.showToast(
+                          msg: translations.calendar_export_success,
+                          backgroundColor: AppTheme.gradeGoodMax,
+                          textColor: AppTheme.etsBlack,
+                        );
+                      } else {
+                        Fluttertoast.showToast(
+                          msg: translations.calendar_export_error,
+                          backgroundColor: AppTheme.etsLightRed,
+                          textColor: AppTheme.etsBlack,
+                        );
+                      }
+                    });
+                  },
+                  child: const Text('Export'),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+}
