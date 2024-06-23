7fcba5715666fde233a52f3362f5464447d6002e
Xavier Paquet-Rapold <xavier.paquet-rapold.1@ens.etsmtl.ca>
2024-06-22 22:24:58 -0400

    Import MonETS api client

---
+++ lib/features/app/monets_api/monets_api_client.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:http/io_client.dart';
+
+import 'package:notredame/features/app/monets_api/models/mon_ets_user.dart';
+import 'package:notredame/constants/urls.dart';
+import 'package:notredame/utils/http_exception.dart';
+
+/// A Wrapper for all calls to MonETS API.
+class MonETSAPIClient {
+  static const String tag = "MonETSApi";
+  static const String tagError = "$tag - Error";
+
+  final http.Client _httpClient;
+
+  MonETSAPIClient({http.Client? client})
+      : _httpClient = client ?? IOClient(HttpClient());
+
+  /// Authenticate the basic MonETS user
+  ///
+  /// Throws an [HttpException] if the MonETSApi return anything
+  /// else than a 200 code
+  Future<MonETSUser> authenticate(
+      {required String username, required String password}) async {
+    final response = await _httpClient.post(
+        Uri.parse(Urls.authenticationMonETS),
+        body: {"Username": username, "Password": password});
+
+    // Log the http error and throw a exception
+    if (response.statusCode != 200) {
+      throw HttpException(
+          message: response.body, prefix: tagError, code: response.statusCode);
+    }
+    return MonETSUser.fromJson(
+        jsonDecode(response.body) as Map<String, dynamic>);
+  }
+}
