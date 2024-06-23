62189c62ef4b407c0ba7e121173fc19d55e4e932
Xavier Paquet-Rapold <xavier.paquet-rapold.1@ens.etsmtl.ca>
2024-06-22 22:24:58 -0400

    Import MonETS api client

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -10,4 +10,27 @@ class Urls {
   static const String clubYoutube =
       "https://youtube.com/channel/UCiSzzfW1bVbE_0KcEZO52ew";
   static const String clubDiscord = "https://discord.gg/adMkWptn6Y";
+  
+  // Urls related to MonETS
+  static const String monEtsAPI = "https://portail.etsmtl.ca/api/";
+  static const String authenticationMonETS = "${monEtsAPI}authentification";
+
+  /// Urls related to SignetsMobile API
+  /// For more information about the operations supported see:
+  /// https://signets-ens.etsmtl.ca/Secure/WebServices/SignetsMobile.asmx
+  static const String signetsAPI =
+      "https://signets-ens.etsmtl.ca/Secure/WebServices/SignetsMobile.asmx";
+
+  // SOAP Operations supported by the Signets API
+  static const String signetsOperationBase = "http://etsmtl.ca/";
+  static const String donneesAuthentificationValides =
+      "donneesAuthentificationValides";
+  static const String infoStudentOperation = "infoEtudiant";
+  static const String listProgramsOperation = "listeProgrammes";
+  static const String listClassScheduleOperation = "lireHoraireDesSeances";
+  static const String listSessionsOperation = "listeSessions";
+  static const String listCourseOperation = "listeCours";
+  static const String listEvaluationsOperation = "listeElementsEvaluation";
+  static const String listeHoraireEtProf = "listeHoraireEtProf";
+  static const String readCourseReviewOperation = "lireEvaluationCours";
 }
