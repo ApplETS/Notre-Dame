f90f55dc41cf3607c90f66386bd4f97dcedae4c6
charlcl180 <93603028+charlcl180@users.noreply.github.com>
2024-06-28 20:43:27 -0400

    implementation of seperate widgets for about_view

--- lib/features/more/about/about_view.dart
+++ lib/features/more/about/about_view.dart
@@ -2,13 +2,10 @@
 import 'package:flutter/material.dart';
 
 // Package imports:
-import 'package:easter_egg_trigger/easter_egg_trigger.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notredame/features/more/about/widget/easter_egg_icon.dart'; // Importez le widget extrait
+import 'package:notredame/features/more/about/widget/social_icons_row.dart'; // Importez le widget extrait
 
-// Project imports:
-import 'package:notredame/constants/urls.dart';
-import 'package:notredame/utils/utils.dart';
 
 class AboutView extends StatefulWidget {
   @override
@@ -49,139 +46,63 @@ class _AboutViewState extends State<AboutView> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
+    return newMethod(context);
+  }
+
+  Scaffold newMethod(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        foregroundColor: Colors.white,
-        title: Text(
-          AppIntl.of(context)!.more_about_applets_title,
-        ),
-        elevation: 0,
-        backgroundColor: Colors.transparent,
+    extendBodyBehindAppBar: true,
+    appBar: AppBar(
+      foregroundColor: Colors.white,
+      title: Text(
+        AppIntl.of(context)!.more_about_applets_title,
       ),
-      body: Stack(children: [
-        Positioned(
-          top: -(MediaQuery.of(context).size.height / 2) - 100,
-          bottom: 0,
-          right: 0,
-          left: 0,
-          child: OverflowBox(
-            maxHeight: MediaQuery.of(context).size.longestSide * 2,
-            maxWidth: MediaQuery.of(context).size.longestSide * 2,
-            minHeight: 0,
-            minWidth: 0,
-            child: AnimatedContainer(
-              duration: const Duration(seconds: 1),
-              width:
-                  _completed ? MediaQuery.of(context).size.longestSide * 2 : 0,
-              height:
-                  _completed ? MediaQuery.of(context).size.longestSide * 2 : 0,
-              decoration: const BoxDecoration(
-                  color: Color.fromARGB(255, 25, 55, 95),
-                  shape: BoxShape.circle),
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+    ),
+    body: Stack(children: [
+      Positioned(
+        top: -(MediaQuery.of(context).size.height / 2) - 100,
+        bottom: 0,
+        right: 0,
+        left: 0,
+        child: OverflowBox(
+          maxHeight: MediaQuery.of(context).size.longestSide * 2,
+          maxWidth: MediaQuery.of(context).size.longestSide * 2,
+          minHeight: 0,
+          minWidth: 0,
+          child: AnimatedContainer(
+            duration: const Duration(seconds: 1),
+            width: _completed ? MediaQuery.of(context).size.longestSide * 2 : 0,
+            height: _completed ? MediaQuery.of(context).size.longestSide * 2 : 0,
+            decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 25, 55, 95),
+                shape: BoxShape.circle),
+          ),
+        ),
+      ),
+      Positioned(
+        top: 100,
+        right: 0,
+        left: 0,
+        child: Column(
+          children: [
+            EasterEggIcon(
+              toggleTrigger: toggleTrigger, 
+              easterEggTrigger: _easterEggTrigger,
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                AppIntl.of(context)!.more_applets_about_details,
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+            const SocialIconsRow(),
+          ],
         ),
-        Positioned(
-          top: 100,
-          right: 0,
-          left: 0,
-          child: Column(
-            children: [
-              EasterEggTrigger(
-                action: () => toggleTrigger(),
-                codes: const [
-                  EasterEggTriggers.SwipeUp,
-                  EasterEggTriggers.SwipeRight,
-                  EasterEggTriggers.SwipeDown,
-                  EasterEggTriggers.SwipeLeft,
-                  EasterEggTriggers.Tap
-                ],
-                child: SizedBox(
-                  width: 100,
-                  height: 100,
-                  child: Hero(
-                    tag: 'about',
-                    child: Image.asset(
-                      "assets/images/favicon_applets.png",
-                      scale: 2.0,
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  AppIntl.of(context)!.more_applets_about_details,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(
-                        icon: const FaIcon(
-                          FontAwesomeIcons.earthAmericas,
-                          color: Colors.white,
-                        ),
-                        onPressed: () => Utils.launchURL(
-                            Urls.clubWebsite, AppIntl.of(context)!)),
-                    IconButton(
-                        icon: const FaIcon(
-                          FontAwesomeIcons.github,
-                          color: Colors.white,
-                        ),
-                        onPressed: () => Utils.launchURL(
-                            Urls.clubGithub, AppIntl.of(context)!)),
-                    IconButton(
-                        icon: const FaIcon(
-                          FontAwesomeIcons.facebook,
-                          color: Colors.white,
-                        ),
-                        onPressed: () => Utils.launchURL(
-                            Urls.clubFacebook, AppIntl.of(context)!)),
-                    IconButton(
-                        icon: const FaIcon(
-                          FontAwesomeIcons.twitter,
-                          color: Colors.white,
-                        ),
-                        onPressed: () => Utils.launchURL(
-                            Urls.clubTwitter, AppIntl.of(context)!)),
-                    IconButton(
-                        icon: const FaIcon(
-                          FontAwesomeIcons.youtube,
-                          color: Colors.white,
-                        ),
-                        onPressed: () => Utils.launchURL(
-                            Urls.clubYoutube, AppIntl.of(context)!)),
-                    IconButton(
-                        icon: const FaIcon(
-                          FontAwesomeIcons.discord,
-                          color: Colors.white,
-                        ),
-                        onPressed: () => Utils.launchURL(
-                            Urls.clubDiscord, AppIntl.of(context)!)),
-                  ],
-                ),
-              ),
-              if (_easterEggTrigger)
-                SizedBox(
-                  width: 200,
-                  height: 200,
-                  child: Hero(
-                    tag: 'capra',
-                    child: Image.asset(
-                      "assets/images/capra_long.png",
-                      scale: 1.0,
-                    ),
-                  ),
-                ),
-            ],
-          ),
-        ),
-      ]),
-    );
+      ),
+    ]),
+  );
   }
 }
