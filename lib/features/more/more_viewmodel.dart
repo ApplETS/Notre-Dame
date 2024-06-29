b9780d04bcc08c9a344df03a95947cb26ab2f0c3
charlcl180 <93603028+charlcl180@users.noreply.github.com>
2024-06-29 00:30:59 -0400

    change more view model

--- lib/features/more/more_viewmodel.dart
+++ lib/features/more/more_viewmodel.dart
@@ -20,7 +20,7 @@ import 'package:notredame/features/more/feedback/in_app_review_service.dart';
 import 'package:notredame/features/app/integration/launch_url_service.dart';
 import 'package:notredame/features/app/navigation/navigation_service.dart';
 import 'package:notredame/features/app/storage/preferences_service.dart';
-import 'package:notredame/features/app/analytics/analytics_service.dart'; // Ajoutez cette ligne
+import 'package:notredame/features/app/analytics/analytics_service.dart';
 import 'package:notredame/features/app/analytics/remote_config_service.dart';
 import 'package:notredame/utils/locator.dart';
 import 'package:notredame/features/welcome/discovery/discovery_components.dart';
