9889248a3774d351d5ea9849b6d2e43a45f7f84a
charlcl180 <93603028+charlcl180@users.noreply.github.com>
2024-06-28 20:46:13 -0400

    impementation of seperation widget of choose_language_view

---
+++ lib/features/more/settings/widget/language_card_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:notredame/utils/utils.dart';
+
+class LanguageCardWidget extends StatelessWidget {
+  final String language;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  const LanguageCardWidget({
+    super.key,
+    required this.language,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Utils.getColorByBrightness(context, Colors.white, Colors.grey[900]!),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTile(
+            title: Text(language),
+            trailing: Icon(isSelected ? Icons.check : null),
+            onTap: onTap,
+          ),
+        ],
+      ),
+    );
+  }
+}
