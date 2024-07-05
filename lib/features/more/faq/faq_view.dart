77f116185e273ca7be78cc1cfd6492533a08ce96
Louis-Philippe <louis.philippe.heon@gmail.com>
2024-07-05 11:35:15 -0400

    Text is no longer justified

--- lib/features/more/faq/faq_view.dart
+++ lib/features/more/faq/faq_view.dart
@@ -34,41 +34,7 @@ class _FaqViewState extends State<FaqView> {
               children: <Widget>[
                 getTitle(),
                 getSubtitle(AppIntl.of(context)!.questions_and_answers),
-                Padding(
-                  padding: const EdgeInsets.only(left: 15.0, right: 15.0),
-                  child: CarouselSlider(
-                    options: CarouselOptions(
-                      height: 250.0,
-                    ),
-                    items: faq.questions.asMap().entries.map((entry) {
-                      final int index = entry.key;
-                      final question = faq.questions[index];
-
-                      return Builder(
-                        builder: (BuildContext context) {
-                          return Container(
-                            width: MediaQuery.of(context).size.width,
-                            margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                            decoration: BoxDecoration(
-                              color: Theme.of(context).brightness ==
-                                      Brightness.light
-                                  ? const Color.fromARGB(255, 240, 238, 238)
-                                  : const Color.fromARGB(255, 40, 40, 40),
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(8.0)),
-                            ),
-                            child: getQuestionCard(
-                              question.title[model.locale?.languageCode] ?? '',
-                              question.description[
-                                      model.locale?.languageCode] ??
-                                  '',
-                            ),
-                          );
-                        },
-                      );
-                    }).toList(),
-                  ),
-                ),
+                getCaroussel(model),
                 getSubtitle(AppIntl.of(context)!.actions),
                 Expanded(
                   child: ListView.builder(
@@ -97,6 +63,41 @@ class _FaqViewState extends State<FaqView> {
         },
       );
 
+  CarouselSlider getCaroussel(FaqViewModel model) {
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: 260.0,
+      ),
+      items: faq.questions.asMap().entries.map((entry) {
+        final int index = entry.key;
+        final question = faq.questions[index];
+
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.symmetric(horizontal: 5.0),
+              decoration: BoxDecoration(
+                color: Theme.of(context).brightness ==
+                    Brightness.light
+                    ? const Color.fromARGB(255, 240, 238, 238)
+                    : const Color.fromARGB(255, 40, 40, 40),
+                borderRadius:
+                const BorderRadius.all(Radius.circular(8.0)),
+              ),
+              child: getQuestionCard(
+                question.title[model.locale?.languageCode] ?? '',
+                question.description[
+                model.locale?.languageCode] ??
+                    '',
+              ),
+            );
+          },
+        );
+      }).toList(),
+    );
+  }
+  
   Padding getTitle() {
     return Padding(
       padding: const EdgeInsets.only(top: 60.0),
@@ -153,37 +154,29 @@ class _FaqViewState extends State<FaqView> {
   Padding getQuestionCard(String title, String description) {
     return Padding(
       padding: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
-      child: Align(
-        alignment: Alignment.topLeft,
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                title,
-                textScaler: TextScaler.noScaling,
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      fontSize: 20,
-                      color: Theme.of(context).brightness == Brightness.light
-                          ? Colors.black
-                          : Colors.white,
-                    ),
-                textAlign: TextAlign.justify,
-              ),
-              Text(
-                description,
-                textScaler: TextScaler.noScaling,
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      fontSize: 16,
-                      color: Theme.of(context).brightness == Brightness.light
-                          ? Colors.black
-                          : Colors.white,
-                    ),
-                textAlign: TextAlign.justify,
-              ),
-            ],
-          ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Text(
+              title,
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    fontSize: 20,
+                    color: Theme.of(context).brightness == Brightness.light
+                        ? Colors.black
+                        : Colors.white,
+                  ),
+            ),
+            const SizedBox(height: 12),
+            Text(
+              description,
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    fontSize: 16,
+                    color: Theme.of(context).brightness == Brightness.light
+                        ? Colors.black
+                        : Colors.white,
+                  ),
+            ),
+          ],
         ),
       ),
     );
@@ -228,52 +221,46 @@ class _FaqViewState extends State<FaqView> {
     );
   }
 
-  Row getActionCardInfo(BuildContext context, String title, String description,
+  Padding getActionCardInfo(BuildContext context, String title, String description,
       IconData iconName, Color iconColor, Color circleColor) {
-    return Row(
-      children: <Widget>[
-        Column(
-          children: <Widget>[
-            CircleAvatar(
-              backgroundColor: circleColor,
-              radius: 25,
-              child: Icon(iconName, color: iconColor),
-            ),
-          ],
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(15, 15, 0, 15),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Text(
                   title,
                   style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                        fontSize: 18,
-                        color: Theme.of(context).brightness == Brightness.light
-                            ? Colors.black
-                            : Colors.white,
-                      ),
-                  textAlign: TextAlign.left,
+                    fontSize: 18,
+                    color: Theme.of(context).brightness == Brightness.light
+                        ? Colors.black
+                        : Colors.white,
+                  ),
                 ),
-                const SizedBox(height: 10.0),
-                Text(
-                  description,
-                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                        fontSize: 16,
-                        color: Theme.of(context).brightness == Brightness.light
-                            ? Colors.black
-                            : Colors.white,
-                      ),
-                  textAlign: TextAlign.justify,
-                )
-              ],
-            ),
+              ),
+              const SizedBox(width: 16),
+              CircleAvatar(
+                backgroundColor: circleColor,
+                radius: 25,
+                child: Icon(iconName, color: iconColor),
+              ),
+            ],
           ),
-        )
-      ],
+          const SizedBox(height: 12.0),
+          Text(
+            description,
+            style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  fontSize: 16,
+                  color: Theme.of(context).brightness == Brightness.light
+                      ? Colors.black
+                      : Colors.white,
+                )
+          )
+        ],
+      ),
     );
   }
 
