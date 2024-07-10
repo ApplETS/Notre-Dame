632ea59cb150b6e802170628b4fbe9961abbd279
DylanApardian <dylan.apardian.1@ens.etsmtl.ca>
2024-07-10 00:08:49 -0400

    Started parsing logic for xml

--- android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/services/SignetsService.kt
+++ android/app/src/main/kotlin/ca/etsmtl/applets/etsmobile/services/SignetsService.kt
@@ -10,11 +10,24 @@ import ca.etsmtl.applets.etsmobile.services.models.sessionPath
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.toRequestBody
-import org.xmlpull.v1.XmlPullParser
-import org.xmlpull.v1.XmlPullParserFactory
+import org.w3c.dom.Document
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
+import java.io.ByteArrayInputStream
 import java.io.IOException
-import java.io.StringReader
+import java.io.StringWriter
 import java.util.concurrent.TimeUnit
+import javax.xml.namespace.NamespaceContext
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.transform.OutputKeys
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamResult
+import javax.xml.xpath.XPath
+import javax.xml.xpath.XPathConstants
+import javax.xml.xpath.XPathExpressionException
+import javax.xml.xpath.XPathFactory
 
 class SignetsService private constructor(): SignetsServiceProtocol {
 
@@ -49,250 +62,112 @@ class SignetsService private constructor(): SignetsServiceProtocol {
 
             override fun onResponse(call: Call, response: Response) {
                 val data = response.body?.string()
-                Log.d("SignetsService", "getSessions onResponse data: $data")
                 if (data != null) {
-                    val xml = parseXml(data)
-                    Log.d("SignetsService", "parsed data: $data")
-                    val error = xml[sessionErrorPath]
-                    if (error != null) {
-                        Log.d("SignetsService", "error: $error")
-                        completion(Result.failure(ApiError(error)))
-                    } else {
-                        val sessionsXml = xml[sessionPath]
-                        if (sessionsXml != null){
-                            val sessions = sessionsXml.map { Session.fromXml(it.toString()) }
-                            Log.d("SignetsService", "sessions: $sessions")
-                            completion(Result.success(sessions))
-                        }else{
-                            println("sessionsXml is null")
-                        }
+                    val xml = parseXML(data)
+
+                    val errorNode = getNodeAsString(xml, sessionErrorPath)
+                    val trimesterNodes = getNodesByPath(xml, sessionPath)
+
+                    Log.d("SignetsService", "errorNode: $errorNode")
+                    Log.d("SignetsService", "trimesterNodes length: ${trimesterNodes.length}")
+
+                    for (i in 0 until trimesterNodes.length){
+                        val trimesterNode = trimesterNodes.item(i)
+                        val shortName = getNodeByPath(trimesterNode as Document, "abrege")?.textContent
+                        Log.d("SignetsService", "shortName: $shortName")
                     }
+                    completion(Result.success(emptyList()))
+
+//                    if (sessionsXml != null) {
+//                        val sessions = sessionsXml.map { Session.fromXml(it) }
+//                        for (session in sessions) {
+//                            Log.d("SignetsService", "session: " + session.shortName)
+//                        }
+//                        Log.d("SignetsService", "sessions: $sessions")
+//                        completion(Result.success(sessions))
+//                    }
                 }
             }
         })
     }
 
-//    override fun getStudentInfo(user: ca.etsmtl.applets.etsmobile.services.models.MonETSUser, completion: (Result<ProfileStudent, ApiError>) -> Unit) {
-//        val request = createRequest("infoEtudiant", user, emptyMap())
-//
-//        client.newCall(request).enqueue(object : Callback {
-//            override fun onFailure(call: Call, e: IOException) {
-//                completion(Result.failure(ApiError(e.message ?: "Unknown error")))
-//            }
-//
-//            override fun onResponse(call: Call, response: Response) {
-//                val data = response.body?.string()
-//                if (data != null) {
-//                    val xml = parseXml(data)
-//                    val error = xml[studentInfoErrorPath]
-//                    if (error != null) {
-//                        completion(Result.failure(ApiError(error)))
-//                    } else {
-//                        val studentInfo = xml[studentInfoPath]
-//                        val info = ProfileStudent.fromXml(studentInfo)
-//                        completion(Result.success(info))
-//                    }
-//                }
-//            }
-//        })
-//    }
-//
-//    override fun getCourses(user: ca.etsmtl.applets.etsmobile.services.models.MonETSUser, completion: (Result<List<Course>, ApiError>) -> Unit) {
-//        val request = createRequest("listeCours", user, emptyMap())
-//
-//        client.newCall(request).enqueue(object : Callback {
-//            override fun onFailure(call: Call, e: IOException) {
-//                completion(Result.failure(ApiError(e.message ?: "Unknown error")))
-//            }
-//
-//            override fun onResponse(call: Call, response: Response) {
-//                val data = response.body?.string()
-//                if (data != null) {
-//                    val xml = parseXml(data)
-//                    val error = xml[courseErrorPath]
-//                    if (error != null) {
-//                        completion(Result.failure(ApiError(error)))
-//                    } else {
-//                        val coursesXml = xml[coursePath]
-//                        val courses = coursesXml.map { Course.fromXml(it) }
-//                        completion(Result.success(courses))
-//                    }
-//                }
-//            }
-//        })
-//    }
-//
-//    override fun getCoursesActivities(user: ca.etsmtl.applets.etsmobile.services.models.MonETSUser, session: String, courseGroup: String, startDate: Date?, endDate: Date?, completion: (Result<List<CourseActivity>, ApiError>) -> Unit) {
-//        val extraParams = mutableMapOf(
-//            "pSession" to session,
-//            "pCoursGroupe" to courseGroup
-//        )
-//        if (startDate != null && endDate != null) {
-//            val formatter = SimpleDateFormat("YY-MM-dd", Locale.getDefault())
-//            extraParams["pDateDebut"] = formatter.format(startDate)
-//            extraParams["pDateFin"] = formatter.format(endDate)
-//        }
-//
-//        val request = createRequest("lireHoraireDesSeances", user, extraParams)
-//
-//        client.newCall(request).enqueue(object : Callback {
-//            override fun onFailure(call: Call, e: IOException) {
-//                completion(Result.failure(ApiError(e.message ?: "Unknown error")))
-//            }
-//
-//            override fun onResponse(call: Call, response: Response) {
-//                val data = response.body?.string()
-//                if (data != null) {
-//                    val xml = parseXml(data)
-//                    val error = xml[courseActivityErrorPath]
-//                    if (error != null) {
-//                        completion(Result.failure(ApiError(error)))
-//                    } else {
-//                        val courseActivityXml = xml[courseActivityPath]
-//                        val courseActivities = courseActivityXml.map { CourseActivity.fromXml(it) }
-//                        completion(Result.success(courseActivities))
-//                    }
-//                }
-//            }
-//        })
-//    }
-//
-//    override fun getScheduleActivities(user: ca.etsmtl.applets.etsmobile.services.models.MonETSUser, session: String, completion: (Result<List<ScheduleActivity>, ApiError>) -> Unit) {
-//        val extraParams = mapOf("pSession" to session)
-//        val request = createRequest("listeHoraireEtProf", user, extraParams)
-//
-//        client.newCall(request).enqueue(object : Callback {
-//            override fun onFailure(call: Call, e: IOException) {
-//                completion(Result.failure(ApiError(e.message ?: "Unknown error")))
-//            }
-//
-//            override fun onResponse(call: Call, response: Response) {
-//                val data = response.body?.string()
-//                if (data != null) {
-//                    val xml = parseXml(data)
-//                    val error = xml[scheduleActivityErrorPath]
-//                    if (error != null) {
-//                        completion(Result.failure(ApiError(error)))
-//                    } else {
-//                        val scheduleXml = xml[scheduleActivityPath]
-//                        val scheduleActivities = scheduleXml.map { ScheduleActivity.fromXml(it) }
-//                        completion(Result.success(scheduleActivities))
-//                    }
-//                }
-//            }
-//        })
-//    }
-//
-//    override fun getCourseSummary(user: ca.etsmtl.applets.etsmobile.services.models.MonETSUser, course: Course, completion: (Result<CourseSummary, ApiError>) -> Unit) {
-//        val extraParams = mapOf(
-//            "pSigle" to course.acronym,
-//            "pGroupe" to course.group,
-//            "pSession" to course.session
-//        )
-//        val request = createRequest("listeElementsEvaluation", user, extraParams)
-//
-//        client.newCall(request).enqueue(object : Callback {
-//            override fun onFailure(call: Call, e: IOException) {
-//                completion(Result.failure(ApiError(e.message ?: "Unknown error")))
-//            }
-//
-//            override fun onResponse(call: Call, response: Response) {
-//                val data = response.body?.string()
-//                if (data != null) {
-//                    val xml = parseXml(data)
-//                    val error = xml[courseSummaryErrorPath]
-//                    if (error != null) {
-//                        completion(Result.failure(ApiError(error)))
-//                    } else {
-//                        val courseXml = xml[courseSummaryPath]
-//                        val courseSummary = CourseSummary.fromXml(courseXml)
-//                        completion(Result.success(courseSummary))
-//                    }
-//                }
-//            }
-//        })
-//    }
-//
-//    override fun getPrograms(user: ca.etsmtl.applets.etsmobile.services.models.MonETSUser, completion: (Result<List<Program>, ApiError>) -> Unit) {
-//        val request = createRequest("listeProgrammes", user, emptyMap())
-//
-//        client.newCall(request).enqueue(object : Callback {
-//            override fun onFailure(call: Call, e: IOException) {
-//                completion(Result.failure(ApiError(e.message ?: "Unknown error")))
-//            }
-//
-//            override fun onResponse(call: Call, response: Response) {
-//                val data = response.body?.string()
-//                if (data != null) {
-//                    val xml = parseXml(data)
-//                    val error = xml[programErrorPath]
-//                    if (error != null) {
-//                        completion(Result.failure(ApiError(error)))
-//                    } else {
-//                        val programXml = xml[programPath]
-//                        val programs = programXml.map { Program.fromXml(it) }
-//                        completion(Result.success(programs))
-//                    }
-//                }
-//            }
-//        })
-//    }
-//
-//    override fun getCourseReviews(user: ca.etsmtl.applets.etsmobile.services.models.MonETSUser, session: ca.etsmtl.applets.etsmobile.services.models.Session, completion: (Result<List<CourseReview>, ApiError>) -> Unit) {
-//        val extraParams = mapOf("pSession" to session.shortName)
-//        val request = createRequest("lireEvaluationCours", user, extraParams)
-//
-//        client.newCall(request).enqueue(object : Callback {
-//            override fun onFailure(call: Call, e: IOException) {
-//                completion(Result.failure(ApiError(e.message ?: "Unknown error")))
-//            }
-//
-//            override fun onResponse(call: Call, response: Response) {
-//                val data = response.body?.string()
-//                if (data != null) {
-//                    val xml = parseXml(data)
-//                    val error = xml[courseReviewErrorPath]
-//                    if (error != null) {
-//                        completion(Result.failure(ApiError(error)))
-//                    } else {
-//                        val courseReviewXml = xml[courseReviewPath]
-//                        val courseReviews = courseReviewXml.map { CourseReview.fromXml(it) }
-//                        completion(Result.success(courseReviews))
-//                    }
-//                }
-//            }
-//        })
-//    }
+    fun parseXML(xml: String): Document {
+        val factory = DocumentBuilderFactory.newInstance()
+        factory.isNamespaceAware = false
+        val builder = factory.newDocumentBuilder()
+        return builder.parse(ByteArrayInputStream(xml.toByteArray()))
+    }
 
-    private fun parseXml(xml: String): Map<List<String>, String> {
-        val factory = XmlPullParserFactory.newInstance()
-        factory.isNamespaceAware = true
-        val parser = factory.newPullParser()
-        parser.setInput(StringReader(xml))
+    fun getNodeAsString(document: Document, path: String): String? {
+        val node = getNodeByPath(document, path)
+        return node?.let { nodeToString(it) }
+    }
 
-        val result = mutableMapOf<List<String>, String>()
-        val path = mutableListOf<String>()
-        var eventType = parser.eventType
-        var text: String? = null
+    fun getNodeByPath(node: Node, path: String): Node? {
+        val xPath = createXPath()
+        return try {
+            xPath.evaluate(path, node, XPathConstants.NODE) as Node?
+        } catch (e: XPathExpressionException) {
+            e.printStackTrace()
+            null
+        }
+    }
 
-        while (eventType != XmlPullParser.END_DOCUMENT) {
-            when (eventType) {
-                XmlPullParser.START_TAG -> {
-                    path.add(parser.name)
-                }
-                XmlPullParser.TEXT -> {
-                    text = parser.text
-                }
-                XmlPullParser.END_TAG -> {
-                    if (text != null) {
-                        result[path.toList()] = text
-                        text = null
-                    }
-                    path.removeAt(path.size - 1)
+    fun getNodesByPath(node: Node, path: String): NodeList {
+        val xPath = createXPath()
+        return try {
+            xPath.evaluate(path, node, XPathConstants.NODESET) as NodeList
+        } catch (e: XPathExpressionException) {
+            e.printStackTrace()
+            createEmptyNodeList()
+        }
+    }
+
+    fun createXPath(): XPath {
+        val xPath = XPathFactory.newInstance().newXPath()
+        xPath.namespaceContext = object : NamespaceContext {
+            override fun getNamespaceURI(prefix: String): String {
+                return when (prefix) {
+                    "soap" -> "http://schemas.xmlsoap.org/soap/envelope/"
+                    "xsi" -> "http://www.w3.org/2001/XMLSchema-instance"
+                    "xsd" -> "http://www.w3.org/2001/XMLSchema"
+                    "" -> "http://etsmtl.ca/"  // default namespace
+                    else -> ""
                 }
             }
-            eventType = parser.next()
+
+            override fun getPrefix(namespaceURI: String): String? {
+                return null
+            }
+
+            override fun getPrefixes(namespaceURI: String): Iterator<String>? {
+                return null
+            }
         }
-        return result
+        return xPath
+    }
+
+    fun nodeToString(node: Node): String {
+        val transformerFactory = TransformerFactory.newInstance()
+        val transformer = transformerFactory.newTransformer()
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes")
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes")
+        val source = DOMSource(node)
+        val writer = StringWriter()
+        val result = StreamResult(writer)
+        transformer.transform(source, result)
+        return writer.toString()
+    }
+
+    fun createEmptyNodeList(): NodeList {
+        return object : NodeList {
+            override fun getLength(): Int {
+                return 0
+            }
+
+            override fun item(index: Int): Node? {
+                return null
+            }
+        }
     }
 }
