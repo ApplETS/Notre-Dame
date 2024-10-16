cf2b0486c42131b63b797885896876d09be47997
Louis-Philippe Héon <83369199+LouisPhilippeHeon@users.noreply.github.com>
2024-10-16 14:15:02 -0400

    Update to latest Flutter version (#1058)
    * Fix ios build
    
    * Fix feature discovery
    
    * Fix tests
    
    * [BOT] Applying version.
    
    * [BOT] Applying pod update.
    
    * remove url_launcher_ios
    
    * [BOT] Applying pod update.
    
    ---------
    
    Co-authored-by: LouisPhilippeHeon <LouisPhilippeHeon@users.noreply.github.com>
    Co-authored-by: clubapplets-server <clubapplets-server@users.noreply.github.com>

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,7 +3,7 @@ import Flutter
 import GoogleMaps
 import flutter_config
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
