7e6258f8cdaa2e854d5a6b4d56e5d67036a4f55a
NakanLuca <Luca.Nakanishi@gmail.com>
2024-11-10 18:34:15 -0500

    removed feature discovery from dashboard_view.dart

--- lib/features/dashboard/dashboard_view.dart
+++ lib/features/dashboard/dashboard_view.dart
@@ -2,7 +2,6 @@
 import 'package:flutter/material.dart';
 
 // Package imports:
-import 'package:feature_discovery_fork/feature_discovery.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:skeletonizer/skeletonizer.dart';
