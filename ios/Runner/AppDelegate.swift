32527368526fc9c8c1eebc5c655b51668d29c317
Xavier Paquet-Rapold <xavier.paquet-rapold.1@ens.etsmtl.ca>
2024-11-16 19:29:42 -0500

    Try fix

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("\(Bundle.main.object(forInfoDictionaryKey: "MAPS_API_KEY"))")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
