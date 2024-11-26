5327ed9dd880538ec1bf770756757d9d34ba72cd
BenjaminBelanger <benjamin-belanger@hotmail.com>
2024-11-26 14:57:10 -0500

    Move ProgramCompletion in separate file

--- lib/features/student/profile/profile_view.dart
+++ lib/features/student/profile/profile_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 
 // Package imports:
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:notredame/features/student/profile/program_completion.dart';
 import 'package:stacked/stacked.dart';
 
 // Project imports:
@@ -87,7 +88,7 @@ Widget buildPage(BuildContext context, ProfileViewModel model) => Column(
                 children: <Widget>[
                   Padding(
                     padding: const EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 4.0),
-                    child: getProgramCompletion(model, context),
+                    child: ProgramCompletionCard(model: model),
                   ),
                 ],
               ),
@@ -265,53 +266,6 @@ Card getMyBalanceCard(ProfileViewModel model, BuildContext context) {
             child: Text(
               stringBalance,
               style: const TextStyle(fontSize: 18),
-            ),
-          ),
-        ),
-      ],
-    ),
-  );
-}
-
-Card getProgramCompletion(ProfileViewModel model, BuildContext context) {
-  return Card(
-    child: Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(16),
-          child: Text(
-            AppIntl.of(context)!.profile_program_completion,
-            style: const TextStyle(fontSize: 16),
-          ),
-        ),
-        Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 30),
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                TweenAnimationBuilder<double>(
-                  tween: Tween(begin: 0, end: model.programProgression / 100),
-                  duration: const Duration(milliseconds: 900),
-                  builder: (_, value, __) => SizedBox.square(
-                    dimension: 75,
-                    child: CircularProgressIndicator(
-                      value: value,
-                      strokeWidth: 10,
-                      strokeCap: StrokeCap.round,
-                      backgroundColor: Colors.grey[300],
-                      valueColor: const AlwaysStoppedAnimation(Colors.green),
-                    ),
-                  ),
-                ),
-                Text(
-                  model.programProgression > 0
-                      ? '${model.programProgression}%'
-                      : AppIntl.of(context)!
-                          .profile_program_completion_not_available,
-                  style: const TextStyle(fontSize: 20),
-                ),
-              ],
             ),
           ),
         ),
---
+++ lib/features/student/profile/program_completion.dart
@@ -0,0 +1,65 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+// Project imports:
+import 'package:notredame/features/student/profile/profile_viewmodel.dart';
+
+class ProgramCompletionCard extends StatelessWidget {
+  final ProfileViewModel model;
+
+  const ProgramCompletionCard({
+    super.key,
+    required this.model,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text(
+              AppIntl.of(context)!.profile_program_completion,
+              style: const TextStyle(fontSize: 16),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  TweenAnimationBuilder<double>(
+                    tween: Tween(begin: 0, end: model.programProgression / 100),
+                    duration: const Duration(milliseconds: 900),
+                    builder: (_, value, __) => SizedBox.square(
+                      dimension: 75,
+                      child: CircularProgressIndicator(
+                        value: value,
+                        strokeWidth: 10,
+                        strokeCap: StrokeCap.round,
+                        backgroundColor: Colors.grey[300],
+                        valueColor: const AlwaysStoppedAnimation(Colors.green),
+                      ),
+                    ),
+                  ),
+                  Text(
+                    model.programProgression > 0
+                        ? '${model.programProgression}%'
+                        : AppIntl.of(context)!
+                            .profile_program_completion_not_available,
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
