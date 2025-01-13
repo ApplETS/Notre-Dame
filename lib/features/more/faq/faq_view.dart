0da69ae14c228e392ff95f6659adc3f90758188b
EffBureau <francis.bureau2@gmail.com>
2025-01-13 14:54:41 -0500

    Create need_help_notice_dialog

--- lib/features/more/faq/faq_view.dart
+++ lib/features/more/faq/faq_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 // Package imports:
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:notredame/features/app/widgets/need_help_notice_dialog.dart';
 import 'package:stacked/stacked.dart';
 
 // Project imports:
@@ -11,7 +12,6 @@ import 'package:notredame/features/app/widgets/base_scaffold.dart';
 import 'package:notredame/features/more/faq/faq_viewmodel.dart';
 import 'package:notredame/features/more/faq/models/faq.dart';
 import 'package:notredame/features/more/faq/models/faq_actions.dart';
-import 'package:notredame/utils/app_theme.dart';
 
 class FaqView extends StatefulWidget {
   final Color? backgroundColor;
@@ -189,93 +189,8 @@ class _FaqViewState extends State<FaqView> {
           } else if (type.name == ActionType.email.name) {
             showDialog(
                 context: context,
-                builder: (BuildContext context) {
-                  const radius = 5.0;
-                  return AlertDialog(
-                    title: Text(
-                      AppIntl.of(context)!.faq_questions_about_app_alert_title,
-                      style: const TextStyle(
-                          color: AppTheme.primary, fontWeight: FontWeight.bold),
-                    ),
-                    content: Text(
-                        AppIntl.of(context)!
-                            .faq_questions_about_app_alert_confirmation,
-                        style: const TextStyle(fontWeight: FontWeight.bold)),
-                    actions: [
-                      Column(
-                        children: [
-                          SizedBox(
-                            width: double.infinity,
-                            child: TextButton.icon(
-                              style: ButtonStyle(
-                                  backgroundColor: WidgetStateProperty.all<Color>(
-                                      AppTheme.primary),
-                                  foregroundColor:
-                                      WidgetStateProperty.all<Color>(
-                                          AppTheme.lightThemeBackground),
-                                  textStyle: WidgetStateProperty.all<TextStyle>(
-                                      const TextStyle(
-                                          fontWeight: FontWeight.bold)),
-                                  shape: WidgetStateProperty.all<
-                                          RoundedRectangleBorder>(
-                                      RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(radius)))),
-                              onPressed: () {
-                                model.launchWebsite(AppIntl.of(context)!
-                                    .monets_connection_help_page);
-                              },
-                              icon: const Icon(Icons.help_center),
-                              label: Text(AppIntl.of(context)!
-                                  .faq_questions_about_app_alert_password_assistance),
-                            ),
-                          ),
-                          SizedBox(
-                            width: double.infinity,
-                            child: TextButton.icon(
-                              style: ButtonStyle(
-                                  backgroundColor: WidgetStateProperty.all<Color>(
-                                      AppTheme.primary),
-                                  foregroundColor:
-                                      WidgetStateProperty.all<Color>(
-                                          AppTheme.lightThemeBackground),
-                                  textStyle: WidgetStateProperty.all<TextStyle>(
-                                      const TextStyle(
-                                          fontWeight: FontWeight.bold)),
-                                  shape: WidgetStateProperty.all<
-                                          RoundedRectangleBorder>(
-                                      RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(radius)))),
-                              onPressed: () => model.openMail(link, context),
-                              icon: const Icon(Icons.mail),
-                              label: Text(
-                                  AppIntl.of(context)!.continue_to_mail_app),
-                            ),
-                          ),
-                          SizedBox(
-                            width: double.infinity,
-                            child: TextButton.icon(
-                                onPressed: () => Navigator.of(context).pop(),
-                                style: ButtonStyle(
-                                    textStyle: WidgetStateProperty.all<TextStyle>(
-                                        const TextStyle(
-                                            fontWeight: FontWeight.bold)),
-                                    shape: WidgetStateProperty.all(
-                                        RoundedRectangleBorder(
-                                            side: const BorderSide(
-                                                color: AppTheme.primary,
-                                                width: 2.0),
-                                            borderRadius: BorderRadius.circular(
-                                                radius)))),
-                                icon: const Icon(Icons.cancel),
-                                label: Text(
-                                    AppIntl.of(context)!.cancel_button_text)),
-                          ),
-                        ],
-                      ),
-                    ],
-                  );
+                builder: (BuildContext context) {                  
+                  return NeedHelpNoticeDialog(model: model, link: link);
                 });
           }
         },
