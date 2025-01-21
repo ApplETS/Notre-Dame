52686fa76c88bf8d378a4bf5fe10d30d950852bd
EffBureau <francis.bureau2@gmail.com>
2025-01-21 11:48:03 -0500

    Remove "about" from intl variables

--- lib/features/app/widgets/need_help_notice_dialog.dart
+++ lib/features/app/widgets/need_help_notice_dialog.dart
@@ -1,7 +1,11 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
-import 'package:notredame/features/more/faq/faq_viewmodel.dart';
+
+// Package imports:
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+// Project imports:
+import 'package:notredame/features/more/faq/faq_viewmodel.dart';
 import 'package:notredame/utils/app_theme.dart';
 
 class NeedHelpNoticeDialog extends AlertDialog {
@@ -19,12 +23,12 @@ class NeedHelpNoticeDialog extends AlertDialog {
   @override
   Widget build(BuildContext context) => AlertDialog(
         title: Text(
-          AppIntl.of(context)!.faq_questions_about_app_alert_title,
+          AppIntl.of(context)!.faq_questions_app_alert_title,
           style: const TextStyle(
               color: AppTheme.primary, fontWeight: FontWeight.bold),
         ),
         content: Text(
-            AppIntl.of(context)!.faq_questions_about_app_alert_confirmation,
+            AppIntl.of(context)!.faq_questions_app_alert_confirmation,
             style: const TextStyle(fontWeight: FontWeight.bold)),
         actions: [
           getButtons(context),
@@ -33,68 +37,64 @@ class NeedHelpNoticeDialog extends AlertDialog {
 
   Column getButtons(BuildContext context) {
     SizedBox helpPageButton = SizedBox(
-              width: double.infinity,
-              child: TextButton.icon(
-                style: ButtonStyle(
-                    backgroundColor:
-                        WidgetStateProperty.all<Color>(AppTheme.primary),
-                    foregroundColor: WidgetStateProperty.all<Color>(
-                        AppTheme.lightThemeBackground),
-                    textStyle: WidgetStateProperty.all<TextStyle>(
-                        const TextStyle(fontWeight: FontWeight.bold)),
-                    shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(radius)))),
-                onPressed: () {
-                  model.launchWebsite(
-                      AppIntl.of(context)!.monets_connection_help_page);
-                },
-                icon: const Icon(Icons.help_center),
-                label: Text(AppIntl.of(context)!
-                    .faq_questions_about_app_alert_password_assistance),
-              ),
-            );
-    
+      width: double.infinity,
+      child: TextButton.icon(
+        style: ButtonStyle(
+            backgroundColor: WidgetStateProperty.all<Color>(AppTheme.primary),
+            foregroundColor:
+                WidgetStateProperty.all<Color>(AppTheme.lightThemeBackground),
+            textStyle: WidgetStateProperty.all<TextStyle>(
+                const TextStyle(fontWeight: FontWeight.bold)),
+            shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(radius)))),
+        onPressed: () {
+          model.launchWebsite(AppIntl.of(context)!.monets_connection_help_page);
+        },
+        icon: const Icon(Icons.help_center),
+        label: Text(AppIntl.of(context)!
+            .faq_questions_app_alert_password_assistance),
+      ),
+    );
+
     SizedBox continueButton = SizedBox(
-              width: double.infinity,
-              child: TextButton.icon(
-                style: ButtonStyle(
-                    backgroundColor:
-                        WidgetStateProperty.all<Color>(AppTheme.primary),
-                    foregroundColor: WidgetStateProperty.all<Color>(
-                        AppTheme.lightThemeBackground),
-                    textStyle: WidgetStateProperty.all<TextStyle>(
-                        const TextStyle(fontWeight: FontWeight.bold)),
-                    shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(radius)))),
-                onPressed: () => model.openMail(link, context),
-                icon: const Icon(Icons.mail),
-                label: Text(AppIntl.of(context)!.continue_to_mail_app),
-              ),
-            );
+      width: double.infinity,
+      child: TextButton.icon(
+        style: ButtonStyle(
+            backgroundColor: WidgetStateProperty.all<Color>(AppTheme.primary),
+            foregroundColor:
+                WidgetStateProperty.all<Color>(AppTheme.lightThemeBackground),
+            textStyle: WidgetStateProperty.all<TextStyle>(
+                const TextStyle(fontWeight: FontWeight.bold)),
+            shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(radius)))),
+        onPressed: () => model.openMail(link, context),
+        icon: const Icon(Icons.mail),
+        label: Text(AppIntl.of(context)!.continue_to_mail_app),
+      ),
+    );
 
     SizedBox cancelButton = SizedBox(
-              width: double.infinity,
-              child: TextButton.icon(
-                  onPressed: () => Navigator.of(context).pop(),
-                  style: ButtonStyle(
-                      textStyle: WidgetStateProperty.all<TextStyle>(
-                          const TextStyle(fontWeight: FontWeight.bold)),
-                      shape: WidgetStateProperty.all(RoundedRectangleBorder(
-                          side: const BorderSide(
-                              color: AppTheme.primary, width: 2.0),
-                          borderRadius: BorderRadius.circular(radius)))),
-                  icon: const Icon(Icons.cancel),
-                  label: Text(AppIntl.of(context)!.cancel_button_text)),
-            );
-    
+      width: double.infinity,
+      child: TextButton.icon(
+          onPressed: () => Navigator.of(context).pop(),
+          style: ButtonStyle(
+              textStyle: WidgetStateProperty.all<TextStyle>(
+                  const TextStyle(fontWeight: FontWeight.bold)),
+              shape: WidgetStateProperty.all(RoundedRectangleBorder(
+                  side: const BorderSide(color: AppTheme.primary, width: 2.0),
+                  borderRadius: BorderRadius.circular(radius)))),
+          icon: const Icon(Icons.cancel),
+          label: Text(AppIntl.of(context)!.cancel_button_text)),
+    );
+
     return Column(
-          children: [        
-            helpPageButton,
-            continueButton,
-            cancelButton,
-          ],
-        );
+      children: [
+        helpPageButton,
+        continueButton,
+        cancelButton,
+      ],
+    );
   }
 }
