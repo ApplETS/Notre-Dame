1fe37129e76c2902cae8e113a57ae07a599a1b9d
Louis-Philippe <louis.philippe.heon@gmail.com>
2025-02-02 00:01:43 -0500

    Remove unused imports

--- lib/features/welcome/widgets/login_button.dart
+++ lib/features/welcome/widgets/login_button.dart
@@ -4,8 +4,6 @@ import 'package:fluttertoast/fluttertoast.dart';
 // Package imports:
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:notredame/theme/app_theme.dart';
-import 'package:notredame/utils/utils.dart';
-import 'package:notredame/theme/app_palette.dart';
 
 class LoginButton extends StatefulWidget{
   final GlobalKey<FormState> formKey;
