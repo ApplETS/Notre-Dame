6e4c39ffbe52b23ba60f94060e97b21907e3ad7d
EffBureau <francis.bureau2@gmail.com>
2025-02-12 09:24:42 -0500

    Remove tap outside test

--- test/features/app/widgets/need_help_notice_dialog_test.dart
+++ test/features/app/widgets/need_help_notice_dialog_test.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 
 // Package imports:
 import 'package:flutter_test/flutter_test.dart';
-import 'package:notredame/features/app/widgets/need_help_notice_dialog.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 // Project imports:
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:notredame/features/app/widgets/need_help_notice_dialog.dart';
 import '../../../common/helpers.dart';
 import '../../more/settings/mocks/settings_manager_mock.dart';
 
@@ -39,36 +39,6 @@ void main() {
       await tester.tap(cancelButton);
       await tester.pumpAndSettle();
 
-      testWidgets('tapping outside dialog closes it',
-          (WidgetTester tester) async {
-        SettingsManagerMock.stubLocale(settingsManagerMock);
-
-        await tester.pumpWidget(localizedWidget(child: const FaqView()));
-        
-        final Faq faq = Faq();
-
-        await tester.drag(find.byType(ListView), const Offset(0.0, -500));
-        await tester.pumpAndSettle();
-
-        final questionsAbtETSMobileBtn =
-            find.widgetWithText(ElevatedButton, faq.actions[3].title["en"]!);
-        expect(questionsAbtETSMobileBtn, findsOneWidget);
-        
-        await tester.tap(questionsAbtETSMobileBtn);
-        await tester.pumpAndSettle();
-
-        Finder dialog = find.byType(AlertDialog);
-        expect(dialog, findsOne);
-
-        final cancelButton = find.byIcon(Icons.cancel);
-        expect(cancelButton, findsAny);
-
-        await tester.tapAt(Offset.zero);
-        await tester.pumpAndSettle();
-
-        dialog = find.byType(AlertDialog);
-        expect(dialog, findsNothing);
-      });
       final dialogFound = find.byType(dialog.runtimeType);
       expect(dialogFound, findsNothing);
     });
