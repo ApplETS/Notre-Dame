19d72db32f837cd1448679fbc5c9b691a6fcc4a8
Louis-Philippe <louis.philippe.heon@gmail.com>
2025-02-23 15:04:13 -0500

    Remove obsolete method

--- lib/ui/schedule/view_model/schedule_viewmodel.dart
+++ lib/ui/schedule/view_model/schedule_viewmodel.dart
@@ -37,9 +37,4 @@ class ScheduleViewModel extends FutureViewModel {
     calendarFormat = settings[PreferencesFlag.scheduleCalendarFormat] as CalendarTimeFormat;
     setBusyForObject(settings, false);
   }
-
-  // TODO remove
-  bool selectToday() {
-    return true;
-  }
 }
