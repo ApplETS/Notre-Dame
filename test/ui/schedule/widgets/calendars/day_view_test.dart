1782c625bee28abea80fd6e8dcf7bdd122a79b4b
LouisPhilippeHeon <83369199+LouisPhilippeHeon@users.noreply.github.com>
2025-03-02 17:40:03 +0000

    [BOT] Applying format.

--- test/ui/schedule/widgets/calendars/day_view_test.dart
+++ test/ui/schedule/widgets/calendars/day_view_test.dart
@@ -3,14 +3,10 @@ import 'package:flutter/material.dart';
 
 // Package imports:
 import 'package:calendar_view/calendar_view.dart' as calendar_view;
-import 'package:table_calendar/table_calendar.dart' as table_calendar;
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:notredame/data/services/signets-api/models/course_activity.dart';
-import 'package:notredame/ui/dashboard/widgets/course_activity_tile.dart';
-import 'package:notredame/ui/schedule/widgets/calendars/calendar_controller.dart';
-import 'package:notredame/ui/schedule/widgets/calendars/day_calendar.dart';
-import 'package:notredame/ui/schedule/widgets/schedule_calendar_tile.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:table_calendar/table_calendar.dart' as table_calendar;
 
 // Project imports:
 import 'package:notredame/data/repositories/course_repository.dart';
@@ -19,10 +15,14 @@ import 'package:notredame/data/services/analytics_service.dart';
 import 'package:notredame/data/services/navigation_service.dart';
 import 'package:notredame/data/services/networking_service.dart';
 import 'package:notredame/data/services/remote_config_service.dart';
+import 'package:notredame/data/services/signets-api/models/course_activity.dart';
+import 'package:notredame/ui/dashboard/widgets/course_activity_tile.dart';
+import 'package:notredame/ui/schedule/widgets/calendars/calendar_controller.dart';
+import 'package:notredame/ui/schedule/widgets/calendars/day_calendar.dart';
+import 'package:notredame/ui/schedule/widgets/schedule_calendar_tile.dart';
 import '../../../../data/mocks/repositories/course_repository_mock.dart';
 import '../../../../data/mocks/repositories/settings_repository_mock.dart';
 import '../../../../helpers.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   SharedPreferences.setMockInitialValues({});
@@ -64,44 +64,54 @@ void main() {
     });
 
     tearDown(() => {
-      unregister<NavigationService>(),
-      unregister<SettingsRepository>(),
-      unregister<CourseRepository>(),
-      unregister<RemoteConfigService>(),
-      unregister<NetworkingService>(),
-      unregister<AnalyticsService>(),
-    });
+          unregister<NavigationService>(),
+          unregister<SettingsRepository>(),
+          unregister<CourseRepository>(),
+          unregister<RemoteConfigService>(),
+          unregister<NetworkingService>(),
+          unregister<AnalyticsService>(),
+        });
 
     group("list view", () {
       testWidgets("list view", (WidgetTester tester) async {
-          CourseRepositoryMock.stubGetCourses(courseRepositoryMock);
-
-          await tester.runAsync(() async {
-            await tester.pumpWidget(localizedWidget(child: DayCalendar(listView: true, controller: CalendarController())));
-            await tester.pumpAndSettle();
-          });
-
-          expect(find.byType(PageView), findsExactly(2), reason: "The page view is displayed");
-      });
-
-      testWidgets("list view with no event in selected day", (WidgetTester tester) async {
         CourseRepositoryMock.stubGetCourses(courseRepositoryMock);
 
         await tester.runAsync(() async {
-          await tester.pumpWidget(localizedWidget(child: DayCalendar(listView: true, controller: CalendarController())));
+          await tester.pumpWidget(localizedWidget(
+              child: DayCalendar(
+                  listView: true, controller: CalendarController())));
+          await tester.pumpAndSettle();
+        });
+
+        expect(find.byType(PageView), findsExactly(2),
+            reason: "The page view is displayed");
+      });
+
+      testWidgets("list view with no event in selected day",
+          (WidgetTester tester) async {
+        CourseRepositoryMock.stubGetCourses(courseRepositoryMock);
+
+        await tester.runAsync(() async {
+          await tester.pumpWidget(localizedWidget(
+              child: DayCalendar(
+                  listView: true, controller: CalendarController())));
           await tester.pumpAndSettle();
         });
 
         expect(find.text(intl.schedule_no_event), findsOneWidget);
       });
 
-      testWidgets("list view with event in selected day", (WidgetTester tester) async {
-        CourseRepositoryMock.stubCoursesActivities(courseRepositoryMock, toReturn: activites);
+      testWidgets("list view with event in selected day",
+          (WidgetTester tester) async {
+        CourseRepositoryMock.stubCoursesActivities(courseRepositoryMock,
+            toReturn: activites);
 
         CourseRepositoryMock.stubGetCourses(courseRepositoryMock);
 
         await tester.runAsync(() async {
-          await tester.pumpWidget(localizedWidget(child: DayCalendar(listView: true, controller: CalendarController())));
+          await tester.pumpWidget(localizedWidget(
+              child: DayCalendar(
+                  listView: true, controller: CalendarController())));
           await tester.pumpAndSettle();
         });
 
@@ -115,19 +125,26 @@ void main() {
         CourseRepositoryMock.stubGetCourses(courseRepositoryMock);
 
         await tester.runAsync(() async {
-          await tester.pumpWidget(localizedWidget(child: DayCalendar(listView: false, controller: CalendarController())));
+          await tester.pumpWidget(localizedWidget(
+              child: DayCalendar(
+                  listView: false, controller: CalendarController())));
           await tester.pumpAndSettle();
         });
 
-        expect(find.byType(calendar_view.DayView), findsOneWidget, reason: "The page view is displayed");
+        expect(find.byType(calendar_view.DayView), findsOneWidget,
+            reason: "The page view is displayed");
       });
 
-      testWidgets("calendar view with event in selected day", (WidgetTester tester) async {
-        CourseRepositoryMock.stubCoursesActivities(courseRepositoryMock, toReturn: activites);
+      testWidgets("calendar view with event in selected day",
+          (WidgetTester tester) async {
+        CourseRepositoryMock.stubCoursesActivities(courseRepositoryMock,
+            toReturn: activites);
         CourseRepositoryMock.stubGetCourses(courseRepositoryMock);
 
         await tester.runAsync(() async {
-          await tester.pumpWidget(localizedWidget(child: DayCalendar(listView: false, controller: CalendarController())));
+          await tester.pumpWidget(localizedWidget(
+              child: DayCalendar(
+                  listView: false, controller: CalendarController())));
           await tester.pumpAndSettle();
         });
 
@@ -136,19 +153,25 @@ void main() {
     });
 
     group("header", () {
-      testWidgets("calendar view with event in selected day", (WidgetTester tester) async {
-        CourseRepositoryMock.stubCoursesActivities(courseRepositoryMock, toReturn: activites);
+      testWidgets("calendar view with event in selected day",
+          (WidgetTester tester) async {
+        CourseRepositoryMock.stubCoursesActivities(courseRepositoryMock,
+            toReturn: activites);
         CourseRepositoryMock.stubGetCourses(courseRepositoryMock);
 
         await tester.runAsync(() async {
-          await tester.pumpWidget(localizedWidget(child: DayCalendar(listView: false, controller: CalendarController())));
+          await tester.pumpWidget(localizedWidget(
+              child: DayCalendar(
+                  listView: false, controller: CalendarController())));
           await tester.pumpAndSettle();
         });
 
         final dayIndicatorFinder = find.byWidgetPredicate((widget) {
           if (widget is Text) {
             final textStyle = widget.style;
-            return widget.data == '2' && textStyle != null && textStyle.fontSize == 12.0;
+            return widget.data == '2' &&
+                textStyle != null &&
+                textStyle.fontSize == 12.0;
           }
           return false;
         });
