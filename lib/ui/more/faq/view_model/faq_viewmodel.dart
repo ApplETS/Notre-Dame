2699f58449996246956e703307d54ad45288ccb5
EffBureau <francis.bureau2@gmail.com>
2025-03-24 11:44:28 -0400

    Format

--- lib/ui/more/faq/view_model/faq_viewmodel.dart
+++ lib/ui/more/faq/view_model/faq_viewmodel.dart
@@ -3,19 +3,20 @@ import 'package:flutter/material.dart';
 
 // Package imports:
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:notredame/data/services/remote_config_service.dart';
 import 'package:stacked/stacked.dart';
 
 // Project imports:
 import 'package:notredame/data/repositories/settings_repository.dart';
 import 'package:notredame/data/services/launch_url_service.dart';
+import 'package:notredame/data/services/remote_config_service.dart';
 import 'package:notredame/domain/constants/app_info.dart';
 import 'package:notredame/locator.dart';
 
 class FaqViewModel extends BaseViewModel {
   final SettingsRepository _settingsManager = locator<SettingsRepository>();
   final LaunchUrlService _launchUrlService = locator<LaunchUrlService>();
-  final RemoteConfigService _remoteConfigService = locator<RemoteConfigService>();
+  final RemoteConfigService _remoteConfigService =
+      locator<RemoteConfigService>();
 
   Locale? get locale => _settingsManager.locale;
 
@@ -24,7 +25,8 @@ class FaqViewModel extends BaseViewModel {
   }
 
   void launchPasswordReset() {
-    _launchUrlService.launchInBrowser(_remoteConfigService.signetsPasswordResetUrl);
+    _launchUrlService
+        .launchInBrowser(_remoteConfigService.signetsPasswordResetUrl);
   }
 
   Future<void> openMail(String addressEmail, BuildContext context) async {
