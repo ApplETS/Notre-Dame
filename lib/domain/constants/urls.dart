40b304cc634c1b98d759edd984689f375073cdc1
Xavier Paquet-Rapold <xavier.paquet-rapold.1@ens.etsmtl.ca>
2025-03-25 11:53:09 -0400

    Use prod URL

--- lib/domain/constants/urls.dart
+++ lib/domain/constants/urls.dart
@@ -10,5 +10,5 @@ class Urls {
   static const String clubYoutube =
       "https://youtube.com/channel/UCiSzzfW1bVbE_0KcEZO52ew";
   static const String clubDiscord = "https://discord.gg/adMkWptn6Y";
-  static const String signetsAPI = "etsmobileapi-preprod.etsmtl.ca";
+  static const String signetsAPI = "etsmobileapi.etsmtl.ca";
 }
