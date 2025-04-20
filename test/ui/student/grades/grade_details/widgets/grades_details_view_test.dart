9be570fc73fb8bdfc46cec81750c466bea1a6d48
DylanApardian <dylan.apardian.1@ens.etsmtl.ca>
2025-04-20 15:08:16 -0400

    Removed unused import

--- test/ui/student/grades/grade_details/widgets/grades_details_view_test.dart
+++ test/ui/student/grades/grade_details/widgets/grades_details_view_test.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 // Package imports:
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 // Project imports:
