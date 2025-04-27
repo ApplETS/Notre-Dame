bf1d988a14cdf4e988342b3cfc231771b6e215ce
Louis-Philippe <louis.philippe.heon@gmail.com>
2025-04-26 20:31:20 -0400

    Removed unused stuff

--- lib/ui/dashboard/widgets/grades_card.dart
+++ lib/ui/dashboard/widgets/grades_card.dart
@@ -6,21 +6,17 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
 // Project imports:
-import 'package:notredame/data/services/navigation_service.dart';
 import 'package:notredame/data/services/signets-api/models/course.dart';
-import 'package:notredame/domain/constants/router_paths.dart';
-import 'package:notredame/locator.dart';
 import 'package:notredame/ui/core/themes/app_theme.dart';
 import 'package:notredame/ui/core/ui/dismissible_card.dart';
 import 'package:notredame/ui/student/grades/widgets/grade_button.dart';
 
 class GradesCard extends StatelessWidget {
-  final NavigationService _navigationService = locator<NavigationService>();
   final List<Course> courses;
   final VoidCallback onDismissed;
   final bool loading;
 
-  GradesCard({super.key, required this.courses, required this.onDismissed, required this.loading});
+  const GradesCard({super.key, required this.courses, required this.onDismissed, required this.loading});
 
   @override
   Widget build(BuildContext context) {
