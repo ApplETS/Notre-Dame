9ef931276c4742d2d88a6c4b9b882a085de17b7a
Xavier Paquet-Rapold <xavier.paquet-rapold.1@ens.etsmtl.ca>
2025-05-02 15:52:02 -0400

    Make stream repository more general

--- lib/data/repositories/list_sessions_repository.dart
+++ lib/data/repositories/list_sessions_repository.dart
@@ -1,20 +1,34 @@
-import 'package:notredame/data/repositories/base_observable_list_repository.dart';
+import 'package:collection/collection.dart';
+import 'package:notredame/data/repositories/base_stream_repository.dart';
 import 'package:notredame/data/services/signets_client.dart';
 import 'package:notredame/domain/models/signets-api/session.dart';
 import 'package:notredame/locator.dart';
 
-class ListSessionsRepository extends BaseObservableListRepository<Session> {
+class ListSessionsRepository extends BaseStreamRepository<List<Session>> {
   static const String sessionsKey = 'sessions';
   
   final _signetsClient = locator<SignetsClient>();
 
   ListSessionsRepository() : super(sessionsKey);
 
-  Future<String?> getSessions({bool forceUpdate = false}) async {
-    List<Future<String?>> tasks = [];
+  Future getSessions({bool forceUpdate = false}) async {
+    List<Future> tasks = [];
     tasks.add(getFromCache(Session.fromJson));
     tasks.add(getFromApi(() => _signetsClient.getSessionList(), forceUpdate: forceUpdate));
 
-    return Future.wait(tasks).then((value) => value.firstWhere((message) => message != null, orElse: () => null));
+    return Future.wait(tasks);
+  }
+
+  Session? getActiveSession() {
+    if (value == null) {
+      return null;
+    } else {
+      DateTime now = DateTime.now();
+      now = DateTime(now.year, now.month, now.day);
+
+      return value!.firstWhereOrNull((session) =>
+          now.isAfter(session.startDate) && now.isBefore(session.endDate)
+          || now.isAtSameMomentAs(session.startDate) || now.isAtSameMomentAs(session.endDate));
+    }
   }
 }
