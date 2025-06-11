fefab6cb2610198774f351501a5cf15adae60043
Louis-Philippe Héon <83369199+LouisPhilippeHeon@users.noreply.github.com>
2025-06-10 21:40:56 -0400

    Translucent gesture bar (#1249)
    * fix problem
    
    * put it on startup view instead
    
    * [BOT] Applying version.
    
    * [BOT] Applying format.
    
    * fix typo
    
    ---------
    
    Co-authored-by: clubapplets-server <1958869+clubapplets-server@users.noreply.github.com>

--- lib/ui/startup/widgets/startup_view.dart
+++ lib/ui/startup/widgets/startup_view.dart
@@ -1,5 +1,6 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 // Package imports:
 import 'package:flutter_svg/flutter_svg.dart';
@@ -15,35 +16,40 @@ class StartUpView extends StatelessWidget {
   const StartUpView({super.key});
 
   @override
-  Widget build(BuildContext context) => ViewModelBuilder<StartUpViewModel>.nonReactive(
-    viewModelBuilder: () => StartUpViewModel(intl: AppIntl.of(context)!),
-    onViewModelReady: (StartUpViewModel model) {
-      model.handleStartUp();
-    },
-    builder: (context, model, child) => Scaffold(
-      backgroundColor: context.theme.appColors.backgroundVibrant,
-      body: SafeArea(
-        minimum: const EdgeInsets.all(20),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Hero(
-                tag: 'ets_logo',
-                child: SvgPicture.asset(
-                  "assets/images/ets_white_logo.svg",
-                  excludeFromSemantics: true,
-                  width: 90,
-                  height: 90,
-                  colorFilter: ColorFilter.mode(context.theme.appColors.loginAccent, BlendMode.srcIn),
+  Widget build(BuildContext context) {
+    // Make Android's gesture bar transparent on older versions of Android (it is now the default behavior)
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(systemNavigationBarColor: Colors.transparent));
+
+    return ViewModelBuilder<StartUpViewModel>.nonReactive(
+      viewModelBuilder: () => StartUpViewModel(intl: AppIntl.of(context)!),
+      onViewModelReady: (StartUpViewModel model) {
+        model.handleStartUp();
+      },
+      builder: (context, model, child) => Scaffold(
+        backgroundColor: context.theme.appColors.backgroundVibrant,
+        body: SafeArea(
+          minimum: const EdgeInsets.all(20),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Hero(
+                  tag: 'ets_logo',
+                  child: SvgPicture.asset(
+                    "assets/images/ets_white_logo.svg",
+                    excludeFromSemantics: true,
+                    width: 90,
+                    height: 90,
+                    colorFilter: ColorFilter.mode(context.theme.appColors.loginAccent, BlendMode.srcIn),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 15),
-              CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(AppPalette.grey.white)),
-            ],
+                const SizedBox(height: 15),
+                CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(AppPalette.grey.white)),
+              ],
+            ),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
