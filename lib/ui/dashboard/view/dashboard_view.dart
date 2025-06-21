db6735a336dd5033f13a1005106f50e5e3af14c8
Ethan-Estrada <83309694+Ethan-Estrada@users.noreply.github.com>
2025-06-21 02:42:31 +0000

    [BOT] Applying format.

--- lib/ui/dashboard/view/dashboard_view.dart
+++ lib/ui/dashboard/view/dashboard_view.dart
@@ -1,8 +1,8 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 // Package imports:
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:stacked/stacked.dart';
 
 // Project imports:
