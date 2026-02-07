c80a4f83a7a93096aaab611e54cebff2865ec8b6
Louis-Philippe <louis.philippe.heon@gmail.com>
2026-02-07 16:53:41 -0500

    Use color of event in list view

--- lib/ui/schedule/widgets/course_activity_tile.dart
+++ lib/ui/schedule/widgets/course_activity_tile.dart
@@ -44,22 +44,8 @@ class CourseActivityTile extends StatelessWidget {
             Text(timeFormat.format(event.endTime!), style: Theme.of(context).textTheme.bodySmall),
           ],
         ),
-        Skeleton.shade(child: VerticalDivider(color: colorFor(event.courseName), thickness: 2)),
+        Skeleton.shade(child: VerticalDivider(color: event.color, thickness: 2)),
       ],
     ),
   );
-
-  /// Generate a color based on [text].
-  Color colorFor(String text) {
-    var hash = 0;
-    for (var i = 0; i < text.length; i++) {
-      hash = text.codeUnitAt(i) + ((hash << 5) - hash);
-    }
-    final finalHash = hash.abs() % (256 * 256 * 256);
-    final red = (finalHash & 0xFF0000) >> 16;
-    final blue = (finalHash & 0xFF00) >> 8;
-    final green = finalHash & 0xFF;
-    final color = Color.fromRGBO(red, green, blue, 1);
-    return color;
-  }
 }
