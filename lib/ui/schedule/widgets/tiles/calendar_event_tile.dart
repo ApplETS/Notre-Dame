7b1cbc57c089c63675a5d6dab9460e6d3728a493
Louis-Philippe <louis.philippe.heon@gmail.com>
2026-02-10 19:11:44 -0500

    Replace modal by sheet

--- lib/ui/schedule/widgets/tiles/calendar_event_tile.dart
+++ lib/ui/schedule/widgets/tiles/calendar_event_tile.dart
@@ -11,6 +11,7 @@ import 'package:notredame/data/models/event_data.dart';
 // Project imports:
 import 'package:notredame/l10n/app_localizations.dart';
 import 'package:notredame/ui/core/themes/app_palette.dart';
+import 'package:notredame/ui/core/themes/app_theme.dart';
 
 class CalendarEventTile extends StatefulWidget {
   final EventData event;
@@ -28,144 +29,181 @@ class _CalendarEventTileState extends State<CalendarEventTile> {
     final courseLocation = widget.event.locations?.join(", ");
     final courseType = widget.event.activityName;
     final teacherName = widget.event.teacherName;
-    final startTime = "${widget.event.startTime.hour}:${widget.event.startTime.minute.toString().padLeft(2, '0')}";
+    final startTime = DateFormat.Hm().format(widget.event.startTime);
     final endTime = DateFormat.Hm().format(widget.event.endTime);
 
-    showDialog(
+    showModalBottomSheet(
       context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-          title: FittedBox(
-            fit: BoxFit.scaleDown,
-            alignment: Alignment.topLeft,
+      isScrollControlled: true,
+      backgroundColor: context.theme.scaffoldBackgroundColor,
+      builder: (context) {
+        return SafeArea(
+          child: Container(
+            clipBehavior: Clip.hardEdge,
+            decoration: const BoxDecoration(borderRadius: BorderRadius.vertical(top: Radius.circular(24))),
             child: Column(
+              mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  widget.event.group ?? widget.event.courseAcronym,
-                  style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                Container(
+                  decoration: BoxDecoration(color: context.theme.appColors.modalTitle),
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          widget.event.group ?? widget.event.courseAcronym,
+                          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          widget.event.courseName,
+                          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-                Text(widget.event.courseName, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+
+                Container(
+                  margin: EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        spacing: 6.0,
+                        children: [
+                          _sheetRow(
+                            "${AppIntl.of(widget.buildContext)!.schedule_calendar_from_time} "
+                            "$startTime "
+                            "${AppIntl.of(widget.buildContext)!.schedule_calendar_to_time} "
+                            "$endTime",
+                            Icons.access_time,
+                          ),
+                          if (courseLocation != null) _sheetRow(courseLocation, Icons.location_pin),
+                          if (courseType != null) _sheetRow(courseType, Icons.book),
+                          if (teacherName != null)
+                            _sheetRow(
+                              "${AppIntl.of(widget.buildContext)!.schedule_calendar_by} $teacherName",
+                              Icons.person,
+                            ),
+                        ],
+                      ),
+
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: ElevatedButton.icon(
+                          onPressed: () => Navigator.of(context).pop(),
+                          icon: Icon(Icons.close),
+                          label: Text(
+                            AppIntl.of(context)!.close_button_text,
+                            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "${AppIntl.of(widget.buildContext)!.schedule_calendar_from_time} $startTime ${AppIntl.of(widget.buildContext)!.schedule_calendar_to_time} $endTime",
-                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-              ),
-              if (courseLocation != null)
-                Text(courseLocation, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
-              if (courseType != null)
-                Text(courseType, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
-              if (teacherName != null)
-                Text(
-                  "${AppIntl.of(widget.buildContext)!.schedule_calendar_by} $teacherName",
-                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-                ),
-            ],
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: Text(
-                AppIntl.of(context)!.close_button_text,
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-            ),
-          ],
-          actionsPadding: const EdgeInsets.all(10),
         );
       },
+    );
+  }
+
+  Row _sheetRow(String text, IconData icon) {
+    return Row(
+      spacing: 8.0,
+      children: [
+        Icon(icon),
+        Text(text, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+      ],
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-        builder: (context, constraints) {
-          final width = constraints.maxWidth;
-          final height = constraints.maxHeight;
-          final aspectRatio = width / height;
-          // If the card is 4 times wider than tall and is smaller than 60 in height
-          final wideLayout = aspectRatio > 4 || height < 60;
-          // If both dimensions are smaller than 80 or a dimension is smaller than 40
-          final isSmall = max(width, height) < 80 || min(width, height) < 40;
+      builder: (context, constraints) {
+        final width = constraints.maxWidth;
+        final height = constraints.maxHeight;
+        final aspectRatio = width / height;
+        // If the card is 4 times wider than tall and is smaller than 60 in height
+        final wideLayout = aspectRatio > 4 || height < 60;
+        // If both dimensions are smaller than 80 or a dimension is smaller than 40
+        final isSmall = max(width, height) < 80 || min(width, height) < 40;
 
-          final description = widget.event.calendarDescription(!wideLayout);
+        final description = widget.event.calendarDescription(!wideLayout);
 
-          if (isSmall || description == null) {
-            return Card(
-              color: widget.event.color,
-              clipBehavior: Clip.hardEdge,
-              margin: EdgeInsets.all(3.0),
-              child: InkWell(
-                onTap: _showTileInfo,
-                child: Center(
-                  child: RotatedBox(
-                    quarterTurns: width > 40 ? 0 : 1,
-                    child: AutoSizeText(
-                      widget.event.courseAcronym,
-                      minFontSize: 8,
-                      maxLines: 1,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontWeight: FontWeight.bold, color: AppPalette.grey.white),
-                    ),
-                  ),
-                ),
-              ),
-            );
-          }
-
+        if (isSmall || description == null) {
           return Card(
             color: widget.event.color,
             clipBehavior: Clip.hardEdge,
             margin: EdgeInsets.all(3.0),
             child: InkWell(
               onTap: _showTileInfo,
-              child: Flex(
-                direction: wideLayout ? Axis.horizontal : Axis.vertical,
-                crossAxisAlignment: wideLayout ? CrossAxisAlignment.center : CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: wideLayout ? min(70, width / 2.5) : null,
-                    child: DecoratedBox(
-                      decoration: BoxDecoration(color: const Color.fromRGBO(0, 0, 0, 0.2)),
-                      child: Center(
-                        child: Padding(
-                          padding: const EdgeInsets.only(top: 8.0, bottom: 6.0),
-                          child: AutoSizeText(
-                            widget.event.courseAcronym,
-                            minFontSize: 8,
-                            maxLines: 1,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(fontWeight: FontWeight.bold, color: AppPalette.grey.white, height: 1),
-                          ),
+              child: Center(
+                child: RotatedBox(
+                  quarterTurns: width > 40 ? 0 : 1,
+                  child: AutoSizeText(
+                    widget.event.courseAcronym,
+                    minFontSize: 8,
+                    maxLines: 1,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.bold, color: AppPalette.grey.white),
+                  ),
+                ),
+              ),
+            ),
+          );
+        }
+
+        return Card(
+          color: widget.event.color,
+          clipBehavior: Clip.hardEdge,
+          margin: EdgeInsets.all(3.0),
+          child: InkWell(
+            onTap: _showTileInfo,
+            child: Flex(
+              direction: wideLayout ? Axis.horizontal : Axis.vertical,
+              crossAxisAlignment: wideLayout ? CrossAxisAlignment.center : CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: wideLayout ? min(70, width / 2.5) : null,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(color: const Color.fromRGBO(0, 0, 0, 0.2)),
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 8.0, bottom: 6.0),
+                        child: AutoSizeText(
+                          widget.event.courseAcronym,
+                          minFontSize: 8,
+                          maxLines: 1,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontWeight: FontWeight.bold, color: AppPalette.grey.white, height: 1),
                         ),
                       ),
                     ),
                   ),
-                  Flexible(
-                    child: Container(
-                      padding: widget.padding,
-                      child: AutoSizeText(
-                        description,
-                        minFontSize: 8,
-                        maxLines: widget.event.locations!.length + 1,
-                        style: TextStyle(color: AppPalette.grey.white),
-                      ),
+                ),
+                Flexible(
+                  child: Container(
+                    padding: widget.padding,
+                    child: AutoSizeText(
+                      description,
+                      minFontSize: 8,
+                      maxLines: widget.event.locations!.length + 1,
+                      style: TextStyle(color: AppPalette.grey.white),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          );
-        },
-
+          ),
+        );
+      },
     );
   }
 }
