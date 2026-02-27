fbc8a22a7dcb2060435528a1a4d61937c4e54aaa
Louis-Philippe <louis.philippe.heon@gmail.com>
2026-02-26 20:24:58 -0500

    Remove useless property

--- test/ui/more/map/widgets/map_view_test.dart
+++ test/ui/more/map/widgets/map_view_test.dart
@@ -5,16 +5,12 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 // Project imports:
-import 'package:notredame/l10n/app_localizations.dart';
 import 'package:notredame/ui/more/map/map_view.dart';
 import '../../../../helpers.dart';
 
 void main() {
   group('MapView - ', () {
-    late AppIntl appIntl;
-
     setUp(() async {
-      appIntl = await setupAppIntl();
       setupNetworkingServiceMock();
     });
 
