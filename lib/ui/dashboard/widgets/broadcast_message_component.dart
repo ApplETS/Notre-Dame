77ebf7f1bf29a76868d536f06e10dcbf1a012540
arihuss <ariane.hussaini.1@ens.etsmtl.ca>
2026-03-20 00:06:24 -0400

    add analytic when hyperlink is pressed

--- lib/ui/dashboard/widgets/broadcast_message_component.dart
+++ lib/ui/dashboard/widgets/broadcast_message_component.dart
@@ -6,12 +6,15 @@ import 'package:notredame/data/models/broadcast_message.dart';
 import 'package:notredame/l10n/app_localizations.dart';
 import 'package:notredame/ui/core/themes/app_theme.dart';
 import 'package:notredame/ui/dashboard/view_model/dashboard_viewmodel.dart';
+import 'package:notredame/data/services/analytics_service.dart';
+import 'package:notredame/locator.dart';
 
 class BroadcastMessageComponent extends StatelessWidget {
   final BroadcastMessage? broadcastMessage;
   final TextStyle? style;
+  final AnalyticsService _analyticsService = locator<AnalyticsService>();
 
-  const BroadcastMessageComponent({super.key, this.broadcastMessage, required this.style});
+  BroadcastMessageComponent({super.key, this.broadcastMessage, required this.style});
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +33,10 @@ class BroadcastMessageComponent extends StatelessWidget {
         if (broadcastMessage!.url != "")
           IconButton(
             tooltip: AppIntl.of(context)!.website_open,
-            onPressed: () => DashboardViewModel.launchBroadcastUrl(broadcastMessage!.url),
+            onPressed: () {
+              _analyticsService.logEvent("BroadcastMessage", "Hyperlink pressed");
+              DashboardViewModel.launchBroadcastUrl(broadcastMessage!.url);
+            },
             icon: Icon(Icons.open_in_new, color: context.theme.primaryTextTheme.titleLarge!.color, size: 26.0),
           ),
       ],
