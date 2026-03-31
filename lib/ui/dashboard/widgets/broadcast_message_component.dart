c4798a1c7a70597fa4fe18dbe53e8061cd59af64
arihuss <ariane.hussaini.1@ens.etsmtl.ca>
2026-03-31 14:08:12 -0400

    format fix

--- lib/ui/dashboard/widgets/broadcast_message_component.dart
+++ lib/ui/dashboard/widgets/broadcast_message_component.dart
@@ -1,6 +1,8 @@
 // Flutter imports:
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:auto_size_text/auto_size_text.dart';
 
 // Project imports:
 import 'package:notredame/data/models/broadcast_message.dart';
