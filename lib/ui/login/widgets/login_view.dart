a8513526da8d7ad7a02427035cc21ec75ec811ad
DogithuboM <114493805+DogithuboM@users.noreply.github.com>
2026-04-08 00:30:01 -0400

    ajout du gradient et espacement entre les boutons

--- lib/ui/login/widgets/login_view.dart
+++ lib/ui/login/widgets/login_view.dart
@@ -29,80 +29,97 @@ class _LoginViewState extends State<LoginView> {
       viewModelBuilder: () => LoginViewModel(intl: AppIntl.of(context)!),
       builder: (context, model, child) => Scaffold(
         backgroundColor: context.theme.appColors.backgroundVibrant,
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: Hero(
-                    tag: 'ets_logo',
-                    child: SvgPicture.asset(
-                      "assets/images/ets_white_logo.svg",
-                      excludeFromSemantics: true,
-                      width: 90,
-                      height: 90,
-                      colorFilter: ColorFilter.mode(context.theme.appColors.loginAccent, BlendMode.srcIn),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 30),
-                  child: Text(
-                    AppIntl.of(context)!.login_startup_title,
-                    style: TextStyle(fontWeight: FontWeight.normal, fontSize: 18, color: AppPalette.grey.white),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: Stack(
-                    alignment: Alignment.centerRight,
-                    children: [
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          setState(() {
-                            _isLoading = true;
-                          });
-                          model.authenticate();
-                        },
-                        icon: const FaIcon(FontAwesomeIcons.lockOpen, color: Colors.white),
-                        label: Text(
-                          AppIntl.of(context)!.login_action_sign_in,
-                          style: TextStyle(color: AppPalette.grey.white, fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: AppPalette.grey.darkGrey,
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                          minimumSize: const Size(300, 50),
-                        ),
-                      ),
-                      if (_isLoading) ...[
-                        CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(AppPalette.grey.white)),
-                      ],
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: ElevatedButton.icon(
-                    onPressed: () {
-                      model.navigationService.pushNamed(RouterPaths.faq);
-                    },
-                    icon: const FaIcon(FontAwesomeIcons.question, color: Colors.white),
-                    label: const Text("FAQ"),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: AppPalette.grey.black,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                    ),
-                  ),
-                ),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                context.theme.appColors.backgroundVibrant,
+                AppPalette.etsDarkRed,
               ],
             ),
           ),
-        ),
+          child:Center(
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Hero(
+                      tag: 'ets_logo',
+                      child: SvgPicture.asset(
+                        "assets/images/ets_white_logo.svg",
+                        excludeFromSemantics: true,
+                        width: 90,
+                        height: 90,
+                        colorFilter: ColorFilter.mode(context.theme.appColors.loginAccent, BlendMode.srcIn),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 30),
+                    child: Text(
+                      AppIntl.of(context)!.login_startup_title,
+                      style: TextStyle(fontWeight: FontWeight.normal, fontSize: 18, color: AppPalette.grey.white),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 40),
+                    child: Stack(
+                      alignment: Alignment.centerRight,
+                      children: [
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            setState(() {
+                              _isLoading = true;
+                            });
+                            model.authenticate();
+                          },
+                          icon: const FaIcon(FontAwesomeIcons.lockOpen, color: Colors.white),
+                          label: Text(
+                            AppIntl.of(context)!.login_action_sign_in,
+                            style: TextStyle(color: AppPalette.grey.white, fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: AppPalette.grey.black,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                            minimumSize: const Size(300, 50),
+                          ),
+                        ),
+                        if (_isLoading) ...[
+                          CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(AppPalette.grey.white)),
+                        ],
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+                        model.navigationService.pushNamed(RouterPaths.faq);
+                      },
+                      icon: const FaIcon(FontAwesomeIcons.question, color: Colors.white),
+                      label: Text(
+                        "FAQ",
+                        style: TextStyle(color: AppPalette.grey.white, fontSize: 13, fontWeight: FontWeight.bold),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppPalette.grey.darkGrey,
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        )
       ),
     );
   }
